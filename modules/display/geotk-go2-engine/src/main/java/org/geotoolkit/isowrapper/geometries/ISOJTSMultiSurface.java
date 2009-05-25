96f9941ec295b21c6bfb312bfa86a5dd03235d41
Johann Sorel <johann.sorel@geomatys.fr>
2009-05-25 12:23:16 +0200

    continue work on iso jts wrapper

--- modules/display/geotk-go2-engine/src/main/java/org/geotoolkit/isowrapper/geometries/ISOJTSMultiSurface.java
+++ modules/display/geotk-go2-engine/src/main/java/org/geotoolkit/isowrapper/geometries/ISOJTSMultiSurface.java
@@ -1,6 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *    Geotoolkit - An Open Source Java GIS Toolkit
+ *    http://www.geotoolkit.org
+ *
+ *    (C) 2009, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  */
 
 package org.geotoolkit.isowrapper.geometries;
@@ -9,28 +21,34 @@ import java.util.HashSet;
 import java.util.Set;
 import org.opengis.geometry.aggregate.MultiSurface;
 import org.opengis.geometry.primitive.OrientableSurface;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 /**
  *
- * @author sorel
+ * @author Johann Sorel (Geomatys)
  */
 public class ISOJTSMultiSurface extends AbstractISOJTSGeometry<com.vividsolutions.jts.geom.MultiPolygon> implements MultiSurface{
 
-    public ISOJTSMultiSurface(com.vividsolutions.jts.geom.MultiPolygon pl) {
-        super(pl);
+    private Set<OrientableSurface> surfaces;
+
+    public ISOJTSMultiSurface(com.vividsolutions.jts.geom.MultiPolygon pl, CoordinateReferenceSystem crs) {
+        super(pl,crs);
     }
 
     @Override
     public Set<OrientableSurface> getElements() {
-        Set<OrientableSurface> curves = new HashSet<OrientableSurface>();
-        int num = jtsGeometry.getNumGeometries();
+        if(surfaces == null){
+            surfaces = new HashSet<OrientableSurface>();
+        
+            int num = jtsGeometry.getNumGeometries();
 
-        for(int i=0; i<num;i++){
-            com.vividsolutions.jts.geom.Polygon poly = (com.vividsolutions.jts.geom.Polygon) jtsGeometry.getGeometryN(i);
-            curves.add(new ISOJTSSurface(poly));
+            for(int i=0; i<num;i++){
+                com.vividsolutions.jts.geom.Polygon poly = (com.vividsolutions.jts.geom.Polygon) jtsGeometry.getGeometryN(i);
+                surfaces.add(new ISOJTSSurface(poly,crs));
+            }
         }
 
-        return curves;
+        return surfaces;
     }
 
     @Override
