b9c332c27ef38c27309984c12f69b0ba5bdd233d
cedricbr <cedric.briancon@geomatys.fr>
2009-05-28 13:33:17 +0200

    jaxb: update headers of all jaxb modules to the geotoolkit header.

--- modules/jaxb-xml-binding/geotk-xml-csw/src/main/java/org/geotoolkit/csw/xml/v202/GetRecordByIdResponseType.java
+++ modules/jaxb-xml-binding/geotk-xml-csw/src/main/java/org/geotoolkit/csw/xml/v202/GetRecordByIdResponseType.java
@@ -2,7 +2,7 @@
  *    Geotoolkit - An Open Source Java GIS Toolkit
  *    http://www.geotoolkit.org
  *
- *    (C) 2009, Geomatys
+ *    (C) 2008 - 2009, Geomatys
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
--- modules/jaxb-xml-binding/geotk-xml-sld/src/main/java/org/geotoolkit/sld/xml/v110/package-info.java
+++ modules/jaxb-xml-binding/geotk-xml-sld/src/main/java/org/geotoolkit/sld/xml/v110/package-info.java
@@ -1,23 +1,18 @@
 /*
- *    GeoTools - The Open Source Java GIS Toolkit
- *    http://geotools.org
- * 
- *    (C) 2008, Open Source Geospatial Foundation (OSGeo)
+ *    Geotoolkit - An Open Source Java GIS Toolkit
+ *    http://www.geotoolkit.org
+ *
+ *    (C) 2008 - 2009, Geomatys
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
- *    License as published by the Free Software Foundation;
- *    version 2.1 of the License.
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
  *
  *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
- */
-/**
- * {@linkplain org.geotoolkit.metadata.iso.DefaultMetaData Metadata} implementation. An explanation
- * for this package is provided in the {@linkplain org.opengis.metadata OpenGIS&reg; javadoc}.
- * The remaining discussion on this page is specific to the Geotools implementation.
  */
 @XmlSchema(elementFormDefault= XmlNsForm.QUALIFIED, namespace="http://www.opengis.net/sld")
 package org.geotoolkit.sld.xml.v110;
