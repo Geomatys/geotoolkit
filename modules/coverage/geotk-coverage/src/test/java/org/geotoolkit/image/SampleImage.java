4f1fef80e780bc4c31940051e6c16b22c780903f
desruisseaux <martin.desruisseaux@geomatys.fr>
2009-08-25 17:27:57 +0200

    Coverage: avoid a platform-dependant test failure.

--- modules/coverage/geotk-coverage/src/test/java/org/geotoolkit/image/SampleImage.java
+++ modules/coverage/geotk-coverage/src/test/java/org/geotoolkit/image/SampleImage.java
@@ -22,11 +22,15 @@ import javax.imageio.ImageIO;
 import java.lang.ref.Reference;
 import java.lang.ref.SoftReference;
 import java.awt.image.BufferedImage;
+import java.awt.image.SampleModel;
+import java.awt.image.PixelInterleavedSampleModel;
+import java.awt.image.MultiPixelPackedSampleModel;
 
 import org.geotoolkit.test.Commons;
 import org.geotoolkit.test.TestData;
 
 import static org.junit.Assert.*;
+import static org.junit.Assume.*;
 
 
 
@@ -34,7 +38,7 @@ import static org.junit.Assert.*;
  * Enumeration of sample data that can be loaded by {@link ImageTestCase#loadSampleImage}.
  *
  * @author Martin Desruisseaux (Geomatys)
- * @version 3.02
+ * @version 3.03
  *
  * @since 3.00
  */
@@ -42,12 +46,12 @@ public enum SampleImage {
     /**
      * A RGB image having a rotation.
      */
-    RGB_ROTATED("MODIS.png", 3650654124L),
+    RGB_ROTATED("MODIS.png", PixelInterleavedSampleModel.class, 3650654124L),
 
     /**
      * Sea Surface Temperature (SST) using indexed color model.
      */
-    INDEXED("QL95209.png", 1873283205L),
+    INDEXED("QL95209.png", PixelInterleavedSampleModel.class, 1873283205L),
 
     /**
      * Chlorophyl-a concentration (CHL) using indexed color model. From a color model point of view,
@@ -57,18 +61,25 @@ public enum SampleImage {
      *
      * @since 3.02
      */
-    INDEXED_LOGARITHMIC("CHL01195.png", 600909489L),
+    INDEXED_LOGARITHMIC("CHL01195.png", PixelInterleavedSampleModel.class, 600909489L),
 
     /**
      * The boundary of a shape. Used for testing <cite>scanline flood fill</cite>.
      * There is no accurate CRS associated to this image.
      */
-    CONTOUR("Contour.png", 1089548139L);
+    CONTOUR("Contour.png", MultiPixelPackedSampleModel.class, 1089548139L);
 
     /**
      * The filename to load.
      */
     public final String filename;
+
+    /**
+     * The expected sample model for which the checksum has been computed.
+     * Some platform will use a different sample model, in which case we
+     * can not expect the checksum to work.
+     */
+    private final Class<? extends SampleModel> model;
 
     /**
      * The expected checksum.
@@ -86,13 +97,18 @@ public enum SampleImage {
      * @param filename The filename of the image to be loaded.
      * @param checksum The expected checksum.
      */
-    private SampleImage(final String filename, final long checksum) {
+    private SampleImage(final String filename, final Class<? extends SampleModel> model, final long checksum) {
         this.filename = filename;
+        this.model    = model;
         this.checksum = checksum;
     }
 
     /**
      * Loads the sample image. This method may returns a cached image for performance reasons.
+     * <p>
+     * If the loaded image doesn't use the expected sample model (which may happen on some
+     * platforms), the checksum will not be correct. In such case the test will be stopped
+     * with an {@code assumeTrue(boolean)} instruction.
      *
      * @return The sample image.
      * @throws IOException If the image can not be read.
@@ -106,6 +122,7 @@ public enum SampleImage {
             }
         }
         image = ImageIO.read(TestData.url(SampleImage.class, filename));
+        assumeTrue(model.isInstance(image.getSampleModel()));
         assertEquals(filename, checksum, Commons.checksum(image));
         cache = new SoftReference<BufferedImage>(image);
         return image;
