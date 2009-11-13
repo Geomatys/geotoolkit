8c675a6e7c42968eab31d030e4163a1afcab4202
Johann Sorel <johann.sorel@geomatys.fr>
2009-11-13 12:35:01 +0100

    Improve wfs datastore

--- modules/datastore/geotk-wfs/src/main/java/org/geotoolkit/data/wfs/WFSDataStore.java
+++ modules/datastore/geotk-wfs/src/main/java/org/geotoolkit/data/wfs/WFSDataStore.java
@@ -20,13 +20,20 @@ package org.geotoolkit.data.wfs;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Level;
+import javax.xml.bind.JAXBException;
 
 import org.geotoolkit.data.AbstractDataStore;
+import org.geotoolkit.data.DataUtilities;
+import org.geotoolkit.data.FeatureCollectionUtilities;
 import org.geotoolkit.data.FeatureReader;
 import org.geotoolkit.data.collection.FeatureCollection;
+import org.geotoolkit.feature.xml.XmlFeatureReader;
+import org.geotoolkit.feature.xml.jaxp.JAXPEventFeatureReader;
 import org.geotoolkit.wfs.xml.v110.FeatureTypeListType;
 import org.geotoolkit.wfs.xml.v110.FeatureTypeType;
 import org.geotoolkit.wfs.xml.v110.WFSCapabilitiesType;
@@ -55,7 +62,11 @@ public class WFSDataStore extends AbstractDataStore{
         final FeatureTypeListType lst = capabilities.getFeatureTypeList();
         for(final FeatureTypeType ftt : lst.getFeatureType()){
             final String typeName = ftt.getName().getLocalPart();
-            types.put(typeName,requestType(typeName));
+            try {
+                types.put(typeName, requestType(typeName));
+            } catch (IOException ex) {
+                LOGGER.log(Level.SEVERE, null, ex);
+            }
         }
 
     }
@@ -88,20 +99,48 @@ public class WFSDataStore extends AbstractDataStore{
      */
     @Override
     public FeatureReader<SimpleFeatureType, SimpleFeature> getFeatureReader(String typeName) throws IOException {
-        return null;
+        final SimpleFeatureType sft = types.get(typeName);
+        final FeatureCollection<SimpleFeatureType,SimpleFeature> collection = requestFeature(typeName);
+
+        return DataUtilities.wrapToReader(sft, collection.features());
     }
 
-    private SimpleFeatureType requestType(String typeName){
+    private SimpleFeatureType requestType(String typeName) throws IOException{
         final DescribeFeatureTypeRequest request = server.createDescribeFeatureType();
         request.setTypeName(typeName);
-
-//        request.getURL();
+        try {
+            request.getURL();
+        } catch (MalformedURLException ex) {
+            throw new IOException(ex);
+        }
 
         return null;
     }
 
     private FeatureCollection<SimpleFeatureType,SimpleFeature> requestFeature(String typeName) throws IOException {
         final SimpleFeatureType sft = types.get(typeName);
+
+        final GetFeatureRequest request = server.createGetFeature();
+        request.setTypeName(typeName);
+
+        try {
+            final XmlFeatureReader reader = new JAXPEventFeatureReader(sft);
+            final URL url = request.getURL();
+            System.out.println("Get feature url : " + url);
+            final Object result = reader.read(url.openStream());
+
+            if(result instanceof SimpleFeature){
+                final SimpleFeature sf = (SimpleFeature) result;
+                final FeatureCollection<SimpleFeatureType,SimpleFeature> col = FeatureCollectionUtilities.createCollection("id", sft);
+                col.add(sf);
+                return col;
+            }else if(result instanceof FeatureCollection){
+                return (FeatureCollection<SimpleFeatureType, SimpleFeature>) result;
+            }
+
+        } catch (JAXBException ex) {
+            throw new IOException(ex);
+        }
 
         return null;
     }
