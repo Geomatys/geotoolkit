f71f2750340522c4c8a1dae57aed26b8e579bf49
Johann Sorel <johann.sorel@geomatys.fr>
2010-01-15 19:43:57 +0100

    Storage : pull out feature mapper interface and implementation

---
+++ modules/datastore/geotk-datastore-core/src/main/java/org/geotoolkit/data/memory/mapping/FeatureMapper.java
@@ -0,0 +1,29 @@
+/*
+ *    Geotoolkit - An Open Source Java GIS Toolkit
+ *    http://www.geotoolkit.org
+ *
+ *    (C) 2010, Johann Sorel
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotoolkit.data.memory.mapping;
+
+import org.opengis.feature.Feature;
+
+/**
+ *
+ * @author Johann Sorel (Puzzle-GIS)
+ * @module pending
+ */
+public interface FeatureMapper {
+
+    Feature transform(Feature feature);
+}
