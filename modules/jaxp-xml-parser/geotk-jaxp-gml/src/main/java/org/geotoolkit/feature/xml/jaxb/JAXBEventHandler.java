5e72872da6d486a44a336101fe95b855422b6a15
guilhem <guilhem@piexiu.interne-mtd>
2010-02-01 16:22:19 +0100

    WFS: clean JAXB event handler

--- modules/jaxp-xml-parser/geotk-jaxp-gml/src/main/java/org/geotoolkit/feature/xml/jaxb/JAXBEventHandler.java
+++ modules/jaxp-xml-parser/geotk-jaxp-gml/src/main/java/org/geotoolkit/feature/xml/jaxb/JAXBEventHandler.java
@@ -1,35 +1,41 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *    Geotoolkit - An Open Source Java GIS Toolkit
+ *    http://www.geotoolkit.org
+ *
+ *    (C) 2009, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  */
+
 
 package org.geotoolkit.feature.xml.jaxb;
 
 import javax.xml.bind.ValidationEvent;
 import javax.xml.bind.ValidationEventHandler;
-import javax.xml.bind.ValidationEventLocator;
 
 /**
- *
- * @author guilhem
+ * A JAXB Validation Handler allowing not to lost the Exception throw by the CoordinateReferenceSystemAdapter
+ * When a GML geometry srsName is unknow.
+ * 
+ * @author Guilhem Legal (Geomatys)
  */
 public class JAXBEventHandler implements ValidationEventHandler {
 
-      public boolean handleEvent(ValidationEvent ve) {
-        if (ve.getSeverity()==ve.FATAL_ERROR ||ve .getSeverity()==ve.ERROR){
-            ValidationEventLocator  locator = ve.getLocator();
-            /*print message from valdation event
-            System.out.println("Message is " + ve.getMessage());
-            //output line and column number
-            System.out.println("Column is " +
-                  locator.getColumnNumber() +
-                  " at line number " + locator.getLineNumber());*/
+    @Override
+    public boolean handleEvent(ValidationEvent ve) {
+        if (ve.getSeverity() == ve.FATAL_ERROR || ve.getSeverity() == ve.ERROR) {
             if (ve.getMessage() != null && ve.getMessage().startsWith("org.opengis.referencing.NoSuchAuthorityCodeException")) {
                 return false;
             }
-
-         }
-         return true;
-       }
-
-   }
+        }
+        return true;
+    }
+}
