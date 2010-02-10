da0e415b18786b497d61222bdeb34b5f558e04b2
Johann Sorel <johann.sorel@geomatys.fr>
2010-02-10 11:07:47 +0100

    SVG : cleaning SVGUtils

--- modules/display/geotk-render/src/main/java/org/geotoolkit/renderer/svg/SvgUtils.java
+++ modules/display/geotk-render/src/main/java/org/geotoolkit/renderer/svg/SvgUtils.java
@@ -19,19 +19,21 @@ package org.geotoolkit.renderer.svg;
 
 import java.awt.Dimension;
 import java.awt.Image;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
+import java.awt.RenderingHints;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.Reader;
+
 import org.apache.batik.transcoder.SVGAbstractTranscoder;
 import org.apache.batik.transcoder.TranscoderException;
 import org.apache.batik.transcoder.TranscoderInput;
 import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.TranscodingHints;
+import org.apache.batik.transcoder.TranscodingHints.Key;
+import org.geotoolkit.factory.Hints;
+
+import org.w3c.dom.Document;
+import org.xml.sax.XMLReader;
 
 /**
  *
@@ -40,34 +42,74 @@ import org.apache.batik.transcoder.TranscoderOutput;
  */
 public class SvgUtils {
 
-    private static SvgUtils INSTANCE = null;
-
     private SvgUtils(){}
 
-    public Image read(URL svgfile, final Dimension dim) throws TranscoderException, IOException{
+    public static Image read(Document svgdom, final Dimension dim) throws TranscoderException, IOException{
+        return read(svgdom,dim,null);
+    }
+
+    public static Image read(InputStream stream, final Dimension dim) throws TranscoderException, IOException{
+       return read(stream,dim,null);
+    }
+
+    public static Image read(Reader svgfile, final Dimension dim) throws TranscoderException, IOException{
+        return read(svgfile,dim,null);
+    }
+
+    public static Image read(String svgfile, final Dimension dim) throws TranscoderException, IOException{
+        return read(svgfile,dim,null);
+    }
+
+    public static Image read(XMLReader svgfile, final Dimension dim) throws TranscoderException, IOException{
+        return read(svgfile,dim,null);
+    }
+
+    public static Image read(Document svgdom, final Dimension dim, RenderingHints hints) throws TranscoderException, IOException{
+        final TranscoderInput in = new TranscoderInput(svgdom);
+        return read(in, dim, hints);
+    }
+
+    public static Image read(InputStream stream, final Dimension dim, RenderingHints hints) throws TranscoderException, IOException{
+        final TranscoderInput in = new TranscoderInput(stream);
+        return read(in, dim, hints);
+    }
+
+    public static Image read(Reader svgfile, final Dimension dim, RenderingHints hints) throws TranscoderException, IOException{
+        final TranscoderInput in = new TranscoderInput(svgfile);
+        return read(in, dim, hints);
+    }
     
+    public static Image read(String svgfile, final Dimension dim, RenderingHints hints) throws TranscoderException, IOException{
+        final TranscoderInput in = new TranscoderInput(svgfile);
+        return read(in, dim, hints);
+    }
+
+    public static Image read(XMLReader svgfile, final Dimension dim, RenderingHints hints) throws TranscoderException, IOException{
+        final TranscoderInput in = new TranscoderInput(svgfile);
+        return read(in, dim, hints);
+    }
+
+    public static Image read(TranscoderInput in, Dimension dim, RenderingHints hints) throws TranscoderException, IOException{
+
         if(dim.height <=0 || dim.width <=0){
             throw new IllegalArgumentException("Height and width must be superior to 0");
         }
         final InternalTranscoder svgTranscoder = new InternalTranscoder();
-        final TranscoderInput in               = new TranscoderInput(svgfile.openStream());
         TranscoderOutput out                   = new TranscoderOutput();
-        
+
+        if(hints != null){
+            for(Object key : hints.keySet()){
+                if(key instanceof TranscodingHints.Key){
+                    svgTranscoder.addTranscodingHint((Key) key,hints.get(key));
+                }
+            }
+        }
+
         svgTranscoder.addTranscodingHint(SVGAbstractTranscoder.KEY_HEIGHT, new Float(dim.height));
         svgTranscoder.addTranscodingHint(SVGAbstractTranscoder.KEY_WIDTH, new Float(dim.width));
         svgTranscoder.transcode(in, out);
 
         return svgTranscoder.getImage();
     }
-
-    public static SvgUtils getInstance(){
-
-        if(INSTANCE == null){
-            INSTANCE = new SvgUtils();
-        }
-
-        return INSTANCE;
-    }
-
 
 }
