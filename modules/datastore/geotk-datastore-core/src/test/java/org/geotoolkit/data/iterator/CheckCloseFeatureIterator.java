81d7d3ac21367f9d102d281d507bef05949e4491
Johann Sorel <johann.sorel@geomatys.fr>
2010-03-15 17:13:25 +0100

    Datastore : add tests on retype and reproject iterator and tests for iterator closing

---
+++ modules/datastore/geotk-datastore-core/src/test/java/org/geotoolkit/data/iterator/CheckCloseFeatureIterator.java
@@ -0,0 +1,77 @@
+/*
+ *    Geotoolkit - An Open Source Java GIS Toolkit
+ *    http://www.geotoolkit.org
+ *
+ *    (C) 2010, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+
+package org.geotoolkit.data.iterator;
+
+import org.geotoolkit.data.FeatureIterator;
+import org.geotoolkit.data.FeatureReader;
+import org.opengis.feature.Feature;
+import org.opengis.feature.type.FeatureType;
+
+/**
+ * Wrap a feature Iterator and check that it is properly close.
+ *
+ * @author Johann Sorel (Geomatys)
+ * @module pending
+ */
+public class CheckCloseFeatureIterator implements FeatureIterator<Feature>, FeatureReader<FeatureType, Feature>{
+
+    private final FeatureIterator ite;
+    private boolean isClosed = false;
+
+    public CheckCloseFeatureIterator(FeatureIterator ite){
+        if(ite == null){
+            throw new NullPointerException("Iterator can not be null.");
+        }
+        this.ite = ite;
+    }
+
+    public boolean isClosed(){
+        return isClosed;
+    }
+
+    @Override
+    public Feature next() {
+        return ite.next();
+    }
+
+    @Override
+    public void close() {
+        isClosed = true;
+        ite.close();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return ite.hasNext();
+    }
+
+    @Override
+    public void remove() {
+        ite.remove();
+    }
+
+    @Override
+    public FeatureType getFeatureType() {
+        if(ite instanceof FeatureReader){
+            return ((FeatureReader)ite).getFeatureType();
+        }else{
+            throw new IllegalArgumentException("Iterator is not a Feature reader.");
+        }
+    }
+
+}
--- modules/datastore/geotk-datastore-core/src/test/java/org/geotoolkit/data/iterator/GenericIteratorTest.java
+++ modules/datastore/geotk-datastore-core/src/test/java/org/geotoolkit/data/iterator/GenericIteratorTest.java
@@ -2,7 +2,7 @@
  *    Geotoolkit - An Open Source Java GIS Toolkit
  *    http://www.geotoolkit.org
  *
- *    (C) 2009, Open Source Geospatial Foundation (OSGeo)
+ *    (C) 2010, Open Source Geospatial Foundation (OSGeo)
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
@@ -17,11 +17,15 @@
 
 package org.geotoolkit.data.iterator;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import junit.framework.TestCase;
 
+import org.geotoolkit.data.DataStoreException;
 import org.geotoolkit.data.DataUtilities;
 import org.geotoolkit.data.FeatureCollection;
 import org.geotoolkit.data.FeatureIterator;
@@ -31,12 +35,19 @@ import org.geotoolkit.data.memory.GenericEmptyFeatureIterator;
 import org.geotoolkit.data.memory.GenericFilterFeatureIterator;
 import org.geotoolkit.data.memory.GenericMaxFeatureIterator;
 import org.geotoolkit.data.memory.GenericModifyFeatureIterator;
+import org.geotoolkit.data.memory.GenericReprojectFeatureIterator;
+import org.geotoolkit.data.memory.GenericRetypeFeatureIterator;
 import org.geotoolkit.data.memory.GenericSortByFeatureIterator;
 import org.geotoolkit.data.memory.GenericStartIndexFeatureIterator;
+import org.geotoolkit.data.query.Query;
+import org.geotoolkit.data.query.QueryBuilder;
 import org.geotoolkit.factory.FactoryFinder;
 import org.geotoolkit.feature.DefaultName;
+import org.geotoolkit.feature.SchemaException;
 import org.geotoolkit.feature.simple.SimpleFeatureBuilder;
 import org.geotoolkit.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotoolkit.referencing.CRS;
+import org.geotoolkit.referencing.crs.DefaultGeographicCRS;
 
 import org.junit.Test;
 
@@ -49,6 +60,8 @@ import org.opengis.filter.Filter;
 import org.opengis.filter.FilterFactory;
 import org.opengis.filter.sort.SortBy;
 import org.opengis.filter.sort.SortOrder;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.NoSuchAuthorityCodeException;
 
 /**
  * Tests of the different iterators.
@@ -57,39 +70,62 @@ import org.opengis.filter.sort.SortOrder;
  */
 public class GenericIteratorTest extends TestCase{
 
+    private static final double DELTA = 0.000001d;
     private static final FilterFactory FF = FactoryFinder.getFilterFactory(null);
+    private static final GeometryFactory GF = new GeometryFactory();
 
     private final FeatureCollection<SimpleFeature> collection;
     private final Name name;
-    private final SimpleFeatureType type;
+    private final SimpleFeatureType originalType;
+    private final SimpleFeatureType reducedType;
+    private final SimpleFeatureType reprojectedType;
     private final String id1;
     private final String id2;
     private final String id3;
 
-    public GenericIteratorTest(){
+    public GenericIteratorTest() throws NoSuchAuthorityCodeException, FactoryException{
         final SimpleFeatureTypeBuilder builder = new SimpleFeatureTypeBuilder();
         name = new DefaultName("http://test.com", "TestSchema");
         builder.reset();
         builder.setName(name);
+        builder.add("att_geom", Point.class, DefaultGeographicCRS.WGS84);
         builder.add("att_string", String.class);
         builder.add("att_double", Double.class);
-        type = builder.buildFeatureType();
+        originalType = builder.buildFeatureType();
 
-        collection = DataUtilities.collection("id", type);
+        //build a reduced type for retype iterator
+        builder.reset();
+        builder.setName(name);
+        builder.add("att_double", Double.class);
+        reducedType = builder.buildFeatureType();
 
-        SimpleFeature sf = SimpleFeatureBuilder.template(type, "");
+        //build a reprojected type for reproject iterator
+        builder.reset();
+        builder.setName(name);
+        builder.add("att_geom", Point.class, CRS.decode("EPSG:4326"));
+        builder.add("att_string", String.class);
+        builder.add("att_double", Double.class);
+        reprojectedType = builder.buildFeatureType();
+
+
+        collection = DataUtilities.collection("id", originalType);
+
+        SimpleFeature sf = SimpleFeatureBuilder.template(originalType, "");
+        sf.setAttribute("att_geom", GF.createPoint(new Coordinate(3, 0)));
         sf.setAttribute("att_string", "bbb");
         sf.setAttribute("att_double", 3d);
         collection.add(sf);
         id1 = name.getLocalPart()+"."+0;
 
-        sf = SimpleFeatureBuilder.template(type, "");
+        sf = SimpleFeatureBuilder.template(originalType, "");
+        sf.setAttribute("att_geom", GF.createPoint(new Coordinate(1, 0)));
         sf.setAttribute("att_string", "ccc");
         sf.setAttribute("att_double", 1d);
         collection.add(sf);
         id2 = name.getLocalPart()+"."+1;
 
-        sf = SimpleFeatureBuilder.template(type, "");
+        sf = SimpleFeatureBuilder.template(originalType, "");
+        sf.setAttribute("att_geom", GF.createPoint(new Coordinate(2, 0)));
         sf.setAttribute("att_string", "aaa");
         sf.setAttribute("att_double", 2d);
         collection.add(sf);
@@ -206,8 +242,17 @@ public class GenericIteratorTest extends TestCase{
             //ok
         }
 
+        //check has next do not iterate
         ite = GenericFilterFeatureIterator.wrap(collection.iterator(), Filter.INCLUDE);
         testIterationOnNext(ite, 3);
+
+        //check sub iterator is properly closed
+        CheckCloseFeatureIterator checkIte = new CheckCloseFeatureIterator(collection.iterator());
+        assertFalse(checkIte.isClosed());
+        ite = GenericFilterFeatureIterator.wrap(checkIte, Filter.INCLUDE);
+        while(ite.hasNext()) ite.next();
+        ite.close();
+        assertTrue(checkIte.isClosed());
     }
 
     @Test
@@ -227,15 +272,24 @@ public class GenericIteratorTest extends TestCase{
             //ok
         }
 
+        //check has next do not iterate
         ite = GenericMaxFeatureIterator.wrap(collection.iterator(), 10);
         testIterationOnNext(ite, 3);
+
+        //check sub iterator is properly closed
+        CheckCloseFeatureIterator checkIte = new CheckCloseFeatureIterator(collection.iterator());
+        assertFalse(checkIte.isClosed());
+        ite = GenericMaxFeatureIterator.wrap(checkIte, 10);
+        while(ite.hasNext()) ite.next();
+        ite.close();
+        assertTrue(checkIte.isClosed());
     }
 
     @Test
     public void testModifyIterator(){
         Filter filter = Filter.INCLUDE;
         Map<PropertyDescriptor,Object> values = new HashMap<PropertyDescriptor, Object>();
-        values.put(type.getDescriptor("att_string"), "toto");
+        values.put(originalType.getDescriptor("att_string"), "toto");
 
         FeatureIterator ite = GenericModifyFeatureIterator.wrap(collection.iterator(), filter, values);
         assertEquals(3, DataUtilities.calculateCount(ite));
@@ -272,8 +326,99 @@ public class GenericIteratorTest extends TestCase{
             //ok
         }
 
+        //check has next do not iterate
         ite = GenericModifyFeatureIterator.wrap(collection.iterator(), filter, values);
         testIterationOnNext(ite, 3);
+
+        //check sub iterator is properly closed
+        CheckCloseFeatureIterator checkIte = new CheckCloseFeatureIterator(collection.iterator());
+        assertFalse(checkIte.isClosed());
+        ite = GenericModifyFeatureIterator.wrap(checkIte, filter, values);
+        while(ite.hasNext()) ite.next();
+        ite.close();
+        assertTrue(checkIte.isClosed());
+
+    }
+
+    @Test
+    public void testReprojectFeatureIterator() throws DataStoreException, FactoryException, SchemaException{
+        QueryBuilder qb = new QueryBuilder();
+        qb.setTypeName(originalType.getName());
+        Query query = qb.buildQuery();
+        FeatureReader reader = collection.getSession().getDataStore().getFeatureReader(query);
+
+        FeatureReader retyped = GenericReprojectFeatureIterator.wrap(reader, CRS.decode("EPSG:4326"));
+        assertEquals(reprojectedType,retyped.getFeatureType());
+
+        Feature f;
+
+        f = retyped.next();
+        assertEquals(3, f.getProperties().size());
+        assertEquals(GF.createPoint(new Coordinate(0, 3)).toString(), f.getProperty("att_geom").getValue().toString());
+
+        f = retyped.next();
+        assertEquals(3, f.getProperties().size());
+        assertEquals(GF.createPoint(new Coordinate(0, 1)).toString(), f.getProperty("att_geom").getValue().toString());
+
+        f = retyped.next();
+        assertEquals(3, f.getProperties().size());
+        assertEquals(GF.createPoint(new Coordinate(0, 2)).toString(), f.getProperty("att_geom").getValue().toString());
+
+
+        //check has next do not iterate
+        reader = collection.getSession().getDataStore().getFeatureReader(query);
+        retyped = GenericReprojectFeatureIterator.wrap(reader, CRS.decode("EPSG:4326"));
+        testIterationOnNext(retyped, 3);
+
+        //check sub iterator is properly closed
+        reader = collection.getSession().getDataStore().getFeatureReader(query);
+        CheckCloseFeatureIterator checkIte = new CheckCloseFeatureIterator(reader);
+        assertFalse(checkIte.isClosed());
+        retyped = GenericReprojectFeatureIterator.wrap(checkIte, CRS.decode("EPSG:4326"));
+        while(retyped.hasNext()) retyped.next();
+        retyped.close();
+        assertTrue(checkIte.isClosed());
+    }
+
+    @Test
+    public void testRetypeFeatureIterator() throws DataStoreException{
+        QueryBuilder qb = new QueryBuilder();
+        qb.setTypeName(originalType.getName());
+        Query query = qb.buildQuery();
+        FeatureReader reader = collection.getSession().getDataStore().getFeatureReader(query);
+
+        FeatureReader retyped = GenericRetypeFeatureIterator.wrap(reader,reducedType,null);
+        assertEquals(reducedType,retyped.getFeatureType());
+
+        Feature f;
+
+        f = retyped.next();
+        assertEquals(1, f.getProperties().size());
+        assertEquals(3d, (Double)f.getProperty("att_double").getValue(), DELTA);
+
+        f = retyped.next();
+        assertEquals(1, f.getProperties().size());
+        assertEquals(1d, (Double)f.getProperty("att_double").getValue(), DELTA);
+
+        f = retyped.next();
+        assertEquals(1, f.getProperties().size());
+        assertEquals(2d, (Double)f.getProperty("att_double").getValue(), DELTA);
+
+
+        //check has next do not iterate
+        reader = collection.getSession().getDataStore().getFeatureReader(query);
+        retyped = GenericRetypeFeatureIterator.wrap(reader,reducedType,null);
+        testIterationOnNext(retyped, 3);
+
+        //check sub iterator is properly closed
+        reader = collection.getSession().getDataStore().getFeatureReader(query);
+        CheckCloseFeatureIterator checkIte = new CheckCloseFeatureIterator(reader);
+        assertFalse(checkIte.isClosed());
+        retyped = GenericRetypeFeatureIterator.wrap(checkIte,reducedType,null);
+        while(retyped.hasNext()) retyped.next();
+        retyped.close();
+        assertTrue(checkIte.isClosed());
+
     }
 
     @Test
@@ -298,8 +443,17 @@ public class GenericIteratorTest extends TestCase{
             //ok
         }
 
+        //check has next do not iterate
         ite = GenericSortByFeatureIterator.wrap(collection.iterator(), sorts);
         testIterationOnNext(ite, 3);
+
+        //check sub iterator is properly closed
+        CheckCloseFeatureIterator checkIte = new CheckCloseFeatureIterator(collection.iterator());
+        assertFalse(checkIte.isClosed());
+        ite = GenericSortByFeatureIterator.wrap(checkIte, sorts);
+        while(ite.hasNext()) ite.next();
+        ite.close();
+        assertTrue(checkIte.isClosed());
     }
 
     @Test
@@ -319,8 +473,17 @@ public class GenericIteratorTest extends TestCase{
             //ok
         }
 
+        //check has next do not iterate
         ite = GenericStartIndexFeatureIterator.wrap(collection.iterator(), 1);
         testIterationOnNext(ite, 2);
+
+        //check sub iterator is properly closed
+        CheckCloseFeatureIterator checkIte = new CheckCloseFeatureIterator(collection.iterator());
+        assertFalse(checkIte.isClosed());
+        ite = GenericStartIndexFeatureIterator.wrap(checkIte, 1);
+        while(ite.hasNext()) ite.next();
+        ite.close();
+        assertTrue(checkIte.isClosed());
     }
 
 }
