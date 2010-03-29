73b201987943deb27f80ac2bca47934c45d95f5b
cedricbr <cedric.briancon@geomatys.fr>
2010-03-29 17:33:06 +0200

    add client-wcs module, with unit tests

---
+++ modules/clients/geotk-client-wcs/src/main/java/org/geotoolkit/wcs/AbstractDescribeCoverage.java
@@ -0,0 +1,71 @@
+/*
+ *    Geotoolkit - An Open Source Java GIS Toolkit
+ *    http://www.geotoolkit.org
+ *
+ *    (C) 2009, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotoolkit.wcs;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.geotoolkit.client.AbstractRequest;
+import org.geotoolkit.util.StringUtilities;
+
+
+/**
+ * Abstract describe coverage request.
+ *
+ * @author Cédric Briançon (Geomatys)
+ * @module pending
+ */
+public abstract class AbstractDescribeCoverage extends AbstractRequest implements DescribeCoverageRequest {
+
+    protected final String version;
+
+    private String[] coverage;
+
+    protected AbstractDescribeCoverage(String serverURL, String version){
+        super(serverURL);
+        this.version = version;
+    }
+
+    @Override
+    public String[] getCoverage() {
+        return coverage;
+    }
+
+    @Override
+    public void setCoverage(String... coverage) {
+        this.coverage = coverage;
+    }
+
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public URL getURL() throws MalformedURLException {
+        requestParameters.put("SERVICE",  "WCS");
+        requestParameters.put("REQUEST",  "DescribeCoverage");
+        requestParameters.put("VERSION",  version);
+        requestParameters.put("COVERAGE", StringUtilities.toCommaSeparatedValues(coverage));
+        return super.getURL();
+    }
+
+    @Override
+    public InputStream getSOAPResponse() throws IOException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ modules/clients/geotk-client-wcs/src/test/java/org/geotoolkit/wcs/GetCoverageTest.java
@@ -0,0 +1,74 @@
+/*
+ *    Geotoolkit - An Open Source Java GIS Toolkit
+ *    http://www.geotoolkit.org
+ *
+ *    (C) 2010, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotoolkit.wcs;
+
+import java.awt.Dimension;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.geotoolkit.geometry.GeneralEnvelope;
+import org.geotoolkit.referencing.CRS;
+import org.geotoolkit.wcs.v100.GetCoverage100;
+import org.junit.Test;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.NoSuchAuthorityCodeException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import static org.junit.Assert.*;
+
+
+/**
+ * Testing class for GetCoverage requests, in version 1.0.0.
+ *
+ * @author Cédric Briançon (Geomatys)
+ */
+public class GetCoverageTest {
+    public GetCoverageTest() {}
+
+    /**
+     * Ensures the {@link GetCoverage100#getURL()} method returns a well-built url,
+     * with the parameters given.
+     *
+     * @throws NoSuchAuthorityCodeException
+     * @throws FactoryException
+     */
+    @Test
+    public void testGetCoverage100() throws NoSuchAuthorityCodeException, FactoryException {
+        final CoordinateReferenceSystem crs = CRS.decode("CRS:84");
+        final GeneralEnvelope env = new GeneralEnvelope(crs);
+        env.setRange(0, -180, 180);
+        env.setRange(1, -90, 90);
+        final GetCoverage100 coverage100 = new GetCoverage100("http://test.com");
+        coverage100.setDimension(new Dimension(800, 600));
+        coverage100.setFormat("image/png");
+        coverage100.setCoverage("test");
+        coverage100.setEnvelope(env);
+        final URL url;
+        try {
+            url = coverage100.getURL();
+        } catch (MalformedURLException ex) {
+            fail(ex.getLocalizedMessage());
+            return;
+        }
+        final String sUrl = url.toString();
+        assertTrue(sUrl.startsWith("http://test.com?"));
+        assertTrue(sUrl.contains("BBOX=-180.0,-90.0,180.0,90.0"));
+        assertTrue(sUrl.contains("CRS=CRS:84"));
+        assertTrue(sUrl.contains("FORMAT=image/png"));
+        assertTrue(sUrl.contains("WIDTH=800"));
+        assertTrue(sUrl.contains("HEIGHT=600"));
+        assertTrue(sUrl.contains("COVERAGE=test"));
+    }
+}
