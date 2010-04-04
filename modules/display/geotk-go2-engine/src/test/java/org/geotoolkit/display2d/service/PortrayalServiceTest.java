4f4d350d55a7eeb13c0d78f79f7555ae60965a81
Johann Sorel <johann.sorel@geomatys.fr>
2010-04-04 11:22:24 +0200

    Renderer : add feature collection rendering tests

--- modules/display/geotk-go2-engine/src/test/java/org/geotoolkit/display2d/service/PortrayalServiceTest.java
+++ modules/display/geotk-go2-engine/src/test/java/org/geotoolkit/display2d/service/PortrayalServiceTest.java
@@ -16,16 +16,31 @@
  */
 package org.geotoolkit.display2d.service;
 
-import java.awt.Color;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
 import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+import org.geotoolkit.data.DataUtilities;
+import org.geotoolkit.data.FeatureCollection;
+import org.geotoolkit.data.FeatureWriter;
 import org.geotoolkit.display.exception.PortrayalException;
+import org.geotoolkit.feature.simple.SimpleFeatureTypeBuilder;
 import org.geotoolkit.geometry.GeneralEnvelope;
 import org.geotoolkit.map.MapBuilder;
 import org.geotoolkit.map.MapContext;
 import org.geotoolkit.referencing.CRS;
+import org.geotoolkit.referencing.crs.DefaultGeographicCRS;
+import org.geotoolkit.style.DefaultStyleFactory;
+import org.geotoolkit.style.MutableStyleFactory;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.geometry.Envelope;
 import static org.junit.Assert.*;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.NoSuchAuthorityCodeException;
@@ -37,7 +52,71 @@ import org.opengis.referencing.NoSuchAuthorityCodeException;
  */
 public class PortrayalServiceTest {
 
-    public PortrayalServiceTest() {
+    private static final GeometryFactory GF = new GeometryFactory();
+    private static final MutableStyleFactory SF = new DefaultStyleFactory();
+    private final FeatureCollection col;
+
+    private final List<Envelope> envelopes = new ArrayList<Envelope>();
+
+    public PortrayalServiceTest() throws Exception {
+
+        // create the feature collection for tests -----------------------------
+        final SimpleFeatureTypeBuilder sftb = new SimpleFeatureTypeBuilder();
+        sftb.setName("test");
+        sftb.add("geom", Point.class, DefaultGeographicCRS.WGS84);
+        sftb.add("att1", String.class);
+        sftb.add("att2", Double.class);
+        final SimpleFeatureType sft = sftb.buildFeatureType();
+        col = DataUtilities.collection("id", sft);
+
+        final FeatureWriter writer = col.getSession().getDataStore().getFeatureWriterAppend(sft.getName());
+
+        SimpleFeature sf = (SimpleFeature) writer.next();
+        sf.setAttribute("geom", GF.createPoint(new Coordinate(0, 0)));
+        sf.setAttribute("att1", "value1");
+        writer.write();
+        sf = (SimpleFeature) writer.next();
+        sf.setAttribute("geom", GF.createPoint(new Coordinate(-180, -90)));
+        sf.setAttribute("att1", "value1");
+        writer.write();
+        sf = (SimpleFeature) writer.next();
+        sf.setAttribute("geom", GF.createPoint(new Coordinate(-180, 90)));
+        sf.setAttribute("att1", "value1");
+        writer.write();
+        sf = (SimpleFeature) writer.next();
+        sf.setAttribute("geom", GF.createPoint(new Coordinate(180, -90)));
+        sf.setAttribute("att1", "value1");
+        writer.write();
+        sf = (SimpleFeature) writer.next();
+        sf.setAttribute("geom", GF.createPoint(new Coordinate(180, -90)));
+        sf.setAttribute("att1", "value1");
+        writer.write();
+
+        writer.close();
+
+
+        //create a serie of envelopes for tests --------------------------------
+        GeneralEnvelope env = new GeneralEnvelope(CRS.decode("EPSG:4326"));
+        env.setRange(0, -90, 90);
+        env.setRange(1, -180, 180);
+        envelopes.add(env);
+        env = new GeneralEnvelope(CRS.decode("EPSG:4326"));
+        env.setRange(0, -12, 31);
+        env.setRange(1, -5, 46);
+        envelopes.add(env);
+        env = new GeneralEnvelope(DefaultGeographicCRS.WGS84);
+        env.setRange(0, -180, 180);
+        env.setRange(1, -90, 90);
+        envelopes.add(env);
+        env = new GeneralEnvelope(DefaultGeographicCRS.WGS84);
+        env.setRange(0, -5, 46);
+        env.setRange(1, -12, 31);
+        envelopes.add(env);
+        env = new GeneralEnvelope(CRS.decode("EPSG:3395"));
+        env.setRange(0, -1200000, 3100000);
+        env.setRange(1, -500000, 4600000);
+        envelopes.add(env);
+
     }
 
     @BeforeClass
@@ -48,8 +127,8 @@ public class PortrayalServiceTest {
     public static void tearDownClass() throws Exception {
     }
 
-     @Test
-     public void testEnvelopeNotNull() throws NoSuchAuthorityCodeException, FactoryException, PortrayalException {
+    @Test
+    public void testEnvelopeNotNull() throws NoSuchAuthorityCodeException, FactoryException, PortrayalException {
         MapContext context = MapBuilder.createContext(CRS.decode("EPSG:4326"));
         GeneralEnvelope env = new GeneralEnvelope(CRS.decode("EPSG:4326"));
         env.setRange(0, -180, 180);
@@ -74,6 +153,29 @@ public class PortrayalServiceTest {
                 new ViewDef(env));
 
 
-     }
+    }
+
+    @Test
+    public void testFeatureRendering() throws Exception{
+        final MapContext context = MapBuilder.createContext(DefaultGeographicCRS.WGS84);
+        context.layers().add(MapBuilder.createFeatureLayer(col, SF.style(SF.pointSymbolizer())));
+
+        assertEquals(1, context.layers().size());
+
+        for(Envelope env : envelopes){
+            BufferedImage img = DefaultPortrayalService.portray(
+                new CanvasDef(new Dimension(800, 600), null),
+                new SceneDef(context),
+                new ViewDef(env));
+            assertNotNull(img);
+        }
+        
+    }
+
+//    @Test
+//    public void testCoverageRendering() throws Exception{
+//
+//    }
+
 
 }
