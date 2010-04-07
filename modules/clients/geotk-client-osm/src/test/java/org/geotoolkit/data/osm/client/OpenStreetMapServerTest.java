4bc7c95c586282a40cd706f53773c42248f09c8b
Johann Sorel <johann.sorel@geomatys.fr>
2010-04-07 12:53:32 +0200

    OSM : add more osm server tests

--- modules/clients/geotk-client-osm/src/test/java/org/geotoolkit/data/osm/client/OpenStreetMapServerTest.java
+++ modules/clients/geotk-client-osm/src/test/java/org/geotoolkit/data/osm/client/OpenStreetMapServerTest.java
@@ -17,22 +17,24 @@
 
 package org.geotoolkit.data.osm.client;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.xml.stream.XMLStreamException;
+
 import org.geotoolkit.data.osm.model.Api;
 import org.geotoolkit.data.osm.model.Node;
+import org.geotoolkit.data.osm.model.Relation;
+import org.geotoolkit.data.osm.model.Way;
 import org.geotoolkit.data.osm.xml.OSMXMLReader;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assume;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -91,7 +93,7 @@ public class OpenStreetMapServerTest {
     }
 
     @Test
-    public void testReadElement() throws XMLStreamException, IOException{
+    public void testReadNodeElement() throws XMLStreamException, IOException{
         final ReadElementRequest request = server.createReadElement();
         request.setElementType(Node.class);
         request.setId(310);
@@ -102,9 +104,104 @@ public class OpenStreetMapServerTest {
         Object candidate = reader.next();
         assertNotNull(candidate);
         assertTrue(candidate instanceof Node);
+        assertFalse(reader.hasNext());
         Node n = (Node) candidate;
         assertEquals(310, n.getId());
 
+        request.setVersion(1);
+        reader = new OSMXMLReader(request.getURL().openStream());
+        Node v1 = (Node) reader.next();
+        assertEquals(310, n.getId());
+
+        request.setVersion(3);
+        reader = new OSMXMLReader(request.getURL().openStream());
+        Node v3 = (Node) reader.next();
+        assertEquals(310, n.getId());
+
+        assertNotSame(v1, v3);
+        assertNotSame(v1.getLatitude(), v3.getLatitude());
+        assertNotSame(v1.getLongitude(), v3.getLongitude());
+    }
+
+    @Test
+    public void testReadWayElement() throws XMLStreamException, IOException{
+        final ReadElementRequest request = server.createReadElement();
+        request.setElementType(Way.class);
+        request.setId(310);
+        request.setVersion(-1);
+
+        OSMXMLReader reader = new OSMXMLReader(request.getURL().openStream());
+        assertTrue(reader.hasNext());
+        Object candidate = reader.next();
+        assertNotNull(candidate);
+        assertTrue(candidate instanceof Way);
+        assertFalse(reader.hasNext());
+        Way n = (Way) candidate;
+        assertEquals(310, n.getId());
+
+        request.setVersion(1);
+        reader = new OSMXMLReader(request.getURL().openStream());
+        Way v1 = (Way) reader.next();
+        assertEquals(310, n.getId());
+
+        request.setVersion(3);
+        reader = new OSMXMLReader(request.getURL().openStream());
+        Way v3 = (Way) reader.next();
+        assertEquals(310, n.getId());
+
+        assertNotSame(v1, v3);
+        assertNotSame(v1.toString(), v3.toString());
+    }
+
+    @Test
+    public void testReadRelationElement() throws XMLStreamException, IOException{
+        final ReadElementRequest request = server.createReadElement();
+        request.setElementType(Relation.class);
+        request.setId(410);
+        request.setVersion(-1);
+
+        OSMXMLReader reader = new OSMXMLReader(request.getURL().openStream());
+        assertTrue(reader.hasNext());
+        Object candidate = reader.next();
+        assertNotNull(candidate);
+        assertTrue(candidate instanceof Relation);
+        assertFalse(reader.hasNext());
+        Relation n = (Relation) candidate;
+        assertEquals(410, n.getId());
+
+        request.setVersion(1);
+        reader = new OSMXMLReader(request.getURL().openStream());
+        Relation v1 = (Relation) reader.next();
+        assertEquals(410, n.getId());
+
+        request.setVersion(3);
+        reader = new OSMXMLReader(request.getURL().openStream());
+        Relation v3 = (Relation) reader.next();
+        assertEquals(410, n.getId());
+
+        assertNotSame(v1, v3);
+        assertNotSame(v1.toString(), v3.toString());
+    }
+
+    @Test
+    public void testReadElementHistory() throws XMLStreamException, IOException{
+        final ReadElementHistoryRequest request = server.createHistoryElement();
+        request.setElementType(Node.class);
+        request.setId(310);
+
+        OSMXMLReader reader = new OSMXMLReader(request.getURL().openStream());
+
+        int nbVersion = 0;
+        while(reader.hasNext()){
+            Object candidate = reader.next();
+            assertNotNull(candidate);
+            assertTrue(candidate instanceof Node);
+            Node n = (Node) candidate;
+            assertEquals(310, n.getId());
+            nbVersion++;
+        }
+
+        assertTrue(nbVersion > 1);
     }
 
 }
