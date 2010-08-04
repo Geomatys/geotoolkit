e54b61ef2f11e731c64cc1324e8a49aadf3c8141
Johann Sorel <johann.sorel@geomatys.fr>
2010-08-04 11:03:24 +0200

    Util coverage : add open mosaic method from folder

--- modules/utility/geotk-utility-pending/src/main/java/org/geotoolkit/coverage/io/GridCoverageReaders.java
+++ modules/utility/geotk-utility-pending/src/main/java/org/geotoolkit/coverage/io/GridCoverageReaders.java
@@ -17,17 +17,32 @@
 package org.geotoolkit.coverage.io;
 
 import java.awt.Dimension;
+import java.awt.geom.AffineTransform;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
 import javax.imageio.ImageReader;
+import javax.imageio.metadata.IIOMetadata;
 
+import org.geotoolkit.coverage.grid.GridGeometry2D;
+import org.geotoolkit.image.io.XImageIO;
+import org.geotoolkit.image.io.metadata.MetadataHelper;
+import org.geotoolkit.image.io.metadata.SpatialMetadata;
 import org.geotoolkit.image.io.mosaic.MosaicBuilder;
 import org.geotoolkit.image.io.mosaic.MosaicImageReader;
 import org.geotoolkit.image.io.mosaic.MosaicImageWriteParam;
+import org.geotoolkit.image.io.mosaic.Tile;
 import org.geotoolkit.image.io.mosaic.TileManager;
+import org.geotoolkit.image.io.mosaic.TileManagerFactory;
 import org.geotoolkit.image.io.mosaic.TileWritingPolicy;
 import org.geotoolkit.lang.Static;
+
+import org.opengis.coverage.grid.GridGeometry;
+import org.opengis.coverage.grid.RectifiedGrid;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.MathTransform;
 
 /**
  * Utility class to aquiere a coverage reader from a file.
@@ -37,7 +52,7 @@ import org.geotoolkit.lang.Static;
  */
 @Static
 public class GridCoverageReaders {
-    
+
     private static final File TILE_CACHE_FOLDER = new File(System.getProperty("java.io.tmpdir") + File.separator + "imageTiles");
 
     private GridCoverageReaders(){}
@@ -51,7 +66,7 @@ public class GridCoverageReaders {
         ic.setInput(input);
         return ic;
     }
-    
+
     /**
      * Create a mosaic reader which will create a cache of tiles at different
      * resolutions. Tiles creation time depends on the available memory, the image
@@ -79,7 +94,7 @@ public class GridCoverageReaders {
     /**
      * Create a mosaic reader which will create a cache of tiles at different
      * resolutions.
-     * 
+     *
      * @param tileSize : favorite tile size, this should go over 2000, recommmanded 512 or 256.
      * @param tileFolder : cache directory where tiles will be stored
      */
@@ -103,7 +118,99 @@ public class GridCoverageReaders {
         ic.setInput(reader);
         return ic;
     }
-    
+
+    /**
+     * Open an already existing mosaic.
+     *
+     * @param folder : File folder where all tiles are stored
+     * @return GridCoverageReader
+     * @throws IOException
+     * @throws CoverageStoreException
+     */
+    public static GridCoverageReader openMosaic(File folder) throws IOException, CoverageStoreException{
+
+        //our folder is a collection of tiles for the same layer
+        final Collection<Tile> tiles = new ArrayList<Tile>();
+        final CoordinateReferenceSystem crs = visit(folder, tiles);
+
+        final TileManager[] managers = TileManagerFactory.DEFAULT.create(tiles);
+        final TileManager manager = managers[0];
+
+        final ImageCoverageReader reader = new ImageCoverageReader() {
+            @Override
+            public GridGeometry2D getGridGeometry(int index) throws CoverageStoreException {
+                //override the CRS
+                final GridGeometry gridGeom;
+                try {
+                    gridGeom = manager.getGridGeometry();
+                } catch (IOException ex) {
+                    throw new CoverageStoreException(ex);
+                }
+                return new GridGeometry2D(gridGeom.getGridRange(), gridGeom.getGridToCRS(), crs);
+            }
+
+        };
+
+        reader.setInput(manager);
+        return reader;
+    }
+
+    /**
+     * Visit all files and directories contained in the directory specified.
+     *
+     * @param file The starting file or folder.
+     */
+    private static CoordinateReferenceSystem visit(final File file, Collection<Tile> tiles) {
+
+        CoordinateReferenceSystem crs = null;
+
+        if (file.isDirectory()) {
+            final File[] list = file.listFiles();
+            if (list != null) {
+                for (int i = 0; i < list.length; i++) {
+                    CoordinateReferenceSystem ccrs = visit(list[i],tiles);
+                    if(ccrs != null){
+                        crs = ccrs;
+                    }
+                }
+            }
+        } else {
+            CoordinateReferenceSystem ccrs = test(file,tiles);
+            if(ccrs != null){
+                crs = ccrs;
+            }
+        }
+
+        return crs;
+    }
+
+    /**
+     * @param candidate to be an coverage tile.
+     */
+    private static CoordinateReferenceSystem test(final File candidate, Collection<Tile> tiles){
+        if (candidate.isFile()){
+            try {
+                final ImageReader reader = XImageIO.getReader(candidate, Boolean.TRUE, Boolean.FALSE);
+                final IIOMetadata metadata = reader.getImageMetadata(0);
+
+                if(metadata instanceof SpatialMetadata){
+                    final SpatialMetadata meta = (SpatialMetadata) metadata;
+                    final CoordinateReferenceSystem crs = meta.getInstanceForType(CoordinateReferenceSystem.class);
+                    final RectifiedGrid grid = meta.getInstanceForType(RectifiedGrid.class);
+                    final MathTransform trs = MetadataHelper.INSTANCE.getGridToCRS(grid);
+
+                    final Tile tile = new Tile(reader.getOriginatingProvider(), candidate, 0,null,(AffineTransform)trs);
+                    tiles.add(tile);
+                    return crs;
+                }
+
+            } catch (IOException ex) {
+                //don't log, this can happen very often when testing non image files
+            }
+        }
+        return null;
+    }
+
     /**
      * Create a Mosaic reader.
      */
@@ -121,7 +228,7 @@ public class GridCoverageReaders {
         reader.setInput(manager);
         return reader;
     }
-    
+
     /**
      * Get or create a temp folder to store the mosaic. the folder is based on the
      * file name, so tiles can be find again if the file name hasn't change.
@@ -149,7 +256,7 @@ public class GridCoverageReaders {
         if(lastSlash >= 0){
             pathName = pathName.substring(lastSlash+1, pathName.length());
         }
-        
+
         final int stop = pathName.lastIndexOf('.');
 
         final String name;
@@ -169,5 +276,5 @@ public class GridCoverageReaders {
 
         return cacheFolder;
     }
-    
+
 }
