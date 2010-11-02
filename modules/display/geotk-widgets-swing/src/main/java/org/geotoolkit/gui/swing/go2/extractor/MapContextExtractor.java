25bd2a205ec9f3b1349d168865876bcfe3c24c25
Johann Sorel <johann.sorel@geomatys.fr>
2010-11-02 10:41:22 +0100

    Swing : Update information widget, works on multi-dimentional coverage

--- modules/display/geotk-widgets-swing/src/main/java/org/geotoolkit/gui/swing/go2/extractor/MapContextExtractor.java
+++ modules/display/geotk-widgets-swing/src/main/java/org/geotoolkit/gui/swing/go2/extractor/MapContextExtractor.java
@@ -20,37 +20,19 @@ package org.geotoolkit.gui.swing.go2.extractor;
 import com.vividsolutions.jts.geom.Geometry;
 import java.awt.Component;
 import java.awt.Shape;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.NoninvertibleTransformException;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.List;
 import javax.measure.unit.Unit;
 
-import org.geotoolkit.coverage.GridSampleDimension;
-import org.geotoolkit.coverage.grid.GridCoverage2D;
-import org.geotoolkit.coverage.grid.ViewType;
 import org.geotoolkit.display2d.primitive.ProjectedCoverage;
 import org.geotoolkit.display2d.primitive.ProjectedFeature;
-import org.geotoolkit.coverage.io.CoverageStoreException;
-import org.geotoolkit.coverage.io.GridCoverageReadParam;
-import org.geotoolkit.coverage.io.GridCoverageReader;
-import org.geotoolkit.display.canvas.ReferencedCanvas2D;
 import org.geotoolkit.display2d.canvas.AbstractGraphicVisitor;
 import org.geotoolkit.display2d.primitive.GraphicJ2D;
-import org.geotoolkit.geometry.GeneralDirectPosition;
-import org.geotoolkit.geometry.GeneralEnvelope;
 import org.geotoolkit.map.CoverageMapLayer;
-import org.geotoolkit.referencing.CRS;
 
 import org.opengis.display.primitive.Graphic;
 import org.opengis.feature.Feature;
 import org.opengis.feature.Property;
-import org.opengis.util.FactoryException;
-import org.opengis.referencing.crs.CoordinateReferenceSystem;
-import org.opengis.referencing.operation.MathTransform;
-import org.opengis.referencing.operation.TransformException;
 
 /**
  * @author Johann Sorel (Geomatys)
@@ -123,93 +105,21 @@ public class MapContextExtractor extends AbstractGraphicVisitor {
 
     @Override
     public void visit(ProjectedCoverage projectedCoverage, Shape queryArea) {
-        final StringBuilder builder  = new StringBuilder();
-        final CoverageMapLayer layer = projectedCoverage.getCoverageLayer();
+        final Object[][] results = getCoverageValues(projectedCoverage, queryArea);
 
-        //find center of the selected area
-        final Rectangle2D bounds2D   = queryArea.getBounds2D();
-        final double centerX         = bounds2D.getCenterX();
-        final double centerY         = bounds2D.getCenterY();
-
-        //find grid coverage
-        final ReferencedCanvas2D canvas = projectedCoverage.getCanvas();
-        final GridCoverage2D coverage;
-
-        final AffineTransform dispToObj;
-
-        try{
-            dispToObj = canvas.getController().getTransform().createInverse();
-        }catch(NoninvertibleTransformException ex){
-            ex.printStackTrace();
-            return;
-        }
-
-        GridCoverageReader reader = layer.getCoverageReader();
-        final Rectangle2D displayRect = canvas.getDisplayBounds().getBounds2D();
-        final Rectangle2D objectiveRect;
-        final double[] resolution = new double[2];
-
-        try{
-            objectiveRect = canvas.getObjectiveBounds().getBounds2D();
-        }catch(TransformException ex){
-            ex.printStackTrace();
-            return;
-        }
-
-        resolution[0] = objectiveRect.getWidth()/displayRect.getWidth();
-        resolution[1] = objectiveRect.getHeight()/displayRect.getHeight();
-
-        GeneralEnvelope env = new GeneralEnvelope(objectiveRect);
-        env.setCoordinateReferenceSystem(canvas.getObjectiveCRS());
-
-        GridCoverageReadParam param = new GridCoverageReadParam();
-        param.setEnvelope(env);
-        param.setResolution(resolution);
-
-        try{
-            coverage = (GridCoverage2D) reader.read(0,param);
-        }catch(CoverageStoreException ex){
-            ex.printStackTrace();
-            return;
-        }
-
-
-
-        try {
-            final GridCoverage2D geocoverage = coverage.view(ViewType.GEOPHYSICS);
-//                org.geotoolkit.gui.swing.image.OperationTreeBrowser.show(geocoverage.getRenderedImage());
-
-            final CoordinateReferenceSystem dataCRS = geocoverage.getCoordinateReferenceSystem();
-            final MathTransform objToData           = CRS.findMathTransform(canvas.getObjectiveCRS(), dataCRS,true);
-
-            final Point2D p2d = new Point2D.Double(centerX, centerY);
-
-            //transform to objective CRS
-            dispToObj.transform(p2d, p2d);
-
-            final GeneralDirectPosition dp = new GeneralDirectPosition(p2d);
-            dp.setCoordinateReferenceSystem(canvas.getObjectiveCRS());
-
-            //transform to coverage CRS
-            objToData.transform(dp, dp);
-
-            float[] values = new float[geocoverage.getNumSampleDimensions()];
-            p2d.setLocation(dp.getOrdinate(0), dp.getOrdinate(1));
-            values = geocoverage.evaluate(p2d,values);
-
-            for(int i=0; i<values.length; i++){
-                final float value = values[i];
-                final GridSampleDimension sample = geocoverage.getSampleDimension(i);
-                final Unit unit = sample.getUnits();
-                builder.append("<b>").append(i).append(" = </b> ").append(value).append((unit == null) ? "" : unit.toString()).append("<br>");
+        final StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < results.length; i++) {
+            final Object value = results[i][0];
+            final Unit unit = (Unit) results[i][1];
+            if (value == null) {
+                continue;
             }
-
-        } catch (FactoryException ex) {
-            ex.printStackTrace();
-        } catch (TransformException ex) {
-            ex.printStackTrace();
+            builder.append(value);
+            if (unit != null) {
+                builder.append(" ").append(unit.toString());
+            }
         }
-
+        
         descriptions.add(builder.toString());
     }
 
