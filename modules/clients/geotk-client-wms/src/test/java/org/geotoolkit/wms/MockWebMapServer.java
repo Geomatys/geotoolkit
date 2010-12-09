58348f877d6035e682e3bf35a47446a7c56807d6
Johann Sorel <johann.sorel@geomatys.fr>
2010-12-09 10:00:59 +0100

    WMS : add tests

---
+++ modules/clients/geotk-client-wms/src/test/java/org/geotoolkit/wms/MockWebMapServer.java
@@ -0,0 +1,63 @@
+/*
+ *    Geotoolkit - An Open Source Java GIS Toolkit
+ *    http://www.geotoolkit.org
+ *
+ *    (C) 2010, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+
+package org.geotoolkit.wms;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import org.geotoolkit.wms.xml.AbstractWMSCapabilities;
+import org.geotoolkit.wms.xml.WMSMarshallerPool;
+import org.geotoolkit.wms.xml.WMSVersion;
+import org.geotoolkit.xml.MarshallerPool;
+
+/**
+ * For testing purpose.
+ *
+ * @author Johann Sorel (Geomatys)
+ * @module pending
+ */
+public class MockWebMapServer extends WebMapServer{
+
+    private final AbstractWMSCapabilities capa111;
+    private final AbstractWMSCapabilities capa130;
+
+    public MockWebMapServer(WMSVersion version) throws MalformedURLException, JAXBException{
+        super(new URL("http://localhost/mock/wms?"), version);
+
+        final MarshallerPool pool = WMSMarshallerPool.getInstance();
+        final Unmarshaller unmarshaller = pool.acquireUnmarshaller();
+        try{
+            capa111 = (AbstractWMSCapabilities) unmarshaller.unmarshal(MockWebMapServer.class.getResource("/org/geotoolkit/wms/wms111.xml"));
+            capa130 = (AbstractWMSCapabilities) unmarshaller.unmarshal(MockWebMapServer.class.getResource("/org/geotoolkit/wms/wms130.xml"));
+        }finally{
+            pool.release(unmarshaller);
+        }
+        
+    }
+
+    @Override
+    public AbstractWMSCapabilities getCapabilities() {
+        if(getVersion() == WMSVersion.v111){
+            return capa111;
+        }else{
+            return capa130;
+        }
+    }
+
+}
--- modules/clients/geotk-client-wms/src/test/java/org/geotoolkit/wms/WMSMapLayerTest.java
+++ modules/clients/geotk-client-wms/src/test/java/org/geotoolkit/wms/WMSMapLayerTest.java
@@ -19,16 +19,20 @@ package org.geotoolkit.wms;
 
 import java.awt.Dimension;
 import java.net.MalformedURLException;
-import java.net.URL;
+import javax.xml.bind.JAXBException;
+
 import org.geotoolkit.geometry.GeneralEnvelope;
 import org.geotoolkit.referencing.CRS;
 import org.geotoolkit.referencing.crs.DefaultGeographicCRS;
 import org.geotoolkit.wms.map.WMSMapLayer;
 import org.geotoolkit.wms.xml.WMSVersion;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
+
 import org.opengis.referencing.operation.TransformException;
 import org.opengis.util.FactoryException;
+
+import static org.junit.Assert.*;
 
 /**
  *
@@ -37,40 +41,30 @@ import org.opengis.util.FactoryException;
  */
 public class WMSMapLayerTest {
 
+    private final WebMapServer SERVER_111;
+    private final WebMapServer SERVER_130;
+
+    public WMSMapLayerTest() throws MalformedURLException, JAXBException {
+        SERVER_111 = new MockWebMapServer(WMSVersion.v111);
+        SERVER_130 = new MockWebMapServer(WMSVersion.v130);
+    }
+
     /**
      * This test checks that in the case we use CRS:84 in WMS 1.1.1
      * the URL CRS will be changed by EPSG:4326 but with no coordinate change.
      * since EPSG:4326 in 1.1.1 is actually a CRS:84 .
      */
     @Test
-    public void testCRS84WithWMS110() throws MalformedURLException, TransformException, FactoryException {
-
-        final WebMapServer server = new WebMapServer(new URL("http://localhost/constellation/WS/wms?"), WMSVersion.v111);
-        final WMSMapLayer layer = new WMSMapLayer(server, "BlueMarble");
-
+    public void test_v110_GetMap_CRS84() throws TransformException, MalformedURLException, FactoryException{
+        final WMSMapLayer layer = new WMSMapLayer(SERVER_111, "BlueMarble");
 
         final GeneralEnvelope env = new GeneralEnvelope(DefaultGeographicCRS.WGS84);
         env.setRange(0, -180, 180);
         env.setRange(1, -90, 90);
 
-        String query = layer.query(env, new Dimension(800, 600)).toString();
-
+        final String query = layer.query(env, new Dimension(800, 600)).toString();
         assertTrue( query.substring(query.indexOf("SRS")).startsWith("SRS=EPSG:4326"));
         assertTrue( query.substring(query.indexOf("BBOX")).startsWith("BBOX=-180.0,-90.0,180.0,90.0"));
-        //http://localhost/constellation/WS/wms
-        //?TRANSPARENT=TRUE
-        //&BBOX=-180.0,-90.0,180.0,90.0
-        //&VERSION=1.1.1
-        //&FORMAT=image/png
-        //&SERVICE=WMS
-        //&HEIGHT=600
-        //&LAYERS=BlueMarble
-        //&REQUEST=GetMap
-        //&STYLES=
-        //&SRS=EPSG:4326
-        //&WIDTH=800
-
-
     }
 
     /**
@@ -78,34 +72,56 @@ public class WMSMapLayerTest {
      * the URL BBOX Coordinate must be inverted .
      */
     @Test
-    public void testEPSG4326WithWMS110() throws MalformedURLException, TransformException, FactoryException {
+    public void test_v110_GetMap_EPSG4326() throws MalformedURLException, TransformException, FactoryException {
 
-        final WebMapServer server = new WebMapServer(new URL("http://localhost/constellation/WS/wms?"), WMSVersion.v111);
-        final WMSMapLayer layer = new WMSMapLayer(server, "BlueMarble");
+        final WMSMapLayer layer = new WMSMapLayer(SERVER_111, "BlueMarble");
 
 
         final GeneralEnvelope env = new GeneralEnvelope(CRS.decode("EPSG:4326"));
         env.setRange(0, -90, 90);
         env.setRange(1, -180, 180);
 
-        String query = layer.query(env, new Dimension(800, 600)).toString();
-        System.out.println(query);
-
+        final String query = layer.query(env, new Dimension(800, 600)).toString();
         assertTrue( query.substring(query.indexOf("SRS")).startsWith("SRS=EPSG:4326"));
         assertTrue( query.substring(query.indexOf("BBOX")).startsWith("BBOX=-180.0,-90.0,180.0,90.0"));
-        //http://localhost/constellation/WS/wms
-        //?TRANSPARENT=TRUE
-        //&BBOX=-180.0,-90.0,180.0,90.0
-        //&VERSION=1.1.1
-        //&FORMAT=image/png
-        //&SERVICE=WMS
-        //&HEIGHT=600
-        //&LAYERS=BlueMarble
-        //&REQUEST=GetMap
-        //&STYLES=
-        //&SRS=EPSG:4326
-        //&WIDTH=800
+    }
+    
+    /**
+     * This test checks that in the case we use CRS:84 in WMS 1.3.0
+     * the URL BBOX Coordinate are not changed.
+     */
+    @Test
+    public void test_v130_GetMap_CRS84() throws MalformedURLException, TransformException, FactoryException {
 
+        final WMSMapLayer layer = new WMSMapLayer(SERVER_130, "BlueMarble");
+
+
+        final GeneralEnvelope env = new GeneralEnvelope(CRS.decode("CRS:84"));
+        env.setRange(0, -180, 180);
+        env.setRange(1, -90, 90);
+
+        final String query = layer.query(env, new Dimension(800, 600)).toString();
+        assertTrue( query.substring(query.indexOf("CRS")).startsWith("CRS=CRS:84"));
+        assertTrue( query.substring(query.indexOf("BBOX")).startsWith("BBOX=-180.0,-90.0,180.0,90.0"));
+    }
+    
+    /**
+     * This test checks that in the case we use EPSG:4326 in WMS 1.3.0
+     * the URL BBOX Coordinate are not changed.
+     */
+    @Test
+    public void test_v130_GetMap_EPSG4326() throws MalformedURLException, TransformException, FactoryException {
+
+        final WMSMapLayer layer = new WMSMapLayer(SERVER_130, "BlueMarble");
+
+
+        final GeneralEnvelope env = new GeneralEnvelope(CRS.decode("EPSG:4326"));
+        env.setRange(0, -90, 90);
+        env.setRange(1, -180, 180);
+
+        final String query = layer.query(env, new Dimension(800, 600)).toString();
+        assertTrue( query.substring(query.indexOf("CRS")).startsWith("CRS=EPSG:4326"));
+        assertTrue( query.substring(query.indexOf("BBOX")).startsWith("BBOX=-90.0,-180.0,90.0,180.0"));
     }
     
 }
