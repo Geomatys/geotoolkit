e0fdd1db36cb4b846e882f91095706bb6ae7d4de
desruisseaux <martin.desruisseaux@geomatys.fr>
2011-01-13 20:18:06 +0100

    Updated copyright year from 2010 to 2011.

--- modules/coverage/geotk-coverage-sql/src/main/java/org/geotoolkit/coverage/sql/CoverageTableModel.java
+++ modules/coverage/geotk-coverage-sql/src/main/java/org/geotoolkit/coverage/sql/CoverageTableModel.java
@@ -2,8 +2,8 @@
  *    Geotoolkit.org - An Open Source Java GIS Toolkit
  *    http://www.geotoolkit.org
  *
- *    (C) 2005-2010, Open Source Geospatial Foundation (OSGeo)
- *    (C) 2007-2010, Geomatys
+ *    (C) 2005-2011, Open Source Geospatial Foundation (OSGeo)
+ *    (C) 2007-2011, Geomatys
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
