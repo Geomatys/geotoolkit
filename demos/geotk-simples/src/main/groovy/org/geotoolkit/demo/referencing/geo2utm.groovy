c0dd03abf5f8b06b26ca163844e59f1056b14fb0
desruisseaux <martin.desruisseaux@geomatys.fr>
2011-02-04 16:33:22 +0100

    Demos: added a Groovy example (constribution by Brian Schlining).

---
+++ demos/geotk-simples/src/main/groovy/org/geotoolkit/demo/referencing/geo2utm.groovy
@@ -0,0 +1,69 @@
+#!/usr/bin/env groovy
+/*
+ Use as: geo2utm.groovy [file]
+
+ 'file' should be a CSV file with a header row that contains 'latitude' and 'longitude'
+ columns. This script converts lat and lon to UTM zone 10 and calculates the distance
+ between the point on each consecutive line. The results are appended to the
+ CSV data and printed to the terminal.
+
+ @author Brian Schlining
+ */
+@Grapes([
+    @GrabConfig(systemClassLoader=true),
+    @Grab(group = "org.opengis",      module = "geoapi-pending",    version="2.3-M9"),
+    @Grab(group = "org.geotoolkit",   module = "geotk-utility",     version="3.16"),
+    @Grab(group = "org.geotoolkit",   module = "geotk-metadata",    version="3.16"),
+    @Grab(group = "org.geotoolkit",   module = "geotk-referencing", version="3.16"),
+    @Grab(group = "org.geotoolkit",   module = "geotk-epsg",        version="3.16"),
+    @Grab(group = "org.apache.derby", module = "derbyclient",       version="10.6.1.0"),
+    @Grab(group = "org.apache.derby", module = "derbynet",          version="10.6.1.0"),
+    @Grab(group = "ch.qos.logback",   module = 'logback-classic',   version="0.9.27")
+])
+import java.text.DecimalFormat
+import org.opengis.geometry.DirectPosition
+import org.opengis.referencing.operation.MathTransform
+import org.opengis.referencing.crs.CoordinateReferenceSystem
+import org.geotoolkit.referencing.CRS
+import org.geotoolkit.referencing.crs.DefaultGeographicCRS
+import org.geotoolkit.geometry.DirectPosition2D
+import org.geotoolkit.geometry.GeneralDirectPosition
+
+def file = new File(args[0])
+
+// Get the MathTransform once for all before to loop over the lines.
+// The 3 first lines below are costly, so better to execute them only once.
+def CoordinateReferenceSystem sourceCRS = CRS.decode("EPSG:4326")  // WGS84 (lat, lon)
+def CoordinateReferenceSystem targetCRS = CRS.decode('EPSG:32610') // UTM Zone 10N
+def MathTransform tr = CRS.findMathTransform(sourceCRS, targetCRS)
+def DirectPosition sourcePt   = new DirectPosition2D(sourceCRS)
+def DirectPosition targetPt   = new DirectPosition2D(targetCRS)
+def DirectPosition previousPt = new DirectPosition2D(targetCRS)
+
+// We will find Latitude and Longitude columns
+// in the first iteration of the loop.
+def latColumn  = 0
+def lonColumn  = 0
+def lineNumber = 0
+def df = new DecimalFormat('###0.0')
+
+file.eachLine { line ->
+    def p = line.split(",")
+    if (lineNumber == 0) {
+        latColumn = p.findIndexOf { it.toLowerCase() == 'latitude' }
+        lonColumn = p.findIndexOf { it.toLowerCase() == 'longitude' }
+        println("${line}, Easting, Northing, distanceFromPreviousLine[m]")
+    }
+    else {
+        previousPt.setLocation(targetPt)
+        sourcePt.setLocation(p[latColumn] as double, p[lonColumn] as double)
+        targetPt = tr.transform(sourcePt, targetPt)
+        def distance = Double.NaN
+        if (lineNumber > 1) {
+            distance = Math.hypot(targetPt.x - previousPt.x,
+                                  targetPt.y - previousPt.y)
+        }
+        println("${line}, ${df.format(targetPt.x)}, ${df.format(targetPt.y)}, ${df.format(distance)}")
+    }
+    lineNumber++
+}
--- demos/geotk-simples/src/main/java/org/geotoolkit/demo/referencing/CoordinateConversion.java
+++ demos/geotk-simples/src/main/java/org/geotoolkit/demo/referencing/CoordinateConversion.java
@@ -20,22 +20,30 @@ import org.geotoolkit.geometry.GeneralDirectPosition;
 
 
 /**
- * Demonstration of a few coordinate conversions.
+ * Demonstration of a few coordinate conversions. All demos in this file convert the same
+ * point (the Everest Mount) from and to different CRS.
  * <p>
  * Requirements:
  * <ul>
  *   <li>{@code geotk-utility} module</li>
  *   <li>{@code geotk-metadata} module</li>
  *   <li>{@code geotk-referencing} module</li>
- *   <li>{@code geotk-epsg} module (only for {@link #geocentricToProjected()})</li>
- *   <li>JavaDB or Derby (only for {@link #geocentricToProjected()})</li>
+ *   <li>{@code geotk-epsg} module (only for {@link #geographicToProjected()})</li>
+ *   <li>JavaDB or Derby (only for {@link #geographicToProjected()})</li>
  * </ul>
+ * <p>
+ * For convenience, all the above requirements can be replaced by a single dependency toward
+ * the {@code geotk-epsg-javadb} module, which will bring everything else through transitive
+ * dependencies.
  */
 public class CoordinateConversion {
     /**
      * Converts a coordinate from Geocentric CRS to Geographic CRS.
      * This demo uses predefined CRS constants for simplicity. The
      * coordinate is Everest Mount (27°59'17"N 86°55'31"E﻿).
+     * <p>
+     * Note that the geographic ordinates for this demo are in
+     * (<var>longitude</var>, <var>latitude</var>, <var>altitude</var>) order.
      *
      * @throws FactoryException If an error occurred while searching for a conversion.
      * @throws TransformException If an error occurred while performing the conversion.
@@ -55,11 +63,19 @@ public class CoordinateConversion {
     }
 
     /**
-     * This is the same demo than above, except that the target CRS is now a
-     * projected one instead than a geographic one. Because there is so many
-     * projected CRS available, Geotoolkit.org does not define any constant
-     * for them. We have to pick one from a database.
-     * <p>
+     * This is the same demo than above converting the same point, but the source and target CRS
+     * are different.
+     *
+     * <ul>
+     *   <li><p>The source CRS is now a geographic CRS with axis order compliant to the common usage
+     *   in geodesy, as defined in the EPSG database: (<var>latitude</var>, <var>longitude</var>).
+     *   Note that this is the opposite axis order than the output of previous demos.</p></li>
+     *
+     *   <li><p>The target CRS is now a projected one instead than a geographic one. Because there
+     *   is so many projected CRS available, Geotoolkit.org does not define any constant for them.
+     *   We have to pick one from a database.</p></li>
+     * </ul>
+     *
      * For this demo, we use EPSG:3395 which stands for "WGS 84 / World Mercator".
      * For browsing projections codes on-line, see http://www.epsg-registry.org/
      *
@@ -71,15 +87,17 @@ public class CoordinateConversion {
      * @throws FactoryException If an error occurred while searching for a conversion.
      * @throws TransformException If an error occurred while performing the conversion.
      */
-    public static void geocentricToProjected() throws FactoryException, TransformException {
-        CoordinateReferenceSystem sourceCRS = DefaultGeocentricCRS.CARTESIAN;
-        CoordinateReferenceSystem targetCRS = CRS.decode("EPSG:3395");
+    public static void geographicToProjected() throws FactoryException, TransformException {
+        CoordinateReferenceSystem sourceCRS = CRS.decode("EPSG:4326");  // WGS 84
+        CoordinateReferenceSystem targetCRS = CRS.decode("EPSG:3395");  // WGS 84 / World Mercator
         MathTransform tr = CRS.findMathTransform(sourceCRS, targetCRS);
         /*
          * From this point we can convert an arbitrary amount of coordinates using the
          * same MathTransform object. It could be in concurrent threads if we wish.
          */
-        DirectPosition sourcePt = new GeneralDirectPosition(302742.5, 5636029.0, 2979489.2);
+        DirectPosition sourcePt = new GeneralDirectPosition(
+                27 + (59 + 17.0 / 60) / 60,   // 27°59'17"N
+                86 + (55 + 31.0 / 60) / 60);  // 86°55'31"E
         DirectPosition targetPt = tr.transform(sourcePt, null);
         System.out.println("Source point: " + sourcePt);
         System.out.println("Target point: " + targetPt);
@@ -97,7 +115,9 @@ public class CoordinateConversion {
         geocentricToGeographic();
 
         System.out.println();
-        System.out.println("Geocentric to Projected CRS (World Mercator)");
-        geocentricToProjected();
+        System.out.println("Geographic to Projected CRS (World Mercator)");
+        System.out.println("Please compare the order of ordinate values in \"Source Point\" below");
+        System.out.println("with the order of ordinate values in \"Target Point\" above.");
+        geographicToProjected();
     }
 }
