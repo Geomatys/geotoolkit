44037541c2472cecc6a9f2382fc1b21986fdac85
Johann Sorel <johann.sorel@geomatys.fr>
2011-05-04 09:58:17 +0200

    Swing edition : hide geometry simplification tool for point types

--- modules/display/geotk-widgets-swing/src/main/java/org/geotoolkit/gui/swing/go2/control/edition/GeometrySimplificationTool.java
+++ modules/display/geotk-widgets-swing/src/main/java/org/geotoolkit/gui/swing/go2/control/edition/GeometrySimplificationTool.java
@@ -17,7 +17,7 @@
 package org.geotoolkit.gui.swing.go2.control.edition;
 
 import com.vividsolutions.jts.geom.Geometry;
-import java.awt.Point;
+import com.vividsolutions.jts.geom.Point;
 import org.geotoolkit.gui.swing.go2.JMap2D;
 import org.geotoolkit.gui.swing.resource.MessageBundle;
 import org.geotoolkit.map.FeatureMapLayer;
