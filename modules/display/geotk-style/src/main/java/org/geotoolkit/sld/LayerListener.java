4778e036f6eda76c8ad190afbaf35ba5f8f6d61d
Johann Sorel <johann.sorel@geomatys.fr>
2011-05-25 09:43:52 +0200

    SLD : add weak listeners classes

--- modules/display/geotk-style/src/main/java/org/geotoolkit/sld/LayerListener.java
+++ modules/display/geotk-style/src/main/java/org/geotoolkit/sld/LayerListener.java
@@ -16,7 +16,13 @@
  */
 package org.geotoolkit.sld;
 
+import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Collection;
+import org.geotoolkit.internal.ReferenceQueueConsumer;
+import org.geotoolkit.util.Disposable;
 
 import org.geotoolkit.util.collection.CollectionChangeEvent;
 import org.opengis.sld.Constraint;
@@ -38,5 +44,78 @@ public interface LayerListener extends PropertyChangeListener{
      * Called when a constraint change.
      */
     void constraintChange(CollectionChangeEvent<Constraint> event);
+    
+    /**
+     * Weak SLD listener. Use it when you are not
+     * sure that the listener will be correctly removed by your class.
+     */
+    public static final class Weak extends WeakReference<LayerListener> implements LayerListener,Disposable{
+
+        private final Collection<MutableLayer> sources = new ArrayList<MutableLayer>(1);
+
+        public Weak(final LayerListener ref) {
+            this(null,ref);
+        }
+
+        public Weak(final MutableLayer source, final LayerListener ref) {
+            super(ref, ReferenceQueueConsumer.DEFAULT.queue);
+            registerSource(source);
+        }
+
+        /**
+         * Register this listener on the given source.
+         */
+        public synchronized void registerSource(final MutableLayer source){
+            if(source != null){
+                //register in the new source
+                source.addListener(this);
+                this.sources.add(source);
+            }
+        }
+
+        /**
+         * Unregister this listener on the given source.
+         */
+        public synchronized void unregisterSource(final MutableLayer source){
+            sources.remove(source);
+            source.removeListener(this);
+        }
+
+        @Override
+        public synchronized void dispose() {
+            for(MutableLayer source : sources){
+                source.removeListener(this);
+            }
+            sources.clear();
+        }
+
+        @Override
+        public void propertyChange(final PropertyChangeEvent evt) {
+            final LayerListener listener = get();
+            if (listener != null) {
+                listener.propertyChange(evt);
+            }
+            //if the listener is null, that means we are in the reference queue and it will be disposed soon.
+        }
+
+        @Override
+        public void styleChange(final CollectionChangeEvent<MutableLayerStyle> event) {
+            final LayerListener listener = get();
+            if (listener != null) {
+                listener.styleChange(event);
+            }
+            //if the listener is null, that means we are in the reference queue and it will be disposed soon.
+        }
+
+        @Override
+        public void constraintChange(final CollectionChangeEvent<Constraint> event) {
+            final LayerListener listener = get();
+            if (listener != null) {
+                listener.constraintChange(event);
+            }
+            //if the listener is null, that means we are in the reference queue and it will be disposed soon.
+        }
+
+    }
     
 }
--- modules/display/geotk-style/src/main/java/org/geotoolkit/sld/SLDListener.java
+++ modules/display/geotk-style/src/main/java/org/geotoolkit/sld/SLDListener.java
@@ -2,7 +2,7 @@
  *    Geotoolkit - An Open Source Java GIS Toolkit
  *    http://www.geotoolkit.org
  *
- *    (C) 2008 - 2009, Geomatys
+ *    (C) 2008 - 2011, Geomatys
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
@@ -17,9 +17,15 @@
 package org.geotoolkit.sld;
 
 import java.beans.PropertyChangeEvent;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.EventListener;
 
+import org.geotoolkit.internal.ReferenceQueueConsumer;
+import org.geotoolkit.util.Disposable;
 import org.geotoolkit.util.collection.CollectionChangeEvent;
+
 import org.opengis.sld.SLDLibrary;
 
 /**
@@ -45,5 +51,78 @@ public interface SLDListener extends EventListener{
      * Called when a change occurs in the living SLDLibrary collection.
      */
     void libraryChange(CollectionChangeEvent<SLDLibrary> event);
+    
+    /**
+     * Weak SLD listener. Use it when you are not
+     * sure that the listener will be correctly removed by your class.
+     */
+    public static final class Weak extends WeakReference<SLDListener> implements SLDListener,Disposable{
+
+        private final Collection<MutableStyledLayerDescriptor> sources = new ArrayList<MutableStyledLayerDescriptor>(1);
+
+        public Weak(final SLDListener ref) {
+            this(null,ref);
+        }
+
+        public Weak(final MutableStyledLayerDescriptor source, final SLDListener ref) {
+            super(ref, ReferenceQueueConsumer.DEFAULT.queue);
+            registerSource(source);
+        }
+
+        /**
+         * Register this listener on the given source.
+         */
+        public synchronized void registerSource(final MutableStyledLayerDescriptor source){
+            if(source != null){
+                //register in the new source
+                source.addListener(this);
+                this.sources.add(source);
+            }
+        }
+
+        /**
+         * Unregister this listener on the given source.
+         */
+        public synchronized void unregisterSource(final MutableStyledLayerDescriptor source){
+            sources.remove(source);
+            source.removeListener(this);
+        }
+
+        @Override
+        public synchronized void dispose() {
+            for(MutableStyledLayerDescriptor source : sources){
+                source.removeListener(this);
+            }
+            sources.clear();
+        }
+
+        @Override
+        public void propertyChange(final PropertyChangeEvent evt) {
+            final SLDListener listener = get();
+            if (listener != null) {
+                listener.propertyChange(evt);
+            }
+            //if the listener is null, that means we are in the reference queue and it will be disposed soon.
+        }
+
+        @Override
+        public void layerChange(final CollectionChangeEvent<MutableLayer> event) {
+            final SLDListener listener = get();
+            if (listener != null) {
+                listener.layerChange(event);
+            }
+            //if the listener is null, that means we are in the reference queue and it will be disposed soon.
+        }
+
+        @Override
+        public void libraryChange(final CollectionChangeEvent<SLDLibrary> event) {
+            final SLDListener listener = get();
+            if (listener != null) {
+                listener.libraryChange(event);
+            }
+            //if the listener is null, that means we are in the reference queue and it will be disposed soon.
+        }
+
+    }
     
 }
