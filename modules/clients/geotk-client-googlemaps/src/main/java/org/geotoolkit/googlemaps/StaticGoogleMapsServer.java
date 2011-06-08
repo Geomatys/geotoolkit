073c8df08745dc6051186e66235c5c357e5d7575
Johann Sorel <johann.sorel@geomatys.fr>
2011-06-08 11:03:31 +0200

    GoogleMaps : add GetMap request

--- modules/clients/geotk-client-googlemaps/src/main/java/org/geotoolkit/googlemaps/StaticGoogleMapsServer.java
+++ modules/clients/geotk-client-googlemaps/src/main/java/org/geotoolkit/googlemaps/StaticGoogleMapsServer.java
@@ -2,7 +2,7 @@
  *    Geotoolkit - An Open Source Java GIS Toolkit
  *    http://www.geotoolkit.org
  *
- *    (C) 2010, Geomatys
+ *    (C) 2011, Geomatys
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
@@ -16,11 +16,76 @@
  */
 package org.geotoolkit.googlemaps;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.geotoolkit.client.Server;
+import org.geotoolkit.util.ArgumentChecks;
+import org.geotoolkit.util.logging.Logging;
+
 /**
  * Client for google static maps.
  * 
  * @author Johann Sorel (Geomatys)
+ * @module pending
  */
-public class StaticGoogleMapsServer {
+public class StaticGoogleMapsServer implements Server{
+
+    private static final Logger LOGGER = Logging.getLogger(StaticGoogleMapsServer.class);
+    
+    public static final URL DEFAULT_GOOGLE_STATIC_MAPS;
+    
+    static {
+        try {
+            DEFAULT_GOOGLE_STATIC_MAPS = new URL("http://maps.google.com/maps/api/staticmap");
+        } catch (MalformedURLException ex) {
+            //will not happen
+            throw new RuntimeException(ex.getLocalizedMessage(),ex);
+        }
+    }
+    
+    private final URL serverURL;
+    
+    /**
+     * Builds a google maps server with the default google server address.
+     */
+    public StaticGoogleMapsServer() {
+        this(DEFAULT_GOOGLE_STATIC_MAPS);
+    }
+    
+    /**
+     * Builds a google maps server with the given server url.
+     *
+     * @param serverURL The server base url.
+     */
+    public StaticGoogleMapsServer(final URL serverURL) {
+        ArgumentChecks.ensureNonNull("server url", serverURL);
+        this.serverURL = serverURL;
+    }
+    
+    @Override
+    public URI getURI() {
+        try {
+            return serverURL.toURI();
+        } catch (URISyntaxException ex) {
+            LOGGER.log(Level.WARNING, ex.getLocalizedMessage(), ex);
+        }
+        return null;
+    }
+
+    @Override
+    public URL getURL() {
+        return serverURL;
+    }
+    
+    /**
+     * Returns the map request object.
+     */
+    public GetMapRequest createGetMap() {
+        return new DefaultGetMap(serverURL.toString());
+    }
     
 }
