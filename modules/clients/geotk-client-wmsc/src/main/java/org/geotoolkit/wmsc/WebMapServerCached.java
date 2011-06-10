2ffbc49bd32ebe57d510d7c0dc8327927248067d
Johann Sorel <johann.sorel@geomatys.fr>
2011-06-10 16:23:48 +0200

    WMS-C : start module

---
+++ modules/clients/geotk-client-wmsc/src/main/java/org/geotoolkit/wmsc/WebMapServerCached.java
@@ -0,0 +1,63 @@
+/*
+ *    Geotoolkit - An Open Source Java GIS Toolkit
+ *    http://www.geotoolkit.org
+ *
+ *    (C) 2011, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotoolkit.wmsc;
+
+import java.net.URL;
+import org.geotoolkit.wms.GetMapRequest;
+import org.geotoolkit.wms.WebMapServer;
+import org.geotoolkit.wms.xml.WMSVersion;
+
+/**
+ *
+ * @author Johann Sorel (Geomatys)
+ * @module pending
+ */
+public class WebMapServerCached extends WebMapServer{
+    
+    /**
+     * Builds a web map server with the given server url and version.
+     *
+     * @param serverURL The server base url.
+     * @param version A string representation of the service version.
+     * @throws IllegalArgumentException if the version specified is not applyable.
+     */
+    public WebMapServerCached(final URL serverURL, final String version) {
+        super(serverURL,version);
+        init();
+    }
+    
+    /**
+     * Builds a web map server with the given server url and version.
+     *
+     * @param serverURL The server base url.
+     * @param version The service version.
+     */
+    public WebMapServerCached(final URL serverURL, final WMSVersion version) {
+        super(serverURL, version, null);
+        init();
+    }
+    
+    private void init(){
+   }
+
+    @Override
+    public GetMapRequest createGetMap() {
+        final GetMapRequest request = super.createGetMap();        
+        return request;
+    }
+    
+}
