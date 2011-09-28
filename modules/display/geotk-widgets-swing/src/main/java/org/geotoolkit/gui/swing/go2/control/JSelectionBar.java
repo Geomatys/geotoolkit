fb64b45c0ff46552c245f4aeda8e3993b06a9527
Johann Sorel <johann.sorel@geomatys.fr>
2011-09-28 17:56:05 +0200

    Swing : selection bar handle copy to clipboard

--- modules/display/geotk-widgets-swing/src/main/java/org/geotoolkit/gui/swing/go2/control/JSelectionBar.java
+++ modules/display/geotk-widgets-swing/src/main/java/org/geotoolkit/gui/swing/go2/control/JSelectionBar.java
@@ -17,23 +17,49 @@
  */
 package org.geotoolkit.gui.swing.go2.control;
 
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.AbstractAction;
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
 
+import org.geotoolkit.data.DataStoreRuntimeException;
+import org.geotoolkit.data.FeatureCollection;
+import org.geotoolkit.data.FeatureIterator;
+import org.geotoolkit.data.query.Query;
+import org.geotoolkit.data.query.QueryBuilder;
+import org.geotoolkit.data.query.QueryUtilities;
+import org.geotoolkit.display.container.AbstractContainer2D;
+import org.geotoolkit.display2d.container.ContextContainer2D;
+import org.geotoolkit.feature.DefaultName;
 import org.geotoolkit.gui.swing.go2.JMap2D;
 import org.geotoolkit.gui.swing.go2.control.selection.DefaultSelectionHandler;
 import org.geotoolkit.gui.swing.resource.IconBundle;
 import org.geotoolkit.gui.swing.resource.MessageBundle;
+import org.geotoolkit.map.FeatureMapLayer;
+import org.geotoolkit.map.MapContext;
+import org.geotoolkit.map.MapLayer;
+import org.geotoolkit.storage.DataStoreException;
+import org.geotoolkit.util.GeotkClipboard;
+import org.geotoolkit.util.logging.Logging;
+import org.opengis.filter.Filter;
 
 /**
  * 
@@ -42,6 +68,8 @@ import org.geotoolkit.gui.swing.resource.MessageBundle;
  */
 public class JSelectionBar extends AbstractMapControlBar implements ActionListener{
 
+    private static final Logger LOGGER = Logging.getLogger(JSelectionBar.class);
+    
     private static final ImageIcon ICON_SELECT = IconBundle.getIcon("16_select");
     private static final ImageIcon ICON_CONFIG = IconBundle.getIcon("16_vertical_next");
     private static final ImageIcon ICON_INTERSECT = IconBundle.getIcon("16_select_intersect");
@@ -92,6 +120,19 @@ public class JSelectionBar extends AbstractMapControlBar implements ActionListen
         menu.add(new JSeparator(SwingConstants.HORIZONTAL));
         menu.add(guiGeographic);
         menu.add(guiVisual);
+        menu.add(new JSeparator(SwingConstants.HORIZONTAL));
+        menu.add(new JMenuItem(new AbstractAction(MessageBundle.getString("copytoappclipboard")) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                copyToClipboard(false);
+            }
+        }));
+        menu.add(new JMenuItem(new AbstractAction(MessageBundle.getString("copytosysclipboard")) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                copyToClipboard(true);
+            }
+        }));
 
         guiConfig.setComponentPopupMenu(menu);
         guiConfig.addMouseListener(new MouseListener() {
@@ -151,4 +192,82 @@ public class JSelectionBar extends AbstractMapControlBar implements ActionListen
         map.setHandler(handler);
     }
 
+    
+    private void copyToClipboard(boolean systemclipboard){
+        final AbstractContainer2D container = map.getCanvas().getContainer();
+
+        if(container instanceof ContextContainer2D){
+            final ContextContainer2D cc = (ContextContainer2D) container;
+            final MapContext context = cc.getContext();
+            
+            final List<FeatureCollection> selections = new ArrayList<FeatureCollection>();
+            final StringBuilder sb = new StringBuilder();
+            for(MapLayer layer : context.layers()){
+                if(layer instanceof FeatureMapLayer){
+                    final FeatureMapLayer fml = (FeatureMapLayer) layer;
+                    final Filter selection = fml.getSelectionFilter();
+                    if(selection != null && selection != Filter.EXCLUDE){
+                        final Query sub = QueryUtilities.subQuery(fml.getQuery(), QueryBuilder.filtered(new DefaultName("select"), selection));
+                        FeatureIterator ite = null;
+                        try {
+                            final FeatureCollection col = fml.getCollection().subCollection(sub);
+                            selections.add(col);
+                            if(systemclipboard){
+                                ite = col.iterator();
+                                while(ite.hasNext()){
+                                    sb.append(ite.next());
+                                    sb.append("\n");
+                                }
+                            }
+                        } catch (DataStoreException ex) {
+                            LOGGER.log(Level.WARNING, ex.getMessage(),ex);
+                        } catch (DataStoreRuntimeException ex) {
+                            LOGGER.log(Level.WARNING, ex.getMessage(),ex);
+                        }finally {
+                            if(ite != null){
+                                ite.close();
+                            }
+                        }
+                    }
+                }
+            }
+            
+            if(systemclipboard){
+                GeotkClipboard.setSystemClipboardValue(sb.toString());
+            }else{
+                final Transferable trs = new FeatureCollectionListTransferable(selections);
+                GeotkClipboard.INSTANCE.setContents(trs, null);
+            }
+            
+        }
+    }
+    
+    
+    private static class FeatureCollectionListTransferable implements Transferable{
+
+        private static final String MIME = "geotk/featurecollectionList";
+        private static final DataFlavor FLAVOR = new DataFlavor(List.class,MIME);
+        private final List<FeatureCollection> selections;
+
+        public FeatureCollectionListTransferable(List<FeatureCollection> selections) {
+            this.selections = selections;
+        }
+        
+        @Override
+        public DataFlavor[] getTransferDataFlavors() {
+            return new DataFlavor[]{FLAVOR};
+        }
+
+        @Override
+        public boolean isDataFlavorSupported(DataFlavor flavor) {
+            return FLAVOR.match(flavor);
+        }
+
+        @Override
+        public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
+            return selections;
+        }
+    
+    }
+    
 }
