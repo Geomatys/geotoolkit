1b2cb43ae94e3c6413c4eda0adba02e3b9398f52
qboileau <devnull@localhost>
2011-10-06 10:03:10 +0200

    Demo Processing : rename FindProcess into FindProcessDemo

--- demos/geotk-demo-samples/src/main/java/org/geotoolkit/pending/demo/processing/FindProcessDemo.java
+++ demos/geotk-demo-samples/src/main/java/org/geotoolkit/pending/demo/processing/FindProcessDemo.java
@@ -7,7 +7,7 @@ import org.geotoolkit.process.ProcessingRegistry;
 import org.geotoolkit.process.ProcessDescriptor;
 
 
-public class FindProcess {
+public class FindProcessDemo {
     
     public static void main(String[] args) {
         
