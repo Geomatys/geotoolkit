b5a1fcf49d2248f9fa293a110d4a195804eebcba
Guilhem legal <guilhem.legal@geomatys.fr>
2011-12-08 15:00:06 +0100

    WPS binding: add ows to the jaxb context

--- modules/jaxb-xml-binding/geotk-xml-wps/src/main/java/org/geotoolkit/wps/xml/WPSMarshallerPool.java
+++ modules/jaxb-xml-binding/geotk-xml-wps/src/main/java/org/geotoolkit/wps/xml/WPSMarshallerPool.java
@@ -30,7 +30,10 @@ public class WPSMarshallerPool {
     private static MarshallerPool instance;
     static {
         try {
-            instance = new MarshallerPool("org.geotoolkit.wps.xml.v100:org.geotoolkit.gml.xml.v311:org.geotoolkit.internal.jaxb.geometry");
+            instance = new MarshallerPool("org.geotoolkit.wps.xml.v100:"
+                                        + "org.geotoolkit.gml.xml.v311:"
+                                        + "org.geotoolkit.ows.xml.v100:"
+                                        + "org.geotoolkit.internal.jaxb.geometry");
         } catch (JAXBException ex) {
             Logger.getLogger(WPSMarshallerPool.class.getName()).log(Level.SEVERE, null, ex);
         }
