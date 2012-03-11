12323cc01dd87a39f1a6a42493f08846be880472
desruisseaux <martin.desruisseaux@geomatys.fr>
2012-03-11 20:08:46 +0100

    Report: ProjectionParameters report as a sub-class of the GeoAPI reports.

--- build/tools/geotk-report/src/main/java/org/geotoolkit/project/report/ProjectionParameters.java
+++ build/tools/geotk-report/src/main/java/org/geotoolkit/project/report/ProjectionParameters.java
@@ -17,24 +17,23 @@
  */
 package org.geotoolkit.project.report;
 
+import java.util.Map;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.io.File;
-import java.io.Writer;
 import java.io.IOException;
-import java.util.Comparator;
-import java.util.Arrays;
 
 import org.opengis.metadata.citation.Citation;
-import org.opengis.parameter.GeneralParameterDescriptor;
 import org.opengis.referencing.IdentifiedObject;
 import org.opengis.referencing.operation.Projection;
 import org.opengis.referencing.operation.Conversion;
 import org.opengis.referencing.operation.Transformation;
 import org.opengis.referencing.operation.SingleOperation;
-import org.opengis.referencing.operation.OperationMethod;
-import org.opengis.referencing.operation.MathTransformFactory;
+import org.opengis.test.report.ParameterNamesReport;
 
+import org.geotoolkit.resources.Errors;
 import org.geotoolkit.factory.FactoryFinder;
-import org.geotoolkit.referencing.IdentifiedObjects;
+import org.geotoolkit.util.collection.XCollections;
 import org.geotoolkit.referencing.operation.MathTransformProvider;
 
 import static org.geotoolkit.metadata.iso.citation.Citations.*;
@@ -48,18 +47,13 @@ import static org.geotoolkit.metadata.iso.citation.Citations.*;
  *
  * @since 3.20
  */
-public final class ProjectionParameters extends ReportGenerator implements Comparator<OperationMethod> {
-    /**
-     * The authorities for which to get the names or aliases.
-     */
-    private final Citation[] authorities;
-
+public final class ProjectionParameters extends ParameterNamesReport {
     /**
      * All authority names as {@link String} instances. Those names will be used as
      * column headers in the table of coordinate operation methods. Those headers will
      * typically be "EPSG", "OGC", "ESRI", "NetCDF", "GeoTIFF" and "PROJ4".
      */
-    private final String[] authorityLabels;
+    private final Map<String,String> columnHeaders;
 
     /**
      * The type of coordinate operation methods, in the order to be shown in the HTML report.
@@ -83,15 +77,26 @@ public final class ProjectionParameters extends ReportGenerator implements Compa
      */
     @SuppressWarnings({"unchecked","rawtypes"})
     private ProjectionParameters(final Citation... authorities) {
-        this.authorities = authorities;
-        authorityLabels = new String[authorities.length];
-        for (int i=0; i<authorities.length; i++) {
-            String name = getIdentifier(authorities[i]);
+        super(null);
+        Reports.properties(properties);
+        properties.setProperty("TITLE", "Coordinate Operation parameters");
+        properties.setProperty("DESCRIPTION",
+                "All those <cite>Operation Methods</cite> and parameter names are supported "  +
+                "by the <a href=\"http://www.geotoolkit.org/modules/referencing/index.html\">" +
+                "Geotoolkit.org referencing module</a>.");
+
+        final Map<String,String> columns = new LinkedHashMap<>(XCollections.hashMapCapacity(authorities.length));
+        for (final Citation authority : authorities) {
+            final String code = getIdentifier(authority);
+            String name = code;
             if (name.equalsIgnoreCase("PROJ4")) {
                 name = "Proj.4"; // This is the actual project name.
             }
-            authorityLabels[i] = name;
+            if (columns.put(code, name) != null) {
+                throw new IllegalArgumentException(Errors.format(Errors.Keys.VALUE_ALREADY_DEFINED_$1, code));
+            }
         }
+        columnHeaders = Collections.unmodifiableMap(columns);
         categories = new Class[] {
             Projection.class,
             Conversion.class,
@@ -107,7 +112,54 @@ public final class ProjectionParameters extends ReportGenerator implements Compa
      */
     public static void main(final String[] args) throws IOException {
         final ProjectionParameters writer = new ProjectionParameters();
-        writer.write(FactoryFinder.getMathTransformFactory(null));
+        writer.add(FactoryFinder.getMathTransformFactory(null));
+        writer.write(new File("test.html"));
+    }
+
+    /**
+     * Returns the HTML text to use as a column header for each
+     * {@linkplain ReferenceIdentifier#getCodeSpace() code spaces} or
+     * {@linkplain GenericName#scope() scopes}. For each entry in the returned map, the
+     * {@linkplain java.util.Map.Entry#getKey() key} is the code spaces or scope and the
+     * {@linkplain java.util.Map.Entry#getValue() value} is the column header. The columns
+     * will be shown in iteration order.
+     *
+     * @return The name of all code spaces or scopes. Some typical values are {@code "EPSG"},
+     *         {@code "OGC"}, {@code "ESRI"}, {@code "GeoTIFF"} or {@code "NetCDF"}.
+     */
+    @Override
+    public Map<String,String> getColumnHeaders() {
+        return columnHeaders;
+    }
+
+    /**
+     * Returns a user category for the given object, or {@code null} if none. If non-null,
+     * this category will be formatted as a single row in the HTML table before all subsequent
+     * objects of the same category.
+     *
+     * <p>In order to get good results, the {@link #compare(IdentifiedObject, IdentifiedObject)}
+     * method needs to be defined in such a way that objects of the same category are grouped
+     * together.</p>
+     *
+     * @param  object The object for which to get the category.
+     * @return The category of the given object, or {@code null} if none.
+     */
+    @Override
+    public String getCategory(final IdentifiedObject object) {
+        if (object instanceof MathTransformProvider) {
+            final Class<? extends SingleOperation> c = ((MathTransformProvider) object).getOperationType();
+            if (c != null) {
+                for (final Class<?> category : categories) {
+                    if (category.isAssignableFrom(c)) {
+                        if (category == Projection.class) {
+                            return "Map projections";
+                        }
+                        return category.getSimpleName() + 's';
+                    }
+                }
+            }
+        }
+        return super.getCategory(object);
     }
 
     /**
@@ -122,7 +174,7 @@ public final class ProjectionParameters extends ReportGenerator implements Compa
      * @return -1 if {@code o1} should appears before {@code o2}, or -1 for the converse.
      */
     @Override
-    public int compare(final OperationMethod o1, final OperationMethod o2) {
+    public int compare(final IdentifiedObject o1, final IdentifiedObject o2) {
         boolean c1 = (o1 instanceof MathTransformProvider);
         boolean c2 = (o2 instanceof MathTransformProvider);
         if (c1 && c2) {
@@ -133,140 +185,9 @@ public final class ProjectionParameters extends ReportGenerator implements Compa
                 c2 = category.isAssignableFrom(op2);
                 if (c1 != c2) return c1 ? -1 : +1;
             }
-        } else {
-            if (c1 != c2) return c1 ? -1 : +1; // Sort Geotk implementations before non-Geotk ones.
+        } else if (c1 != c2) {
+            return c1 ? -1 : +1; // Sort Geotk implementations before non-Geotk ones.
         }
-        return IdentifiedObjects.getName(o1, null).compareTo(IdentifiedObjects.getName(o2, null));
-    }
-
-    /**
-     * Returns the category label for the given operation method.
-     *
-     * @param  op The operation method, ignored if {@code c} is non-null.
-     * @param  c  The operation type, or {@code null} to infer from {@code op}.
-     * @param  anchor {@code true} for returning a HTML anchor instead than the human-readable string.
-     * @return A label to write in the HTML report for the category of the operation method.
-     */
-    private String getCategory(final OperationMethod op, Class<? extends SingleOperation> c, final boolean anchor) {
-        if (c == null && op instanceof MathTransformProvider) {
-            c = ((MathTransformProvider) op).getOperationType();
-        }
-        if (c != null) {
-            for (final Class<?> category : categories) {
-                if (category.isAssignableFrom(c)) {
-                    if (anchor) {
-                        return category.getSimpleName().toLowerCase(LOCALE) + 's';
-                    }
-                    if (category == Projection.class) {
-                        return "Map projections";
-                    }
-                    return category.getSimpleName() + 's';
-                }
-            }
-        }
-        return anchor ? "others" : "Others";
-    }
-
-    /**
-     * Invoked from the {@link #main(String[])} method for generating the list of coordinate
-     * operation methods for the given factory.
-     */
-    private void write(final MathTransformFactory factory) throws IOException {
-        try (final Writer out = openHTML(new File("operation-parameters.html"), "Coordinate Operation parameters")) {
-            out.write("<p>This list is generated from Geotoolkit.org version ");
-            out.write(getGeotkVersion());
-            out.write(". All those <cite>Operation Methods</cite> and parameter names are supported " +
-                      "by the <a href=\"http://www.geotoolkit.org/modules/referencing/index.html\">" +
-                      "Geotoolkit.org referencing module</a>.</p>\n");
-            out.write("<p>Content:</p>\n<ul>\n");
-            for (final Class<? extends SingleOperation> category : categories) {
-                out.write("  <li><a href=\"#");
-                out.write(getCategory(null, category, true));
-                out.write("\">");
-                out.write(getCategory(null, category, false));
-                out.write("</a></li>\n");
-            }
-            out.write("</ul>\n");
-            openTable(out);
-            String previousCategory = null;
-            final OperationMethod[] methods = factory.getAvailableMethods(null).toArray(new OperationMethod[0]);
-            Arrays.sort(methods, this);
-            for (final OperationMethod method : methods) {
-                final String category = getCategory(method, null, false);
-                final boolean categoryChange = !category.equals(previousCategory);
-                if (previousCategory != null) {
-                    writeLine(out, categoryChange ? "<hr>" : "&nbsp;", false);
-                }
-                if (categoryChange) {
-                    previousCategory = category;
-                    writeLine(out, "<a name=\"" + getCategory(method, null, true) + "\">" + category + "</a>", true);
-                    writeTableHeader(out, authorityLabels);
-                }
-                writeRow(out, method, true);
-                for (final GeneralParameterDescriptor param : method.getParameters().descriptors()) {
-                    writeRow(out, param, false);
-                }
-            }
-            closeHTML(out);
-        }
-    }
-
-    /**
-     * Writes the given text on a single cell spanning the whole table.
-     */
-    private void writeLine(final Writer out, final String content, final boolean isHeader) throws IOException {
-        out.write("<tr>");
-        out.write(isHeader ? "<th bgcolor=\"" + TABLE_HEADER_BACKGROUND + "\" colspan=\"" : "<td colspan=\"");
-        out.write(Integer.toString(authorities.length));
-        out.write("\">");
-        out.write(content);
-        out.write(isHeader ? "</th>" : "</td>");
-        out.write("</tr>\n");
-    }
-
-    /**
-     * Writes a single row with the names of the given objects.
-     */
-    private void writeRow(final Writer out, final IdentifiedObject object, final boolean isHeader) throws IOException {
-        out.write("<tr>");
-        for (int i=0; i<authorities.length;) {
-            final Citation authority = authorities[i];
-            int colspan = 1;
-            while (++i < authorities.length) {
-                if (IdentifiedObjects.getName(object, authorities[i]) != null) {
-                    break;
-                }
-                colspan++;
-            }
-            out.write("<td nowrap");
-            if (colspan != 1) {
-                out.write(" colspan=\"");
-                out.write(Integer.toString(colspan));
-                out.write('"');
-            }
-            if (isHeader) {
-                out.write(" bgcolor=\"" + TABLE_HIGHLIGHT + "\" height=\"32\"");
-            }
-            out.write('>');
-            if (isHeader) {
-                out.write("<b>");
-            }
-            boolean hasMore = false;
-            for (final String name : IdentifiedObjects.getNames(object, authority)) {
-                if (hasMore) {
-                    out.write("<br>");
-                }
-                if (!isHeader) {
-                    out.write("&nbsp;&bull;&nbsp;");
-                }
-                out.write(name);
-                hasMore = true;
-            }
-            if (isHeader) {
-                out.write("</b>");
-            }
-            out.write("</td>");
-        }
-        out.write("</tr>\n");
+        return super.compare(o1, o2);
     }
 }
