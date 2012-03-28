c9afc422a12e9eaec44f3936ddfb479750c88393
Remi Marechal <remi.marechal@geomatys.com>
2012-03-28 17:57:01 +0200

    Hilbert : update test javadoc

--- modules/index/geotk-index-tree/src/test/java/org/geotoolkit/index/tree/hilbert/Hilbert3DTest.java
+++ modules/index/geotk-index-tree/src/test/java/org/geotoolkit/index/tree/hilbert/Hilbert3DTest.java
@@ -1,15 +1,28 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *    Geotoolkit.org - An Open Source Java GIS Toolkit
+ *    http://www.geotoolkit.org
+ *
+ *    (C) 2008-2012, Open Source Geospatial Foundation (OSGeo)
+ *    (C) 2009-2012, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  */
 package org.geotoolkit.index.tree.hilbert;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
+/**Test Hilbert curve creation in dimension 3.
  *
- * @author rmarech
+ * @author Rémi Marechal(Géomatys).
  */
 public class Hilbert3DTest {
     final int dimension = 3;
