96793cbab9576e9e8a6408d4111618085f1ad537
Johann Sorel <johann.sorel@geomatys.fr>
2012-10-24 18:00:24 +0200

    Demo : add several IGN demos

---
+++ demos/geotk-demo-samples/src/main/java/org/geotoolkit/pending/demo/clients/ignrm/IGN_INSPIRE_WMS.java
@@ -0,0 +1,55 @@
+
+package org.geotoolkit.pending.demo.clients.ignrm;
+
+import java.net.URL;
+import org.geotoolkit.coverage.CoverageReference;
+import org.geotoolkit.coverage.CoverageStore;
+import org.geotoolkit.coverage.CoverageStoreFinder;
+import org.geotoolkit.gui.swing.go2.JMap2DFrame;
+import org.geotoolkit.map.CoverageMapLayer;
+import org.geotoolkit.map.MapBuilder;
+import org.geotoolkit.map.MapContext;
+import org.geotoolkit.parameter.Parameters;
+import org.geotoolkit.security.BasicAuthenticationSecurity;
+import org.geotoolkit.security.ClientSecurity;
+import org.geotoolkit.style.DefaultDescription;
+import org.geotoolkit.util.RandomStyleFactory;
+import org.geotoolkit.util.SimpleInternationalString;
+import org.geotoolkit.wms.WMSServerFactory;
+import org.geotoolkit.wmts.WMTSServerFactory;
+import org.opengis.feature.type.Name;
+import org.opengis.parameter.ParameterValueGroup;
+
+public class IGN_INSPIRE_WMS {
+    
+    public static void main(String[] args) throws Exception {
+                
+        final String key = "yourkey";
+        final String login = "yourlogin";
+        final String password = "yourpassword";
+        
+        final ClientSecurity authentication = new BasicAuthenticationSecurity(login,password);        
+        final URL url = new URL("https://gpp3-wxs.ign.fr/"+key+"/inspire/r/wms?");
+        
+        final ParameterValueGroup params = WMSServerFactory.PARAMETERS.createValue();
+        Parameters.getOrCreate(WMTSServerFactory.URL, params).setValue(url);
+        Parameters.getOrCreate(WMTSServerFactory.SECURITY, params).setValue(authentication);
+        
+        final CoverageStore store = CoverageStoreFinder.open(params);
+                
+        final MapContext context = MapBuilder.createContext();
+        
+        for(Name n : store.getNames()){
+            final CoverageReference cr = store.getCoverageReference(n);
+            
+            final CoverageMapLayer cml = MapBuilder.createCoverageLayer(cr, RandomStyleFactory.createRasterStyle(), "");
+            cml.setDescription(new DefaultDescription(new SimpleInternationalString(n.getLocalPart()), new SimpleInternationalString("")));
+            context.layers().add(cml);
+        }
+        
+        
+        JMap2DFrame.show(context,true,null);
+        
+    }
+    
+}
--- demos/geotk-demo-samples/src/main/java/org/geotoolkit/pending/demo/clients/osmtms/OSMTMSClientDemo.java
+++ demos/geotk-demo-samples/src/main/java/org/geotoolkit/pending/demo/clients/osmtms/OSMTMSClientDemo.java
@@ -2,15 +2,24 @@
 package org.geotoolkit.pending.demo.clients.osmtms;
 
 import java.net.URL;
+import org.geotoolkit.coverage.CoverageReference;
+import org.geotoolkit.coverage.CoverageStore;
+import org.geotoolkit.coverage.CoverageStoreFinder;
 import org.geotoolkit.gui.swing.go2.JMap2DFrame;
+import org.geotoolkit.map.CoverageMapLayer;
 import org.geotoolkit.map.MapBuilder;
 import org.geotoolkit.map.MapContext;
-import org.geotoolkit.osmtms.OSMTileMapServer;
-import org.geotoolkit.osmtms.map.OSMTMSMapLayer;
+import org.geotoolkit.osmtms.OSMTMSServerFactory;
+import org.geotoolkit.parameter.Parameters;
 import org.geotoolkit.pending.demo.Demos;
 import org.geotoolkit.referencing.crs.DefaultGeographicCRS;
+import org.geotoolkit.style.DefaultDescription;
 import org.geotoolkit.style.DefaultStyleFactory;
 import org.geotoolkit.style.MutableStyleFactory;
+import org.geotoolkit.util.RandomStyleFactory;
+import org.geotoolkit.util.SimpleInternationalString;
+import org.opengis.feature.type.Name;
+import org.opengis.parameter.ParameterValueGroup;
 
 
 public class OSMTMSClientDemo {
@@ -29,35 +38,28 @@ public class OSMTMSClientDemo {
     public static MapContext createOSMTMSContext() throws Exception{
         final MapContext context = MapBuilder.createContext(DefaultGeographicCRS.WGS84);
 
-        OSMTileMapServer server = new OSMTileMapServer(
-                new URL("http://tile.openstreetmap.org"),null,18,true);
-        OSMTMSMapLayer layer = new OSMTMSMapLayer(server);
-        layer.setDescription(SF.description("Mapnik", ""));
-        context.layers().add(layer);
+        final ParameterValueGroup params = OSMTMSServerFactory.PARAMETERS.createValue();
+        Parameters.getOrCreate(OSMTMSServerFactory.URL, params).setValue(new URL("http://tile.openstreetmap.org"));
+        Parameters.getOrCreate(OSMTMSServerFactory.IMAGE_CACHE, params).setValue(true);
+        Parameters.getOrCreate(OSMTMSServerFactory.NIO_QUERIES, params).setValue(true);
+        Parameters.getOrCreate(OSMTMSServerFactory.MAX_ZOOM_LEVEL, params).setValue(18);
         
+        final CoverageStore store = CoverageStoreFinder.open(params);
+        
+        for(Name n : store.getNames()){
+            final CoverageReference cr = store.getCoverageReference(n);
+            final CoverageMapLayer cml = MapBuilder.createCoverageLayer(cr, RandomStyleFactory.createRasterStyle(), "");
+            cml.setDescription(new DefaultDescription(
+                    new SimpleInternationalString(n.getLocalPart()), 
+                    new SimpleInternationalString("")));
+            context.layers().add(cml);
+        }
         
         //Other available OSM TMS
-        
-//        server = new OSMTileMapServer(
-//                new URL("http://a.tah.openstreetmap.org/Tiles/tile/"),CRS.decode("EPSG:3857"),17);
-//        layer = new OSMTMSMapLayer(server);
-//        layer.setDescription(SF.description("Osmarender", ""));
-//        context.layers().add(layer);
-//        
-//        server = new OSMTileMapServer(
-//                new URL("http://tile.opencyclemap.org/cycle/"),CRS.decode("EPSG:3857"),18);
-//        layer = new OSMTMSMapLayer(server);
-//        layer.setDescription(SF.description("Carte cyclable", ""));
-//        context.layers().add(layer);
-//        
-//        server = new OSMTileMapServer(
-//                new URL("http://tile.cloudmade.com/fd093e52f0965d46bb1c6c6281022199/3/256/"),CRS.decode("EPSG:3857"),18);
-//        layer = new OSMTMSMapLayer(server);
-//        layer.setDescription(SF.description("SansNom", ""));
-//        context.layers().add(layer);
-        
-        
-        
+        // http://a.tah.openstreetmap.org/Tiles/tile/   17
+        // http://tile.opencyclemap.org/cycle/ 18
+        // http://tile.cloudmade.com/fd093e52f0965d46bb1c6c6281022199/3/256/ 18
+                
         return context;
     }
     
