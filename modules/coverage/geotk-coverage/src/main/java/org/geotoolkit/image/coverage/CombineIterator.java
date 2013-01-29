e61fdd62b7a6fc4d19aac6870c8ee618e75eb81b
Remi Marechal <remi.marechal@geomatys.com>
2013-01-29 09:49:21 +0100

    replace combineIterator

--- modules/coverage/geotk-coverage/src/main/java/org/geotoolkit/image/coverage/CombineIterator.java
+++ modules/coverage/geotk-coverage/src/main/java/org/geotoolkit/image/coverage/CombineIterator.java
@@ -14,7 +14,7 @@
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
  */
-package org.geotoolkit.process.coverage.copy;
+package org.geotoolkit.image.coverage;
 
 import java.util.ArrayList;
 import java.util.Date;
