e23d579bf9ca519cc516e091a956d13f57f1a814
Remi Marechal <remi.marechal@geomatys.com>
2013-02-12 14:08:58 +0100

    Coverage : update pyramidalModelReader find null pointer

--- modules/coverage/geotk-coverageio-store/src/main/java/org/geotoolkit/coverage/PyramidalModelReader.java
+++ modules/coverage/geotk-coverageio-store/src/main/java/org/geotoolkit/coverage/PyramidalModelReader.java
@@ -81,7 +81,8 @@ public class PyramidalModelReader extends GridCoverageReader{
 
     @Deprecated
     public PyramidalModelReader() {
-        this.coverageFinder = CoverageFinderFactory.createDefaultCoverageFinder();
+//        this.coverageFinder = CoverageFinderFactory.createDefaultCoverageFinder();
+        this.coverageFinder = CoverageFinderFactory.createStrictlyCoverageFinder();
     }
     
     public PyramidalModelReader(CoverageFinder coverageFinder) {
