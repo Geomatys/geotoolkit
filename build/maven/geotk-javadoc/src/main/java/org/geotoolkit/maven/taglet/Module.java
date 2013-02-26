67284ff5b5ba46fa6305d534a81e163d9880932e
desruisseaux <martin.desruisseaux@geomatys.fr>
2013-02-27 00:49:48 +0100

    Changed the version number from 3.x-SNAPSHOT to 4.x-SNAPSHOT on the JDK7 branch.

--- build/maven/geotk-javadoc/src/main/java/org/geotoolkit/maven/taglet/Module.java
+++ build/maven/geotk-javadoc/src/main/java/org/geotoolkit/maven/taglet/Module.java
@@ -37,7 +37,7 @@ public final class Module implements Taglet {
     /**
      * Geotoolkit.org version to be referenced by this taglet.
      */
-    private static final String VERSION = "3.x-SNAPSHOT";
+    private static final String VERSION = "4.x-SNAPSHOT";
 
     /**
      * Register this taglet.
