6d011bcd2a2bc957390c8c54cee2540219b32b63
desruisseaux <martin.desruisseaux@geomatys.fr>
2013-06-19 16:00:59 +0200

    Removed the Geotk Logging class.

--- modules/utility/geotk-utility/src/main/java/org/geotoolkit/util/logging/package-info.java
+++ modules/utility/geotk-utility/src/main/java/org/geotoolkit/util/logging/package-info.java
@@ -18,23 +18,13 @@
 
 /**
  * Extensions to the {@linkplain java.util.logging Java logging} framework.
- * This package provides:
- * <p>
- * <ul>
- *   <li>A {@link org.apache.sis.util.logging.Logging} utility class with static methods for
- *       fetching a logger (see <cite>Choosing a logging framework</cite> below) or logging an
- *       exception.</li>
- *   <li>A {@link org.apache.sis.util.logging.PerformanceLevel} with configurable levels for
- *       logging the duration of lengthly processes.</li>
- *   <li>A {@link org.apache.sis.util.logging.MonolineFormatter} for formatting the log
- *       records on single line with colors, for easier reading on the console output.</li>
- * </ul>
+ * See {@link org.apache.sis.util.logging} for more information.
  *
  * {@section Choosing a logging framework}
- * The Geotk project uses the standard {@link java.util.logging.Logger} API for its logging,
+ * The SIS project uses the standard {@link java.util.logging.Logger} API for its logging,
  * but this package allows redirection of logging messages to some other frameworks like
  * <a href="http://logging.apache.org/log4j/">Log4J</a>.
- * <p>
+ *
  * We recommend to stick to standard Java logging when possible. However if inter-operability
  * with an other logging framework is required, then the only action needed is to include
  * <strong>one</strong> of the following JAR on the classpath:
@@ -45,10 +35,10 @@
  * </ul>
  *
  * {@section Note for Geotk developers}
- * All Geotk code should fetch their logger through a call to our custom
+ * All Geotk code should fetch their logger through a call to the SIS
  * {@link org.apache.sis.util.logging.Logging#getLogger(String)} method, <strong>not</strong>
  * the standard {@link java.util.logging.Logger#getLogger(String)} method. This is necessary in
- * order to give Geotk a chance to redirect log events to an other logging framework.
+ * order to give Geotk/SIS a chance to redirect log events to an other logging framework.
  *
  * @author Martin Desruisseaux (IRD, Geomatys)
  * @version 3.16
