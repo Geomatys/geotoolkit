c5f321bdefb3ac0503031ced9aded8b43f3ccf29
Quentin Boileau <quentin.boileau@geomatys.com>
2013-06-21 16:27:04 +0200

    Utility : add some useful method in ParametersExt util class.

--- modules/utility/geotk-utility-pending/src/main/java/org/geotoolkit/parameter/ParametersExt.java
+++ modules/utility/geotk-utility-pending/src/main/java/org/geotoolkit/parameter/ParametersExt.java
@@ -16,15 +16,20 @@
  */
 package org.geotoolkit.parameter;
 
-import java.util.ArrayList;
-import java.util.List;
-import org.opengis.parameter.GeneralParameterValue;
-import org.opengis.parameter.ParameterValue;
-import org.opengis.parameter.ParameterValueGroup;
+import java.util.*;
+
+import org.geotoolkit.referencing.IdentifiedObjects;
+import org.geotoolkit.util.ArgumentChecks;
+import org.opengis.parameter.*;
+import org.opengis.referencing.IdentifiedObject;
+import org.opengis.util.InternationalString;
+
+import javax.measure.unit.Unit;
 
 /**
  * Utility methods for parameters.
  * @author Johann Sorel (Geomatys)
+ * @author QuentinBoileau (Geomatys)
  */
 public final class ParametersExt {
     
@@ -149,5 +154,126 @@ public final class ParametersExt {
         getParameters(group).add(param);
         return param;
     }
-    
+
+
+    /**
+     * Search recursively all GeneralParameterDescriptors which have the requested name.
+     * @param parameter Root GeneralParameterDescriptors
+     * @param name parameter name to search
+     * @param maxDepth the max depth to search for.
+     * @return
+     */
+    public static List<GeneralParameterDescriptor> search(final GeneralParameterDescriptor parameter,
+                                                          final String name, int maxDepth)
+    {
+        final List<GeneralParameterDescriptor> list = new ArrayList<GeneralParameterDescriptor>();
+        search(parameter, name, maxDepth, list);
+        return list;
+    }
+
+    /**
+     * Implementation of the search algorithm. The result is stored in the supplied set.
+     */
+    private static void search(final GeneralParameterDescriptor parameter, final String name,
+                               final int maxDepth, final Collection<GeneralParameterDescriptor> list)
+    {
+        if (maxDepth >= 0) {
+            if (IdentifiedObjects.nameMatches(parameter, name)) {
+                list.add(parameter);
+            }
+            if ((maxDepth != 0) && (parameter instanceof ParameterDescriptorGroup)) {
+                for (final GeneralParameterDescriptor value : ((ParameterDescriptorGroup) parameter).descriptors()) {
+                    search(value, name, maxDepth-1, list);
+                }
+            }
+        }
+    }
+
+    /**
+     * Add a GeneralParameterDescriptor to a ParameterDescriptorGroup.
+     *
+     * @param root
+     * @param newParam GeneralParameterDescriptor to add
+     * @return the new ParameterDescriptorGroup
+     */
+    public static ParameterDescriptorGroup addParameterToDescriptorGroup(final ParameterDescriptorGroup root, final GeneralParameterDescriptor newParam) {
+
+        ArgumentChecks.ensureNonNull("root", root);
+        ArgumentChecks.ensureNonNull("newParam", newParam);
+
+        final List<GeneralParameterDescriptor> parameters = new ArrayList<GeneralParameterDescriptor> (root.descriptors());
+        parameters.add(newParam);
+
+        int minOccurs = root.getMinimumOccurs();
+        int maxOccurs = root.getMaximumOccurs();
+
+        return createParameterDescriptorGroup(root.getName().getCode(), root.getRemarks(), minOccurs, maxOccurs, parameters);
+    }
+
+    /**
+     * Remove a GeneralParameterDescriptor from a ParameterDescriptorGroup.
+     *
+     * @param root
+     * @param toRemove GeneralParameterDescriptor to remove
+     * @return the new ParameterDescriptorGroup
+     */
+    public static ParameterDescriptorGroup removeParameterToDescriptorGroup(final ParameterDescriptorGroup root, final GeneralParameterDescriptor toRemove) {
+
+        ArgumentChecks.ensureNonNull("root", root);
+        ArgumentChecks.ensureNonNull("toRemove", toRemove);
+
+        final List<GeneralParameterDescriptor> parameters = new ArrayList<GeneralParameterDescriptor> (root.descriptors());
+        parameters.remove(toRemove);
+        int minOccurs = root.getMinimumOccurs();
+        int maxOccurs = root.getMaximumOccurs();
+
+        return createParameterDescriptorGroup(root.getName().getCode(), root.getRemarks(), minOccurs, maxOccurs, parameters);
+    }
+
+
+    /**
+     * Regroup all parameters used to create a ParameterDescriptor.
+     *
+     * @param code parameter code String
+     * @param remarks parameter description
+     * @param valueClass parametrer type
+     * @param validValues array of valids values
+     * @param defaultValue default value Object
+     * @param minValue min value Object
+     * @param maxValue max value Object
+     * @param unit parameter unit
+     * @param required mandatory or not
+     * @return a ParameterDescriptor
+     */
+    public static ParameterDescriptor createParameterDescriptor(final String code, final InternationalString remarks,
+                                                                final Class valueClass, final Object[] validValues, final Object defaultValue, final Comparable minValue,
+                                                                final Comparable maxValue, final Unit unit, final boolean required) {
+
+        final Map<String, Object> paramMap = new HashMap<String, Object>();
+        paramMap.put(IdentifiedObject.NAME_KEY, code);
+        paramMap.put(IdentifiedObject.REMARKS_KEY, remarks);
+
+        return new DefaultParameterDescriptor(paramMap, valueClass, validValues, defaultValue, minValue, maxValue, unit, required);
+    }
+
+
+    /**
+     * Regroup all parameters used to create a ParameterDescriptorGroup.
+     *
+     * @param code parameter code String
+     * @param remarks parameter description
+     * @param min min occurences
+     * @param max max occurences
+     * @param parameters list of children parameters.
+     * @return a ParameterDescriptorGroup
+     */
+    public static ParameterDescriptorGroup createParameterDescriptorGroup(final String code, final InternationalString remarks,
+                                                                          final int min, final int max, final List<GeneralParameterDescriptor> parameters) {
+
+        final Map<String, Object> paramMap = new HashMap<String, Object>();
+        paramMap.put(IdentifiedObject.NAME_KEY, code);
+        paramMap.put(IdentifiedObject.REMARKS_KEY, remarks);
+
+        return new DefaultParameterDescriptorGroup(paramMap, min, max, parameters.toArray(new GeneralParameterDescriptor[parameters.size()]));
+    }
 }
