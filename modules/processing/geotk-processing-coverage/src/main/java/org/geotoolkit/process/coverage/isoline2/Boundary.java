f38884f77b7f924d878280adb35b53badcd903e0
Johann Sorel <johann.sorel@geomatys.fr>
2013-07-02 11:30:56 +0200

    Process coverage : isoligne, add in construction structure

--- modules/processing/geotk-processing-coverage/src/main/java/org/geotoolkit/process/coverage/isoline2/Boundary.java
+++ modules/processing/geotk-processing-coverage/src/main/java/org/geotoolkit/process/coverage/isoline2/Boundary.java
@@ -1,19 +1,47 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *    Geotoolkit.org - An Open Source Java GIS Toolkit
+ *    http://www.geotoolkit.org
+ *
+ *    (C) 2013, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  */
 package org.geotoolkit.process.coverage.isoline2;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  *
- * @author jsorel
+ * @author Johann Sorel (Geomatys)
  */
 public class Boundary {
     
-    public List<Coordinate> coordinates = new ArrayList<Coordinate>();
+    /**
+     * Geometries in construction
+     */
+    public LinkedList<Coordinate> HLeft, HMiddle, HRight;
+    public LinkedList<Coordinate> VTop, VMiddle, VBottom;
+    
+    public List<LinkedList<Coordinate>> getConstructions(){
+        final List<LinkedList<Coordinate>> contructions = new ArrayList<LinkedList<Coordinate>>();
+        if(HLeft!=null) contructions.add(HLeft);
+        if(HMiddle!=null) contructions.add(HMiddle);
+        if(HRight!=null) contructions.add(HRight);
+        if(VTop!=null) contructions.add(VTop);
+        if(VMiddle!=null) contructions.add(VMiddle);
+        if(VBottom!=null) contructions.add(VBottom);
+        return contructions;
+    }
     
 }
--- modules/processing/geotk-processing-coverage/src/main/java/org/geotoolkit/process/coverage/isoline2/Isoline2.java
+++ modules/processing/geotk-processing-coverage/src/main/java/org/geotoolkit/process/coverage/isoline2/Isoline2.java
@@ -21,6 +21,9 @@ import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
 import java.awt.image.RenderedImage;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Logger;
 import org.apache.sis.storage.DataStoreException;
 
@@ -67,6 +70,11 @@ public class Isoline2 extends AbstractProcess {
     private FeatureType type;
     private FeatureCollection col;
     
+    //previous triangles informations
+    private Boundary[][] line0TopNeighbor; // [level][X] for previous line
+    private Boundary[][] line1TopNeighbor; // [level][X] for current line
+    private Boundary[] leftNeighbor; // [Level] for current line
+    
     //current pixel box
     private final Coordinate UL = new Coordinate();
     private final Coordinate UR = new Coordinate();
@@ -102,6 +110,9 @@ public class Isoline2 extends AbstractProcess {
             //line buffer
             final int height = image.getHeight();
             final int width = image.getWidth();
+            leftNeighbor = new Boundary[intervals.length];
+            line0TopNeighbor = new Boundary[intervals.length][width];
+            line1TopNeighbor = new Boundary[intervals.length][width];
             double[] line0 = new double[image.getWidth()];
             double[] line1 = new double[image.getWidth()];
             
@@ -112,21 +123,53 @@ public class Isoline2 extends AbstractProcess {
                     
                     //calculate lines
                     if(y>0 && x>0){
+                        //set the 4 corner values
                         UL.x = x-1;  UL.y = y-1;  UL.z = line0[x-1];
                         UR.x = x  ;  UR.y = y-1;  UR.z = line0[x  ];
                         BL.x = x-1;  BL.y = y  ;  BL.z = line1[x-1];
                         BR.x = x  ;  BR.y = y  ;  BR.z = line1[x  ];
                         
-                        for(double level : intervals){
-                            buildTriangles(level);
+                        for(int k=0;k<intervals.length;k++){
+                            final double level = intervals[k];
+                            final Boundary nb = buildTriangles(level,line0TopNeighbor[k][x], leftNeighbor[k]);
+                            //the created boundary is the left boundary of next pixel
+                            //and the top boundary of underneath pixel
+                            leftNeighbor[k] = nb;
+                            line1TopNeighbor[k][x] = nb;
                         }
                     }
                 }
                 
+                if(y>0){
+                    //filter the constructions which are not used
+                    final List<LinkedList<Coordinate>> oldinconstructions = new ArrayList<LinkedList<Coordinate>>();
+                    final List<LinkedList<Coordinate>> newinconstructions = new ArrayList<LinkedList<Coordinate>>();
+                    for(int x=1;x<width;x++){
+                        for(int k=0;k<intervals.length;k++){
+                            if(line0TopNeighbor[k][x] != null){
+                                oldinconstructions.addAll(line0TopNeighbor[k][x].getConstructions());
+                            }
+                            newinconstructions.addAll(line1TopNeighbor[k][x].getConstructions());
+                        }
+                    }
+                    oldinconstructions.removeAll(newinconstructions);
+
+                    //push in the feature collection geometries which are not used anymore
+                    for(final LinkedList<Coordinate> lst : oldinconstructions){
+                        final Coordinate[] coords = lst.toArray(new Coordinate[lst.size()]);
+                        pushGeometry(GF.createLineString(coords), coords[0].z);
+                    }
+                }
+                
                 //swap lines
-                final double[] temp = line0;
+                final double[] templine = line0;
                 line0 = line1;
-                line1 = temp;
+                line1 = templine;
+                final Boundary[][] tempbound = line0TopNeighbor;
+                line0TopNeighbor = line1TopNeighbor;
+                line1TopNeighbor = tempbound;
+                leftNeighbor[0] = null;
+                
             }
             
         }catch(Exception ex){
@@ -136,7 +179,11 @@ public class Isoline2 extends AbstractProcess {
         outputParameters.parameter("outFeatureCollection").setValue(col);
     }
     
-    private void buildTriangles(double level) throws MismatchedDimensionException, TransformException{
+    private Boundary buildTriangles(final double level, final Boundary top, final Boundary left) 
+            throws MismatchedDimensionException, TransformException{
+        
+        //the new triangle
+        final Boundary newBoundary = new Boundary();
         
         final boolean ulCorner = (UL.z == level);
         final boolean urCorner = (UR.z == level);
@@ -229,6 +276,7 @@ public class Isoline2 extends AbstractProcess {
             pushGeometry(geom, level);
         }
         
+        return newBoundary;
     }
     
     private void pushGeometry(Geometry geom, double level) throws MismatchedDimensionException, TransformException{
