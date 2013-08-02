90bda4e82a873b4a23bb7dce48082c457f335569
Quentin Boileau <quentin.boileau@geomatys.com>
2013-08-02 17:06:51 +0200

    Process vector : Update Copy process for handle versioning and some wildcards '*' in input Query.

--- modules/processing/geotk-processing-vector/src/main/java/org/geotoolkit/process/datastore/copy/CopyDescriptor.java
+++ modules/processing/geotk-processing-vector/src/main/java/org/geotoolkit/process/datastore/copy/CopyDescriptor.java
@@ -16,6 +16,7 @@
  */
 package org.geotoolkit.process.datastore.copy;
 
+import java.util.Date;
 import org.geotoolkit.data.FeatureStore;
 import org.geotoolkit.data.query.Query;
 import org.geotoolkit.parameter.DefaultParameterDescriptor;
@@ -36,13 +37,20 @@ import org.opengis.parameter.ParameterValueGroup;
  *     <li>SOURCE_STORE "source_datastore"</li>
  *     <li>TARGET_STORE "target_datastore"</li>
  *     <li>ERASE "erase" the data provider will be erased if true</li>
- *     <li>TYPE_NAME "type_name" feature type name to copy</li>
- *     <li>QUERY "query" query to use to retrieve the feature collection</li>
+ *     <li>NEW_VERSION "new_version" the data provider will create a new version if true</li>
+ *     <li>TYPE_NAME "type_name" feature type name to copy. Support a several type name separated by a comma.</li>
+ *     <li>QUERY "query" query to use to retrieve the feature collection. Support wildcard '*'
+ *     for retrieve all FeatureType of input FeatureStore and another wildcard '*' in BBOX filter query
+ *     to use default geometry name to filter features.</li>
  * </ul>
  * outputs : none
+ * <ul>
+ *     <li>VERSION "version" date of the new version created if output store support versioning.</li>
+ * </ul>
  *
  * @author Johann Sorel (Geomatys)
  * @author Cédric Briançon (Geomatys)
+ * @author Quentin Boileau (Geomatys)
  * @module pending
  */
 public final class CopyDescriptor extends VectorDescriptor {
@@ -74,17 +82,26 @@ public final class CopyDescriptor extends VectorDescriptor {
             Boolean.class, false, true);
 
     /**
+     * Mandatory - create a new version or append to last version.
+     */
+    public static final ParameterDescriptor<Boolean> NEW_VERSION =
+            new DefaultParameterDescriptor("new_version", "Create a new version or append to last version.",
+            Boolean.class, false, true);
+    
+    /**
      * Mandatory - Feature type names to  copy. several names can be passed separated by commas.
      */
     public static final ParameterDescriptor<String> TYPE_NAME =
-            new DefaultParameterDescriptor("type_name", "Name of the feature type to copy. '*' for all.",
+            new DefaultParameterDescriptor("type_name", "Name of the feature type to copy. '*' for all. Support a several type name separated by a comma.",
             String.class, "*", true);
 
     /**
      * Optional - Query to use to retrieve FeatureCollection during the process.
      */
     public static final ParameterDescriptor<Query> QUERY =
-            new DefaultParameterDescriptor("query", "Query used to get the FeatureCollection during the process.",
+            new DefaultParameterDescriptor("query", "Query used to get the FeatureCollection during the process. Support wildcard '*' "
+                    + "for retrieve all FeatureType of input FeatureStore and another wildcard '*' in BBOX filter query "
+                    + "to use default geometry name to filter features.",
             Query.class, null, false);
 
     /**
@@ -92,14 +109,21 @@ public final class CopyDescriptor extends VectorDescriptor {
      */
     public static final ParameterDescriptorGroup INPUT_DESC =
             new DefaultParameterDescriptorGroup("InputParameters",
-            new GeneralParameterDescriptor[]{SOURCE_STORE, TARGET_STORE, ERASE, TYPE_NAME, QUERY});
+            new GeneralParameterDescriptor[]{SOURCE_STORE, TARGET_STORE, ERASE, NEW_VERSION, TYPE_NAME, QUERY});
 
+    /**
+     * Optional - create version
+     */
+    public static final ParameterDescriptor<Date> VERSION = 
+            new DefaultParameterDescriptor("version", "Version date in target FeatureStore created "
+            + "during copy if supported.", Date.class, null, false);
+    
     /**
      * Output Parameters
      */
     public static final ParameterDescriptorGroup OUTPUT_DESC =
             new DefaultParameterDescriptorGroup("OutputParameters",
-            new GeneralParameterDescriptor[]{});
+            new GeneralParameterDescriptor[]{VERSION});
 
     public static final ProcessDescriptor INSTANCE = new CopyDescriptor();
 
