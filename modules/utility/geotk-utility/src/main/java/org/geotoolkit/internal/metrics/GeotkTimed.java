5bf8f47444c3821ed2b3743dec2e511bbd16d068
Olivier Nouguier <olivier.nouguier@geomatys.com>
2014-05-19 16:20:43 +0200

    ugeotk-utility: add GeotkTimed an annotation to record runtime metrics.

---
+++ modules/utility/geotk-utility/src/main/java/org/geotoolkit/internal/metrics/GeotkTimed.java
@@ -0,0 +1,11 @@
+package org.geotoolkit.internal.metrics;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.METHOD)
+public @interface GeotkTimed {
+}
