06414dfe3795d44a164ccb22394e94101369f50c
Alexis Manin <alexis.manin@geomatys.com>
2014-06-03 17:53:19 +0200

    Demos : Updated coverage reader demo to show deferred reading usage.

--- demos/geotk-demo-samples/src/main/java/org/geotoolkit/pending/demo/coverage/CoverageReaderDemo.java
+++ demos/geotk-demo-samples/src/main/java/org/geotoolkit/pending/demo/coverage/CoverageReaderDemo.java
@@ -2,12 +2,19 @@
 package org.geotoolkit.pending.demo.coverage;
 
 import java.awt.geom.Rectangle2D;
+import java.awt.image.RenderedImage;
 import java.io.File;
+import java.nio.file.Files;
+
+import net.sf.jasperreports.engine.export.Grid;
+import org.apache.sis.geometry.GeneralDirectPosition;
 import org.geotoolkit.coverage.grid.GridCoverage2D;
 import org.geotoolkit.coverage.io.CoverageIO;
 import org.geotoolkit.coverage.io.GridCoverageReadParam;
 import org.geotoolkit.coverage.io.GridCoverageReader;
 import org.geotoolkit.gui.swing.render2d.JMap2DFrame;
+import org.geotoolkit.image.iterator.PixelIterator;
+import org.geotoolkit.image.iterator.PixelIteratorFactory;
 import org.geotoolkit.map.CoverageMapLayer;
 import org.geotoolkit.map.MapBuilder;
 import org.geotoolkit.map.MapContext;
@@ -16,6 +23,8 @@ import org.geotoolkit.referencing.crs.DefaultGeographicCRS;
 import org.geotoolkit.style.DefaultStyleFactory;
 import org.geotoolkit.style.MutableStyleFactory;
 import org.geotoolkit.style.StyleConstants;
+import org.geotoolkit.util.FileUtilities;
+import org.opengis.coverage.grid.GridCoverage;
 import org.opengis.metadata.Metadata;
 
 
@@ -25,6 +34,47 @@ public class CoverageReaderDemo {
 
     public static void main(String[] args) throws Exception {
         Demos.init();
+
+        // Create a temp file to extract data from jar file.
+        final File tempData = Files.createTempFile("tempCvg", ".grb").toFile();
+        FileUtilities.buildFileFromStream(
+                CoverageReaderDemo.class.getResourceAsStream("/data/grib/Atlantic.wave.grb"), tempData);
+
+        /*
+         * DEFERRED READING
+         */
+        final GridCoverageReadParam readParam = new GridCoverageReadParam();
+        // Here is the parameter which tells the reader to perform lazy loading.
+        readParam.setDeferred(true);
+
+        final GridCoverageReader reader = CoverageIO.createSimpleReader(tempData);
+        final GridCoverage coverage = reader.read(0, readParam);
+
+        // Ok, so how to use it now ?
+
+        // You can get pixel values directly.
+        coverage.evaluate(new GeneralDirectPosition(-100, 10, 0));
+
+        // But in most cases ...
+        if (coverage instanceof GridCoverage2D) {
+            // ... You will acquire iterator for fast and safe browsing.
+            final RenderedImage cvgData = ((GridCoverage2D) coverage).getRenderedImage();
+            final PixelIterator pxIterator = PixelIteratorFactory.createDefaultIterator(cvgData);
+
+            // What should you avoid to do with deferred reading ?
+
+            //Don't asked for the entire Raster of the image, it load all image data in memory.
+            cvgData.getData();
+
+            // Do not close your coverage reader before you've ended using your coverage, it would close connexion to the source,
+            // and tile loading will return you an error.
+            reader.dispose();
+            try {
+                cvgData.getTile(cvgData.getMinTileX(), cvgData.getMinTileY());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
 //        final File input = new File("data/clouds.jpg");
 //        final GridCoverageReader reader = CoverageIO.createSimpleReader(input);
@@ -42,9 +92,12 @@ public class CoverageReaderDemo {
 //        coverage.show();
 
 
+        /*
+         * USING THE RENDERER.
+         */
         //create a mapcontext
         final MapContext context = MapBuilder.createContext();
-        final CoverageMapLayer cl = MapBuilder.createCoverageLayer(new File("data/clouds.jpg"));
+        final CoverageMapLayer cl = MapBuilder.createCoverageLayer(tempData);
         context.layers().add(cl);
 
         //display it
