072a1db58e72f04df89c42c1b7725ee3c8cd4c03
Johann Sorel <johann.sorel@geomatys.com>
2014-06-26 17:43:20 +0200

    Process : StatisticOp can now exclude NoData values from analyze

--- modules/processing/geotk-processing/src/main/java/org/geotoolkit/process/coverage/copy/StatisticOp.java
+++ modules/processing/geotk-processing/src/main/java/org/geotoolkit/process/coverage/copy/StatisticOp.java
@@ -21,9 +21,11 @@ import java.awt.image.SampleModel;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import org.geotoolkit.coverage.GridSampleDimension;
 import org.geotoolkit.coverage.grid.GridCoverage2D;
 import org.geotoolkit.coverage.io.CoverageReader;
 import org.geotoolkit.coverage.io.CoverageStoreException;
+import org.geotoolkit.coverage.io.GridCoverageReadParam;
 import org.geotoolkit.image.iterator.PixelIterator;
 import org.geotoolkit.image.iterator.PixelIteratorFactory;
 
@@ -52,7 +54,21 @@ public class StatisticOp{
      * @throws CoverageStoreException 
      */
     public static Map<String,Object> analyze(CoverageReader reader, int imageIndex) throws CoverageStoreException {
-        return analyze( ((GridCoverage2D)reader.read(imageIndex, null)).getRenderedImage() );
+        final GridCoverageReadParam param = new GridCoverageReadParam();
+        param.setDeferred(true);
+        final GridCoverage2D coverage = ((GridCoverage2D)reader.read(imageIndex, param));
+        final RenderedImage img = coverage.getRenderedImage();
+        final GridSampleDimension[] dimensions = coverage.getSampleDimensions();
+        double[][] noData = null;
+        if(dimensions!=null){
+            noData = new double[dimensions.length][0];
+            for(int i=0;i<dimensions.length;i++){
+                double[] candidate = dimensions[i].getNoDataValues();
+                if(candidate!=null) noData[i] = candidate;
+            }
+        }
+        
+        return analyze(img,noData);
     }
     
     /**
@@ -62,7 +78,23 @@ public class StatisticOp{
      * Each Entry have a name ("min", "max") and values are an double[] for each bands.
      */
     public static Map<String,Object> analyze(RenderedImage image) {
+        return analyze(image,null);
+    }
+    
+    
+    /**
+     * Analyse image to return min and max value per bands.
+     * @param image
+     * @param noData those values will be ignored in the analyze
+     * @return A Map with two Entry. 
+     * Each Entry have a name ("min", "max") and values are an double[] for each bands.
+     */
+    public static Map<String,Object> analyze(RenderedImage image, double[][] noData) {
         final Map<String,Object> analyze = new HashMap<String, Object>();
+        if(noData !=null){
+            noData = noData.clone();
+            Arrays.sort(noData);
+        }
 
         final SampleModel sm = image.getSampleModel();
         final int nbBands = sm.getNumBands();
@@ -75,7 +107,7 @@ public class StatisticOp{
         final PixelIterator pix = PixelIteratorFactory.createDefaultIterator(image);
         while (pix.next()) {
             final double d = pix.getSampleDouble();
-            if (!Double.isNaN(d)) {
+            if (!Double.isNaN(d) && (noData==null || !(Arrays.binarySearch(noData[b],d)>=0)) ) {
                 min[b] = Math.min(min[b], d);
                 max[b] = Math.max(max[b], d);
             }
