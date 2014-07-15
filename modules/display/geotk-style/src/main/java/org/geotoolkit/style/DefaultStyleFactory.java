8c5c511cda522cb36c8c1079cdf4baa98ef75bc8
amanin <alexis.manin@geomatys.com>
2014-07-15 14:43:16 +0200

    GO2 : Color literal do not need to be initialized with hexa code anymore.

--- modules/display/geotk-style/src/main/java/org/geotoolkit/style/DefaultStyleFactory.java
+++ modules/display/geotk-style/src/main/java/org/geotoolkit/style/DefaultStyleFactory.java
@@ -156,24 +156,7 @@ public class DefaultStyleFactory extends Factory implements MutableStyleFactory 
         if (color == null) {
             return null;
         }
-
-        String redCode = Integer.toHexString(color.getRed());
-        String greenCode = Integer.toHexString(color.getGreen());
-        String blueCode = Integer.toHexString(color.getBlue());
-        if (redCode.length() == 1)      redCode = "0" + redCode;
-        if (greenCode.length() == 1)    greenCode = "0" + greenCode;
-        if (blueCode.length() == 1)     blueCode = "0" + blueCode;
-
-        final String colorCode;
-        int alpha = color.getAlpha();
-        if(alpha != 255){
-            String alphaCode = Integer.toHexString(alpha);
-            if (alphaCode.length() == 1) alphaCode = "0" + alphaCode;
-            colorCode = "#" + alphaCode + redCode + greenCode + blueCode;
-        }else{
-            colorCode = "#" + redCode + greenCode + blueCode;
-        }
-        return FF.literal(colorCode.toUpperCase());
+        return FF.literal(color);
     }
 
 
