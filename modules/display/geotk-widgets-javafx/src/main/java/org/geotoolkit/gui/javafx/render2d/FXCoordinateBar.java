acd1012bd97b85a8f98bbf45512420ff0ca22559
Johann Sorel <johann.sorel@geomatys.com>
2014-07-25 17:28:17 +0200

    JavaFX : scale value in coordinate bar

--- modules/display/geotk-widgets-javafx/src/main/java/org/geotoolkit/gui/javafx/render2d/FXCoordinateBar.java
+++ modules/display/geotk-widgets-javafx/src/main/java/org/geotoolkit/gui/javafx/render2d/FXCoordinateBar.java
@@ -19,45 +19,111 @@ package org.geotoolkit.gui.javafx.render2d;
 
 import java.awt.geom.NoninvertibleTransformException;
 import java.awt.geom.Point2D;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.text.NumberFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.control.ToolBar;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
+import javafx.util.converter.LongStringConverter;
 import org.apache.sis.geometry.DirectPosition2D;
+import org.geotoolkit.gui.swing.render2d.control.JScaleCombo;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.TransformException;
 
 /**
  *
  * @author Johann Sorel (Geomatys)
  */
-public class FXCoordinateBar extends BorderPane {
+public class FXCoordinateBar extends GridPane {
     
     private static final NumberFormat NUMBER_FORMAT = NumberFormat.getNumberInstance();
     
     private final FXMap map;
+    private final PropertyChangeListener listener = new PropertyChangeListener() {
+
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            cbox.valueProperty().removeListener(action);
+            try {
+                final double scale = map.getCanvas().getGeographicScale();
+                cbox.setValue((long)scale);
+                cbox.valueProperty().addListener(action);
+            } catch (TransformException ex) {
+                Logger.getLogger(JScaleCombo.class.getName()).log(Level.WARNING, null, ex);
+            }
+        }
+    };
+    private final ChangeListener action = new ChangeListener() {
+
+        @Override
+        public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+            if (map != null) {
+                try {
+                    map.getCanvas().setGeographicScale((Long)newValue);
+                } catch (TransformException ex) {
+                    Logger.getLogger(JScaleCombo.class.getName()).log(Level.WARNING, null, ex);
+                }
+            }
+        }
+    };
+    
     
     private final ToolBar barLeft = new ToolBar();
     private final ToolBar barCenter = new ToolBar();
     private final ToolBar barRight = new ToolBar();
     
     private final Label coordText = new Label("");
+    private final ComboBox cbox = new ComboBox();
 
     public FXCoordinateBar(FXMap map) {
         this.map = map;
-        barLeft.setMinSize(0, 0);
-        barLeft.setPrefSize(0, 0);
-        barRight.setMinSize(0, 0);
-        barRight.setPrefSize(0, 0);
-        setLeft(barLeft);
-        setCenter(barCenter);
-        setRight(barRight);
+        barLeft.setMaxHeight(Double.MAX_VALUE);
+        barCenter.setMaxHeight(Double.MAX_VALUE);
+        barRight.setMaxHeight(Double.MAX_VALUE);
+        
+        add(barLeft, 0, 0);
+        add(barCenter, 1, 0);
+        add(barRight, 2, 0);
+        
+        
+        final ColumnConstraints col0 = new ColumnConstraints();
+        final ColumnConstraints col1 = new ColumnConstraints();
+        final ColumnConstraints col2 = new ColumnConstraints();
+        col1.setHgrow(Priority.ALWAYS);
+        final RowConstraints row0 = new RowConstraints();
+        row0.setVgrow(Priority.ALWAYS);
+        getColumnConstraints().addAll(col0,col1,col2);
+        getRowConstraints().addAll(row0);
         
         barCenter.getItems().add(coordText);
         
+        cbox.getItems().addAll(  1000l,
+                                 5000l,
+                                20000l,
+                                50000l,
+                               100000l,
+                               500000l);
+        cbox.setEditable(true);
+        cbox.setConverter(new LongStringConverter());
+        
+        barRight.getItems().add(cbox);
+        
         map.addEventHandler(MouseEvent.ANY, new myListener());
+        
+        if (this.map != null) {
+            this.map.getCanvas().addPropertyChangeListener(listener);
+        }        
     }
     
     
