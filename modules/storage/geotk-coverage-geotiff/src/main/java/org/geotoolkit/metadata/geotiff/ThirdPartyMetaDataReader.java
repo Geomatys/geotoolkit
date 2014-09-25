409a1b0de4808833886a9d8a956a3578e0239122
rmarechal <rmarechal@pythagore.(none)>
2014-09-25 18:14:35 +0200

    Adapt grid sample dimension creation in case where metadata information are missing

--- modules/storage/geotk-coverage-geotiff/src/main/java/org/geotoolkit/metadata/geotiff/ThirdPartyMetaDataReader.java
+++ modules/storage/geotk-coverage-geotiff/src/main/java/org/geotoolkit/metadata/geotiff/ThirdPartyMetaDataReader.java
@@ -17,7 +17,6 @@
 
 package org.geotoolkit.metadata.geotiff;
 
-import java.awt.Color;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -36,7 +35,6 @@ import org.geotoolkit.coverage.Category;
 import org.geotoolkit.coverage.GridSampleDimension;
 import org.geotoolkit.image.io.metadata.SpatialMetadata;
 import org.geotoolkit.internal.image.io.DimensionAccessor;
-import org.geotoolkit.resources.Vocabulary;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -52,7 +50,8 @@ import org.xml.sax.SAXException;
  * http://www.awaresystems.be/imaging/tiff/tifftags/private.html
  * http://www.awaresystems.be/imaging/tiff/tifftags/privateifd.html
  * 
- * @author Johann Sorel (Geomatys)
+ * @author Johann Sorel  (Geomatys)
+ * @author Marechal Remi (Geomatys).
  */
 public class ThirdPartyMetaDataReader {
 
@@ -70,16 +69,20 @@ public class ThirdPartyMetaDataReader {
      */
     public void fillSpatialMetaData(SpatialMetadata metadata) throws IOException{
         
-        
         final List<Category> categories = new ArrayList<>();
+        Category noDataCategory = null;
         Double realFillValue = null;
         
+        int samplePerPixels = -1;
+        int bitsPerSamples = -1;
+        int sampleFormat = -1;
+        
         final NodeList children = root.getChildNodes();
-        for(int i=0,n=children.getLength();i<n;i++){
+        for(int i=0, n=children.getLength(); i<n; i++){
             final IIOMetadataNode child = (IIOMetadataNode) children.item(i);
             final int number = Integer.valueOf(child.getAttribute("number"));
             
-            switch(number){
+            switch (number) {
 
                 //GDAL tags
                 case 42112 :  {//metadatas as xml
@@ -94,53 +97,112 @@ public class ThirdPartyMetaDataReader {
                         final NodeList lst = doc.getElementsByTagName("Item");
                         for(int k=0,kn=lst.getLength();k<kn;k++){
                             final Element node = (Element) lst.item(k);
-                            final String name = node.getAttribute("name");
-                            if("STATISTICS_MINIMUM".equals(name)){
+                            final String name  = node.getAttribute("name");
+                            if ("STATISTICS_MINIMUM".equals(name)) {
                                 min = Double.valueOf(node.getTextContent());
-                            }else if("STATISTICS_MAXIMUM".equals(name)){
+                            } else if ("STATISTICS_MAXIMUM".equals(name)) {
                                 max = Double.valueOf(node.getTextContent());
+                            }
+                            if (min != null && max != null) {
+                                categories.add(new Category("data", null, 
+                                        NumberRange.create(1, true, 100, true), NumberRange.create(min, true, max, true)));
                             }
                         }
                     } catch (ParserConfigurationException | SAXException | NumberFormatException ex) {
                         LOGGER.log(Level.WARNING, ex.getMessage(), ex);
                     }
-                    
-                    if(min!=null && max!=null){
-                        categories.add(new Category("data", null, 
-                                NumberRange.create(1, true, 100, true), NumberRange.create(min, true, max, true)));
-                    }
-                    
-                    }break;
+                    break;
+                }
                 case 42113 : {// no data value as ascii text
                     final Node valueNode = child.getChildNodes().item(0);
                     final String str = GeoTiffMetaDataUtils.readTiffAsciis(valueNode);
                     try {
                         realFillValue = Double.valueOf(str);
-                        categories.add(Category.NODATA);
+                        noDataCategory = Category.NODATA;
                     } catch (NumberFormatException e) {
                         LOGGER.log(Level.INFO, "No data value cannot be read.", e);
                     }
                     //categories.add(new Category(Vocabulary.formatInternational(Vocabulary.Keys.NODATA), new Color(0,0,0,0), noData));
-                    }break;
+                    break;
+                }
+                case 258 : { //-- bits per sample
+                    final Node bpsNode = child.getChildNodes().item(0);
+                    bitsPerSamples = (int) GeoTiffMetaDataUtils.readTiffLongs(bpsNode)[0];
+                    break;
+                }
+                case 277 : { //-- samples per pixel
+                    final Node sppNode = child.getChildNodes().item(0);
+                    samplePerPixels = (int) GeoTiffMetaDataUtils.readTiffLongs(sppNode)[0];
+                    break;
+                }
+                case 339 : { //-- sample format 
+                    final Node sfNode = child.getChildNodes().item(0);
+                    sampleFormat = (int) GeoTiffMetaDataUtils.readTiffLongs(sfNode)[0];
+                    break;
+                }
             }
-            
         }
         
+        assert bitsPerSamples  != -1;
+        assert samplePerPixels != -1;
         
-        if(!categories.isEmpty()){
-            final DimensionAccessor accessor = new DimensionAccessor(metadata);
-            if(accessor.childCount()>0){
-                accessor.selectChild(0);
-            }else{
-                accessor.selectChild(accessor.appendChild());
-                final GridSampleDimension dim = new GridSampleDimension("samples", categories.toArray(new Category[0]), null);
-                accessor.setDimension(dim, Locale.ENGLISH);
+        final DimensionAccessor accessor = new DimensionAccessor(metadata);
+        final int categoriArrayLength = (noDataCategory != null) ? 2 : 1;
+        if (categories.isEmpty()) {
+            double min;
+            double max;
+            switch (bitsPerSamples) {
+                case Byte.SIZE : {
+                    min = 0;
+                    max = 255;
+                    break;
+                }
+                case Short.SIZE : {
+                    min = 0;
+                    max = 0xFFFF;
+                    break;
+                }
+                case Integer.SIZE : {
+                    if (sampleFormat == 3) {
+                        //-- Float
+                        min = Float.MIN_VALUE;
+                        max = Float.MAX_VALUE;
+                    } else {
+                        //-- integer
+                        min = Integer.MIN_VALUE;
+                        max = Integer.MAX_VALUE;
+                    }
+                    break;
+                }
+                case Double.SIZE : {
+                    min = Double.MIN_VALUE;
+                    max = Double.MAX_VALUE;
+                    break;
+                }
+                default : throw new IllegalStateException("Unknow sample type");
             }
-            if(realFillValue!=null){
+            for (int s = 0; s < samplePerPixels; s++) {
+                categories.add(new Category("data", null, 
+                                        NumberRange.create(1, true, 100, true), NumberRange.create(min, true, max, true)));
+            }
+        } 
+        
+        final int categoriesNumber = categories.size();
+        for (int c = 0; c < categoriesNumber; c++) {
+            if (accessor.childCount() >= categoriesNumber) {
+                accessor.selectChild(c); 
+            } else {
+                accessor.selectChild(accessor.appendChild());
+            }
+
+            final Category[] cats = new Category[categoriArrayLength];
+            cats[0] = categories.get(c);
+            if (noDataCategory != null) cats[1] = noDataCategory;
+            final GridSampleDimension dim = new GridSampleDimension("samples", cats, null);
+            accessor.setDimension(dim, Locale.ENGLISH);
+            if (realFillValue != null) {
                 accessor.setAttribute("realFillValue", realFillValue);
             }
-        }
-                
+        }    
     }
-    
 }
