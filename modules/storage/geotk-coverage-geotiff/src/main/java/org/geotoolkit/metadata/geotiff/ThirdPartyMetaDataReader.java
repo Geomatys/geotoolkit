e2071fc472761e643c523a1aa95e89208e20e2c9
rmarechal <rmarechal@pythagore.(none)>
2014-09-26 10:29:17 +0200

    Geotiff : correcting grid sample dimension 2

--- modules/storage/geotk-coverage-geotiff/src/main/java/org/geotoolkit/metadata/geotiff/ThirdPartyMetaDataReader.java
+++ modules/storage/geotk-coverage-geotiff/src/main/java/org/geotoolkit/metadata/geotiff/ThirdPartyMetaDataReader.java
@@ -143,6 +143,8 @@ public class ThirdPartyMetaDataReader {
             }
         }
         
+        if (categories.isEmpty() && noDataCategory == null) return;
+        
         assert bitsPerSamples  != -1;
         assert samplePerPixels != -1;
         
