71a2c4e22ba0b19d7721475d25b0875baa9b3ade
Johann Sorel <johann.sorel@geomatys.com>
2014-12-08 16:37:33 +0100

    JavaFX : add TextField completion

---
+++ modules/display/geotk-widgets-javafx/src/main/java/org/geotoolkit/gui/javafx/util/TextFieldCompletion.java
@@ -0,0 +1,127 @@
+/*
+ *    Geotoolkit - An Open Source Java GIS Toolkit
+ *    http://www.geotoolkit.org
+ *
+ *    (C) 2014, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+
+package org.geotoolkit.gui.javafx.util;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Point2D;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Popup;
+
+/**
+ * Auto-completion for textfield.
+ * 
+ * @author Johann Sorel (Geomatys)
+ */
+public class TextFieldCompletion {
+    
+    private final TextField textField;
+    private final Popup popup = new Popup();
+    private final ListView<String> list = new ListView<>();
+    private boolean moveCaretToPos = false;
+    private int caretPos;
+
+    public TextFieldCompletion(final TextField textField) {
+        this.textField = textField;
+        this.textField.setOnKeyPressed((KeyEvent t)->popup.hide());
+        this.textField.setOnKeyReleased(this::onKeyPress);
+        
+        popup.setAutoHide(true);
+        popup.getContent().add(new ScrollPane(list));
+        
+        list.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        list.setOnKeyPressed((KeyEvent event) -> {
+            if(event.getCode()==KeyCode.ENTER){
+                final String val = list.getSelectionModel().getSelectedItem();
+                if(val!=null){
+                    textField.setText(val);
+                }
+                popup.hide();
+            }
+        });
+    }
+    
+    protected ObservableList<String> getChoices(String text){
+        final ObservableList lst = FXCollections.observableArrayList();
+        return lst;
+    }
+    
+    private void onKeyPress(KeyEvent event) {
+        final KeyCode code = event.getCode();
+        final String text = textField.getText();
+        final ObservableList baseData = getChoices(text);
+        
+        final Point2D popupPos = textField.localToScreen(0, textField.getHeight());
+        
+        if (code == KeyCode.UP) {
+            caretPos = -1;
+            moveCaret(text.length());
+            return;
+        } else if (code == KeyCode.DOWN) {
+            if (!popup.isShowing()) {
+                popup.show(textField,popupPos.getX(),popupPos.getY());
+            }
+            caretPos = -1;
+            moveCaret(text.length());
+            return;
+        } else if (code == KeyCode.BACK_SPACE) {
+            moveCaretToPos = true;
+            caretPos = textField.getCaretPosition();
+        } else if (code == KeyCode.DELETE) {
+            moveCaretToPos = true;
+            caretPos = textField.getCaretPosition();
+        }
+        if (code == KeyCode.RIGHT || code == KeyCode.LEFT || event.isControlDown() || code == KeyCode.HOME || code == KeyCode.END || code == KeyCode.TAB) {
+            return;
+        }
+        
+        final String searchText = textField.getText().toLowerCase();
+        final ObservableList dataList = FXCollections.observableArrayList(
+                baseData.filtered((Object t) -> String.valueOf(t).toLowerCase().startsWith(searchText))
+        );
+        
+        list.setItems(dataList);
+        list.getSelectionModel().clearSelection();
+        textField.setText(text);
+        if (!moveCaretToPos) {
+            caretPos = -1;
+        }
+        
+        moveCaret(text.length());
+        if (!dataList.isEmpty()) {
+            popup.show(textField,popupPos.getX(),popupPos.getY());
+        }else{
+            popup.hide();
+        }
+    }
+
+    private void moveCaret(int textLength) {
+        if (caretPos == -1) {
+            textField.positionCaret(textLength);
+        } else {
+            textField.positionCaret(caretPos);
+        }
+        moveCaretToPos = false;
+    }
+    
+}
