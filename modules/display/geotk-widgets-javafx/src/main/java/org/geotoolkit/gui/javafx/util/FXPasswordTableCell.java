a95ff94c5394dfdf92c405b80ac3a49673a9691d
Samuel Andrés <samuel.andres@geomatys.com>
2015-01-20 17:57:10 +0100

    JavaFX widgets : changes to EnumTableCell, PasswordTableCell.

--- modules/display/geotk-widgets-javafx/src/main/java/org/geotoolkit/gui/javafx/util/FXPasswordTableCell.java
+++ modules/display/geotk-widgets-javafx/src/main/java/org/geotoolkit/gui/javafx/util/FXPasswordTableCell.java
@@ -16,13 +16,12 @@
  */
 package org.geotoolkit.gui.javafx.util;
 
-import java.util.Arrays;
-import javafx.collections.FXCollections;
+import java.security.MessageDigest;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TableCell;
 
 /**
@@ -31,28 +30,40 @@ import javafx.scene.control.TableCell;
  * @param <S>
  * @param <T>
  */
-public class FXEnumTableCell<S, T extends Enum> extends TableCell<S, T> {
-    private final ComboBox<T> field = new ComboBox<>();
+public class FXPasswordTableCell<S> extends TableCell<S, String>{
+    
+    private final PasswordField field = new PasswordField();
+    private final MessageDigest messageDigest;
 
-    public FXEnumTableCell() {
+    public FXPasswordTableCell() {
+        this(null);
+    }
+
+    public FXPasswordTableCell(final MessageDigest messageDigest) {
+        setGraphic(field);
+        setAlignment(Pos.CENTER);
+        setContentDisplay(ContentDisplay.CENTER);
         field.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent event) {
-                commitEdit(field.getValue());
+                commitEdit(field.getText());
             }
         });
-        setGraphic(field);
-        setAlignment(Pos.CENTER);
-        setContentDisplay(ContentDisplay.CENTER);
+        this.messageDigest=messageDigest;
+    }
+    
+    private String digest(final String toEncrypt){
+        if(messageDigest==null) return toEncrypt;
+        else {
+            return new String(messageDigest.digest(toEncrypt.getBytes()));
+        }
     }
 
     @Override
     public void startEdit() {
-        T value = getItem();
-        T[] values = (T[]) value.getClass().getEnumConstants();
-        field.setItems(FXCollections.observableArrayList(Arrays.asList(values)));
-        field.getSelectionModel().select(value);
+        String value = (String) getItem();
+        field.setText(value);
         super.startEdit();
         setText(null);
         setGraphic(field);
@@ -60,10 +71,10 @@ public class FXEnumTableCell<S, T extends Enum> extends TableCell<S, T> {
     }
 
     @Override
-    public void commitEdit(T newValue) {
-        itemProperty().set(newValue);
-        super.commitEdit(newValue);
-        updateItem(newValue, false);
+    public void commitEdit(String newValue) {
+        final String digest = digest(newValue);
+        itemProperty().set(digest);
+        super.commitEdit(digest);
     }
 
     @Override
@@ -73,7 +84,7 @@ public class FXEnumTableCell<S, T extends Enum> extends TableCell<S, T> {
     }
 
     @Override
-    protected void updateItem(T item, boolean empty) {
+    protected void updateItem(String item, boolean empty) {
         super.updateItem(item, empty);
         setText(null);
         setGraphic(null);
