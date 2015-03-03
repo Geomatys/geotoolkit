5617ce0aeb1ef65b198e97989d53a59ab7ae342c
Johann Sorel <johann.sorel@geomatys.com>
2015-03-03 15:26:17 +0100

    Feature : add prefix and pattern operation property type

---
+++ modules/feature/geotk-feature/src/main/java/org/geotoolkit/feature/op/PatternOperation.java
@@ -0,0 +1,94 @@
+/*
+ *    Geotoolkit - An Open Source Java GIS Toolkit
+ *    http://www.geotoolkit.org
+ *
+ *    (C) 2015, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotoolkit.feature.op;
+
+import java.util.Collections;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.sis.feature.DefaultAttributeType;
+import org.apache.sis.feature.DefaultOperation;
+import org.apache.sis.parameter.DefaultParameterDescriptorGroup;
+import org.apache.sis.util.iso.Names;
+import org.opengis.feature.Attribute;
+import org.opengis.feature.AttributeType;
+import org.opengis.feature.Feature;
+import org.opengis.parameter.ParameterValueGroup;
+import org.opengis.util.GenericName;
+
+/**
+ * Pattern concatenation operation.
+ * 
+ * @author Johann Sorel (Geomatys)
+ */
+public class PatternOperation extends DefaultOperation{
+
+    private static final DefaultParameterDescriptorGroup PARAMS =
+            new DefaultParameterDescriptorGroup(Collections.singletonMap("name", "noargs"), 0, 1);
+    private static final AttributeType RESULTTYPE = new DefaultAttributeType(Collections.singletonMap("name", "value"), String.class, 1, 1, null);
+    
+    private final GenericName[] refs;
+    private final Pattern pattern;
+
+    public PatternOperation(String identification, String regex, String ... referenceProperties) {
+        this(Names.createLocalName(null, ":", identification),regex,toNameArray(referenceProperties));
+    }
+
+    public PatternOperation(GenericName identification, String regex, GenericName ... referneceProperties) {
+        super(Collections.singletonMap("name", identification), PARAMS, RESULTTYPE);
+        this.pattern = Pattern.compile(regex);
+        this.refs = referneceProperties;
+    }
+
+    private static GenericName[] toNameArray(String ... referenceProperties){
+        final GenericName[] names = new GenericName[referenceProperties.length];
+        for(int i=0;i<referenceProperties.length;i++){
+            names[i] = Names.createLocalName(null, ":", referenceProperties[i]);
+        }
+        return names;
+    }
+
+    public GenericName[] getReferenceProperties() {
+        return refs;
+    }
+
+    public Attribute invoke(Feature feature, ParameterValueGroup parameters){
+        final Attribute att = RESULTTYPE.newInstance();
+        final Object[] values = new Object[refs.length];
+        for(int i=0;i<refs.length;i++) values[i] = feature.getPropertyValue(refs[i].toString());
+        att.setValue(invoke(values));
+        return att;
+    }
+
+    public String invoke(Object[] refPropertyValues){
+        //TODO
+        throw new UnsupportedOperationException("Waiting for operation interface to be finalized before implementation.");
+    }
+
+    public String[] reverse(String value){
+        final String[] res = new String[refs.length];
+        if(value==null || value.isEmpty()) return res;
+
+        final Matcher m = pattern.matcher(value);
+        if (m.matches()) {
+            for(int i=0;i<refs.length;i++){
+                res[i] = m.group(i+1);
+            }
+        }
+        return res;
+    }
+
+}
---
+++ modules/feature/geotk-feature/src/main/java/org/geotoolkit/feature/op/PrefixAndSuffixOperation.java
@@ -0,0 +1,90 @@
+/*
+ *    Geotoolkit - An Open Source Java GIS Toolkit
+ *    http://www.geotoolkit.org
+ *
+ *    (C) 2015, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotoolkit.feature.op;
+
+import java.util.Collections;
+import org.apache.sis.feature.DefaultAttributeType;
+import org.apache.sis.feature.DefaultOperation;
+import org.apache.sis.parameter.DefaultParameterDescriptorGroup;
+import org.apache.sis.util.iso.Names;
+import org.opengis.feature.Attribute;
+import org.opengis.feature.AttributeType;
+import org.opengis.feature.Feature;
+import org.opengis.parameter.ParameterValueGroup;
+import org.opengis.util.GenericName;
+
+/**
+ * Simple operation which concatenant another property with a prefix and a suffix.
+ *
+ * @author Johann Sorel (Geomatys)
+ */
+public class PrefixAndSuffixOperation extends DefaultOperation{
+
+    private static final DefaultParameterDescriptorGroup PARAMS =
+            new DefaultParameterDescriptorGroup(Collections.singletonMap("name", "noargs"), 0, 1);
+    private static final AttributeType RESULTTYPE = new DefaultAttributeType(Collections.singletonMap("name", "value"), String.class, 1, 1, null);
+
+    private final String prefix;
+    private final String suffix;
+    private final GenericName ref;
+
+    public PrefixAndSuffixOperation(String identification, String referenceProperty, String prefix, String suffix) {
+        this(Names.createLocalName(null, ":", identification), Names.createLocalName(null, ":", referenceProperty),prefix,suffix);
+    }
+
+    public PrefixAndSuffixOperation(GenericName identification, GenericName referenceProperty, String prefix, String suffix) {
+        super(Collections.singletonMap("name", identification), PARAMS, RESULTTYPE);
+        this.prefix = prefix;
+        this.suffix = suffix;
+        this.ref = referenceProperty;
+    }
+
+    public GenericName getReferenceProperty() {
+        return ref;
+    }
+
+    public Attribute invoke(Feature feature, ParameterValueGroup parameters){
+        final Attribute att = RESULTTYPE.newInstance();
+        att.setValue(invoke(feature.getPropertyValue(ref.toString())));
+        return att;
+    }
+
+    public String invoke(Object refPropertyValue){
+        if(prefix==null){
+            return new StringBuilder(String.valueOf(refPropertyValue)).append(suffix).toString();
+        }else if(suffix==null){
+            return new StringBuilder(prefix).append(refPropertyValue).toString();
+        }else{
+            return new StringBuilder(prefix).append(refPropertyValue).append(suffix).toString();
+        }
+    }
+
+    public String reverse(String value){
+        if(value==null || value.isEmpty()) return value;
+
+        if(prefix!=null && value.startsWith(prefix)){
+            value = value.substring(prefix.length());
+        }
+
+        if(suffix!=null && value.endsWith(suffix)){
+            value = value.substring(0, value.length()-suffix.length());
+        }
+
+        return value;
+    }
+
+}
