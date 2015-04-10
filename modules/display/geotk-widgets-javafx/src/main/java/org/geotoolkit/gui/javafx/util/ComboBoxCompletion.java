bfca2173b74e244dab234ca55696f60ed01225cb
Alexis Manin <alexis.manin@geomatys.com>
2015-04-10 16:09:06 +0200

    JavaFX : refactor combo-box completion to update on combo-box list change.

--- modules/display/geotk-widgets-javafx/src/main/java/org/geotoolkit/gui/javafx/util/ComboBoxCompletion.java
+++ modules/display/geotk-widgets-javafx/src/main/java/org/geotoolkit/gui/javafx/util/ComboBoxCompletion.java
@@ -18,81 +18,95 @@
 package org.geotoolkit.gui.javafx.util;
 
 import java.util.regex.Pattern;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 /**
  * Auto-completion for combobox.
+ * Perform text auto-completion on textfields of editable combo-box. combo-box 
+ * {@link ComboBox#items } are adapted  based on user typing.
  * 
  * @author Johann Sorel (Geomatys)
+ * @author Alexis Manin (Geomatys)
  */
 public class ComboBoxCompletion {
     
     private final ComboBox comboBox;
-    private final ObservableList baseData;
-    private boolean moveCaretToPos = false;
-    private int caretPos;
+    private ObservableList baseData;
+    private ObservableList filteredData;
 
     public ComboBoxCompletion(final ComboBox comboBox) {
         this.comboBox = comboBox;
-        this.baseData = comboBox.getItems();
-        this.comboBox.setOnKeyPressed((KeyEvent t)->comboBox.hide());
         this.comboBox.setOnKeyReleased(this::onKeyPress);
+        baseData = comboBox.getItems();
+        comboBox.itemsProperty().addListener(this::updateBaseData);
     }
 
+    /**
+     * Update available {@link ComboBox#items} according to typed value.
+     * @param event 
+     */
     private void onKeyPress(KeyEvent event) {
-        final KeyCode code = event.getCode();
-        final String text = comboBox.getEditor().getText();
-        
-        if (code == KeyCode.UP) {
-            caretPos = -1;
-            moveCaret(text.length());
+        if (baseData == null || baseData.isEmpty()) {
             return;
-        } else if (code == KeyCode.DOWN) {
+        }
+        
+        final KeyCode code = event.getCode();
+        if (event.isControlDown() || code == KeyCode.HOME || code == KeyCode.END || code == KeyCode.TAB) {
+            return;
+        }
+        
+        final TextField editor = comboBox.getEditor();
+        final int caretPosition = editor.getCaretPosition();
+        if (code == KeyCode.DOWN) {
             if (!comboBox.isShowing()) {
                 comboBox.show();
+                editor.positionCaret(caretPosition);
             }
-            caretPos = -1;
-            moveCaret(text.length());
-            return;
-        } else if (code == KeyCode.BACK_SPACE) {
-            moveCaretToPos = true;
-            caretPos = comboBox.getEditor().getCaretPosition();
-        } else if (code == KeyCode.DELETE) {
-            moveCaretToPos = true;
-            caretPos = comboBox.getEditor().getCaretPosition();
-        }
-        if (code == KeyCode.RIGHT || code == KeyCode.LEFT || event.isControlDown() || code == KeyCode.HOME || code == KeyCode.END || code == KeyCode.TAB) {
-            return;
-        }
-        
-        final Pattern searchPattern = Pattern.compile("(?i)"+comboBox.getEditor().getText());
-        final ObservableList list = FXCollections.observableArrayList(
-                baseData.filtered((Object t) -> searchPattern.matcher(comboBox.getConverter().toString(t)).find())
-        );
-        
-        comboBox.setItems(list);
-        comboBox.getEditor().setText(text);
-        if (!moveCaretToPos) {
-            caretPos = -1;
-        }
-        
-        moveCaret(text.length());
-        if (!list.isEmpty()) {
-            comboBox.show();
-        }
-    }
-
-    private void moveCaret(int textLength) {
-        if (caretPos == -1) {
-            comboBox.getEditor().positionCaret(textLength);
+            
         } else {
-            comboBox.getEditor().positionCaret(caretPos);
+            final String completeText = editor.getText();
+            final String currentText = editor.getText(0, caretPosition);
+            if (currentText != null) {
+                comboBox.hide(); // hide to force resize when we'll have to show it back.
+                final Pattern searchPattern = buildPattern(currentText);
+                filteredData = FXCollections.observableArrayList(
+                        baseData.filtered((Object t) -> searchPattern.matcher(comboBox.getConverter().toString(t)).find()));
+                comboBox.setItems(filteredData);
+                if (!filteredData.isEmpty()) {
+                    comboBox.show();
+                }
+                // we're forced to set back editor text, because change of items 
+                // also change it.
+                editor.setText(completeText);
+                editor.positionCaret(caretPosition);
+            }
         }
-        moveCaretToPos = false;
     }
     
+    private void updateBaseData(ObservableValue observable, Object oldValue, Object newValue) {
+        if (comboBox.getItems() != filteredData) {
+            baseData = comboBox.getItems();
+        }
+    }
+    
+    /**
+     * Adapt text typed in editor to build a permissive regex to search for  
+     * correspondance in combo box items.
+     * @param fieldText The text to transform in regex pattern.
+     * @return 
+     */
+    public static Pattern buildPattern(final String fieldText) {
+        final String pattern = fieldText
+                // We do not need groups in this particular case, so we assume user has real parenthesis in his text. 
+                .replaceAll("(\\()|(\\))", "\\$1")
+                // In case user mistype word separation (Ex : typed "Point10" instead of "Point 10" or "type-1" instead of "type - 1").
+                .replaceAll("([a-zA-Z]+|\\d+|\\W+)\\s*", "$1\\\\s*");
+        return Pattern.compile("(?i)"+pattern);                
+    }
 }
