37a53dda4b107bb06a08f962fde2b9c5b3424133
Johann Sorel <johann.sorel@geomatys.com>
2015-06-22 10:21:45 +0200

    fix(XML Parsing): add xsd feature store documentation

--- modules/jaxp-xml-parser/geotk-jaxp-gml/src/main/java/org/geotoolkit/feature/xml/XSDFeatureStore.java
+++ modules/jaxp-xml-parser/geotk-jaxp-gml/src/main/java/org/geotoolkit/feature/xml/XSDFeatureStore.java
@@ -6,10 +6,25 @@ import org.apache.sis.storage.DataStoreException;
 import org.opengis.util.GenericName;
 
 /**
+ * Feature stores may have some specific XSD definitions.
+ * The base featuretype to XSD transformer do not produce the exact same XSD 
+ * as the original, this is because some informations are xml specific and are
+ * lost when mapped to feature types.
  *
  * @author Guilhem Legal (Geomatys)
+ * @author Johann Sorel (Geomatys)
  */
 public interface XSDFeatureStore {
+
+    /**
+     * Get a list of namespace -> location to add in the generated xsd.
+     * The values can be a Strinc, in this case it is a location where is xsd can be found.
+     * Or it can be an Schema object.
+     *
+     * @param name type name
+     * @return
+     * @throws DataStoreException
+     */
+    Map<String,?> getSchema(final GenericName name) throws DataStoreException;
     
-    Map<String,String> getSchema(final GenericName name) throws DataStoreException;
 }
