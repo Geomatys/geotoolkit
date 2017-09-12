c98a2783f99facfefc2ff959017daa3abf0bae97
Johann Sorel <johann.sorel@geomatys.com>
2017-09-12 16:56:10 +0200

    feat(Storage): implement add,removeIf,replaceIf method on DefaultFeatureResource, fallback on FeatureStore methods

--- modules/storage/geotk-feature-store/src/main/java/org/geotoolkit/data/DefaultFeatureResource.java
+++ modules/storage/geotk-feature-store/src/main/java/org/geotoolkit/data/DefaultFeatureResource.java
@@ -19,17 +19,23 @@ package org.geotoolkit.data;
 import java.util.Iterator;
 import java.util.Spliterator;
 import java.util.Spliterators;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 import org.apache.sis.storage.DataStoreException;
+import org.apache.sis.storage.ReadOnlyStorageException;
+import org.apache.sis.util.ArgumentChecks;
 import org.geotoolkit.data.query.Query;
 import org.geotoolkit.data.query.QueryBuilder;
 import org.geotoolkit.data.query.QueryUtilities;
+import org.geotoolkit.feature.FeatureExt;
 import org.geotoolkit.storage.AbstractFeatureSet;
 import org.geotoolkit.storage.StorageListener;
 import org.geotoolkit.util.NamesExt;
 import org.opengis.feature.Feature;
 import org.opengis.feature.FeatureType;
+import org.opengis.filter.Filter;
 import org.opengis.util.GenericName;
 
 /**
@@ -70,6 +76,57 @@ public final class DefaultFeatureResource extends AbstractFeatureSet implements 
         final Spliterator<Feature> spliterator = Spliterators.spliteratorUnknownSize((Iterator)reader, Spliterator.ORDERED);
         final Stream<Feature> stream = StreamSupport.stream(spliterator, false);
         return stream.onClose(reader::close);
+    }
+
+    /**
+     * This method fallback on {@link FeatureStore}
+     * @param features
+     * @throws ReadOnlyStorageException
+     * @throws DataStoreException
+     */
+    @Override
+    public void add(Iterator<? extends Feature> features) throws ReadOnlyStorageException, DataStoreException {
+        try (final FeatureWriter writer = store.getFeatureWriter(QueryBuilder.filtered(query.getTypeName(), Filter.EXCLUDE))) {
+            while (features.hasNext()) {
+                FeatureExt.copy(features.next(), writer.next(), true);
+                writer.write();
+            }
+        }
+    }
+
+    @Override
+    public boolean removeIf(Predicate<? super Feature> filter) throws ReadOnlyStorageException, DataStoreException {
+        ArgumentChecks.ensureNonNull("predicate", filter);
+        boolean removed = false;
+        try (final FeatureWriter writer = store.getFeatureWriter(query)) {
+            while (writer.hasNext()) {
+                Feature feature = writer.next();
+                if (filter.test(feature)) {
+                    writer.remove();
+                    removed = true;
+                }
+            }
+        }
+        return removed;
+    }
+
+    @Override
+    public void replaceIf(Predicate<? super Feature> filter, UnaryOperator<Feature> updater) throws ReadOnlyStorageException, DataStoreException {
+        ArgumentChecks.ensureNonNull("predicate", filter);
+        ArgumentChecks.ensureNonNull("updater", updater);
+        try (final FeatureWriter writer = store.getFeatureWriter(query)) {
+            while (writer.hasNext()) {
+                Feature feature = writer.next();
+                if (filter.test(feature)) {
+                    feature = updater.apply(feature);
+                    if (feature == null) {
+                       writer.remove();
+                    } else {
+                        writer.write();
+                    }
+                }
+            }
+        }
     }
 
     /**
