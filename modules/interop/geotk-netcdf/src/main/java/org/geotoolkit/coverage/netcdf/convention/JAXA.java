4f8e3d073654d7ab5e5660770c75d025cedb7181
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2019-02-22 20:17:51 +0100

    chore(Storage): more attributes to take in account.

--- modules/interop/geotk-netcdf/src/main/java/org/geotoolkit/coverage/netcdf/convention/JAXA.java
+++ modules/interop/geotk-netcdf/src/main/java/org/geotoolkit/coverage/netcdf/convention/JAXA.java
@@ -16,10 +16,12 @@
  */
 package org.geotoolkit.coverage.netcdf.convention;
 
+import java.util.Map;
 import org.apache.sis.internal.netcdf.Convention;
 import org.apache.sis.internal.netcdf.Decoder;
 import org.apache.sis.internal.netcdf.Variable;
 import org.apache.sis.referencing.operation.transform.TransferFunction;
+import org.apache.sis.measure.NumberRange;
 
 import ucar.nc2.constants.ACDD;
 
@@ -33,6 +35,15 @@ import ucar.nc2.constants.ACDD;
 public final class JAXA extends Convention {
     private static final String SATELLITE_KEY = "Satellite";
     private static final String SATELLITE_VAL = "Global Change Observation Mission - Climate (GCOM-C)";
+
+    private static final String[] NO_DATA = {
+        "Error_DN",
+        "Land_DN",
+        "Cloud_error_DN",
+        "Retrieval_error_DN"
+    };
+
+    private static final String SUFFIX = "_DN";
 
     @Override
     protected boolean isApplicableTo(final Decoder decoder) {
@@ -79,12 +90,42 @@ public final class JAXA extends Convention {
     }
 
     @Override
+    public NumberRange<?> validRange(final Variable data) {
+        NumberRange<?> range = super.validRange(data);
+        if (range == null) {
+            final double min = data.getAttributeAsNumber("Minimum_valid_DN");
+            final double max = data.getAttributeAsNumber("Maximum_valid_DN");
+            if (Double.isFinite(min) && Double.isFinite(max)) {
+                range = NumberRange.createBestFit(min, true, max, true);
+            }
+        }
+        return range;
+    }
+
+    @Override
+    public Map<Number,Object> nodataValues(final Variable data) {
+        final Map<Number, Object> pads = super.nodataValues(data);
+        for (String name : NO_DATA) {
+            final double value = data.getAttributeAsNumber(name);
+            if (Double.isFinite(value)) {
+                if (name.endsWith(SUFFIX)) {
+                    name = name.substring(0, name.length() - SUFFIX.length());
+                }
+                pads.put(value, name.replace('_', ' '));
+            }
+        }
+        return pads;
+    }
+
+    @Override
     public TransferFunction transferFunction(final Variable source) {
         final TransferFunction tr = super.transferFunction(source);
-        final double slope  = source.getAttributeAsNumber("Slope");
-        final double offset = source.getAttributeAsNumber("Offset");
-        if (Double.isFinite(slope))  tr.setOffset(slope);
-        if (Double.isFinite(offset)) tr.setOffset(offset);
+        if (tr.isIdentity()) {
+            final double slope  = source.getAttributeAsNumber("Slope");
+            final double offset = source.getAttributeAsNumber("Offset");
+            if (Double.isFinite(slope))  tr.setScale (slope);
+            if (Double.isFinite(offset)) tr.setOffset(offset);
+        }
         return tr;
     }
 }
