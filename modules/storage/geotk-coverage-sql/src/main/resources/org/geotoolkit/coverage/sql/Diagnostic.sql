d78ff86cfe706edda862b0ac2cedafa8ce8c6d9c
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2019-04-19 17:32:08 +0200

    feat(Storage): list duplicated series and prune them.

--- modules/storage/geotk-coverage-sql/src/main/resources/org/geotoolkit/coverage/sql/Diagnostic.sql
+++ modules/storage/geotk-coverage-sql/src/main/resources/org/geotoolkit/coverage/sql/Diagnostic.sql
@@ -3,11 +3,18 @@
 -- This query should return an empty result.
 --
 SELECT "format", "band", "name", "lower", "upper", "scale", "offset" FROM rasters."Categories"
-  GROUP BY ("format", "band", "name", "lower", "upper", "scale", "offset") HAVING COUNT("format") > 1
+  GROUP BY ("format", "band", "name", "lower", "upper", "scale", "offset") HAVING COUNT("format") > 1;
 
 --
 -- Lists prefixes of formats that are repeated many times
 -- (not necessarily with the exact same sample dimensions).
 --
 SELECT REVERSE(SUBSTRING(REVERSE("name"), POSITION('-' IN REVERSE("name")))) AS "prefix", "driver", COUNT(*)
-  FROM rasters."Formats" WHERE "name" SIMILAR TO '%-\d+' GROUP BY "prefix", "driver"
+  FROM rasters."Formats" WHERE "name" SIMILAR TO '%-\d+' GROUP BY "prefix", "driver";
+
+--
+-- Lists duplicated Series
+--
+SELECT MIN("identifier") AS "id", "product", "dataset", "directory", "extension", "format", "comments"
+  FROM rasters."Series" GROUP BY ("product", "dataset", "directory", "extension", "format", "comments")
+  HAVING COUNT(*) > 1;
