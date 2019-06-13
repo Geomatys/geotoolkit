775e903900cc1499c3ae299bc7357643ec6b47c8
Alexis Manin <alexis.manin@gmail.com>
2019-06-13 12:49:13 +0200

    fix(Processing): fix drift predictor test.

--- modules/processing/geotk-processing/src/main/java/org/geotoolkit/processing/science/drift/v2/SimpleUVSource.java
+++ modules/processing/geotk-processing/src/main/java/org/geotoolkit/processing/science/drift/v2/SimpleUVSource.java
@@ -3,16 +3,6 @@
  */
 package org.geotoolkit.processing.science.drift.v2;
 
-import java.awt.Point;
-import java.awt.geom.Point2D;
-import java.awt.image.Raster;
-import java.awt.image.RenderedImage;
-import java.time.Instant;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
-import java.util.logging.Level;
-import javax.vecmath.Vector2d;
 import org.apache.sis.coverage.SampleDimension;
 import org.apache.sis.coverage.grid.GridCoverage;
 import org.apache.sis.coverage.grid.GridDerivation;
@@ -27,7 +17,6 @@ import org.apache.sis.storage.DataStoreException;
 import org.apache.sis.storage.GridCoverageResource;
 import org.apache.sis.util.ArgumentChecks;
 import org.apache.sis.util.collection.BackingStoreException;
-import static org.geotoolkit.processing.science.drift.v2.Utilities.*;
 import org.opengis.coverage.PointOutsideCoverageException;
 import org.opengis.geometry.DirectPosition;
 import org.opengis.geometry.Envelope;
@@ -36,12 +25,22 @@ import org.opengis.referencing.crs.SingleCRS;
 import org.opengis.referencing.crs.TemporalCRS;
 import org.opengis.referencing.crs.VerticalCRS;
 import org.opengis.referencing.datum.PixelInCell;
-import org.opengis.referencing.operation.CoordinateOperation;
-import org.opengis.referencing.operation.MathTransform;
-import org.opengis.referencing.operation.MathTransform1D;
-import org.opengis.referencing.operation.MathTransform2D;
-import org.opengis.referencing.operation.TransformException;
+import org.opengis.referencing.operation.*;
 import org.opengis.util.FactoryException;
+
+import javax.vecmath.Vector2d;
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.awt.image.Raster;
+import java.awt.image.RenderedImage;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.logging.Level;
+
+import static org.apache.sis.coverage.grid.GridRoundingMode.ENCLOSING;
+import static org.geotoolkit.processing.science.drift.v2.Utilities.*;
 
 /**
  * TODO : add logic to re-use previously queried Calibration2D and Snapshot objects when same slices are used multiple
@@ -253,6 +252,7 @@ class SimpleUVSource implements UVSource {
                 final RenderedImage snapshotImg;
                 try {
                     GridGeometry snapshotGeom = sliceGeom.derive()
+                            .rounding(ENCLOSING)
                             .subgrid(target)
                             .build()
                             .reduce(horizontalAxisIdx, horizontalAxisIdx+1);
