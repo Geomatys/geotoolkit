03a0f2c66908baccb86cb1880156c7e70a00a7b3
jsorel <johann.sorel@geomatys.com>
2019-08-20 11:11:23 +0200

    feat(GML): update XmlFeatureSet API

--- modules/jaxp-xml-parser/geotk-jaxp-gml/src/main/java/org/geotoolkit/feature/xml/XmlFeatureSet.java
+++ modules/jaxp-xml-parser/geotk-jaxp-gml/src/main/java/org/geotoolkit/feature/xml/XmlFeatureSet.java
@@ -3,10 +3,9 @@ package org.geotoolkit.feature.xml;
 
 import java.util.Map;
 import org.apache.sis.storage.DataStoreException;
-import org.opengis.util.GenericName;
 
 /**
- * Feature stores may have some specific XSD definitions.
+ * Data stores resources may have some specific XSD definitions.
  * The base featuretype to XSD transformer do not produce the exact same XSD
  * as the original, this is because some informations are xml specific and are
  * lost when mapped to feature types.
@@ -14,17 +13,16 @@ import org.opengis.util.GenericName;
  * @author Guilhem Legal (Geomatys)
  * @author Johann Sorel (Geomatys)
  */
-public interface XSDFeatureStore {
+public interface XmlFeatureSet {
 
     /**
      * Get a list of namespace -> location to add in the generated xsd.
-     * The values can be a Strinc, in this case it is a location where is xsd can be found.
+     * The values can be a String, in this case it is a location where is xsd can be found.
      * Or it can be an Schema object.
      *
-     * @param name type name
      * @return
      * @throws DataStoreException
      */
-    Map<String,?> getSchema(final GenericName name) throws DataStoreException;
+    Map<String,?> getSchema() throws DataStoreException;
 
 }
