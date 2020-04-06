e769813d1de4bbd24ed0c112f58ffbddc1a27e7a
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2020-04-06 22:16:18 +0200

    Remove bicubic interpolation tests. The code removed in this commit migrated to the "feat/bicubic" branch of Apache SIS, to be merged to master after more tests and debugging have been done.
    https://issues.apache.org/jira/browse/SIS-494

--- modules/storage/geotk-coverage-imagery/src/test/java/org/geotoolkit/image/interpolation/ResampleTest.java
+++ modules/storage/geotk-coverage-imagery/src/test/java/org/geotoolkit/image/interpolation/ResampleTest.java
@@ -34,7 +34,6 @@ import org.opengis.referencing.NoSuchAuthorityCodeException;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.opengis.referencing.crs.ProjectedCRS;
 import org.opengis.referencing.operation.MathTransform;
-import org.opengis.referencing.operation.NoninvertibleTransformException;
 import org.opengis.referencing.operation.TransformException;
 import org.opengis.util.FactoryException;
 
@@ -45,31 +44,9 @@ import org.opengis.util.FactoryException;
  */
 public class ResampleTest extends org.geotoolkit.test.TestBase {
     /**
-     * Expected result about bicubic interpolation.
-     */
-    private static double[] BICUBIC_Result = new double[]{0.0, 0.0,                0.0,                0.0,                0.0,                0.0,                0.0,                0.0,                0.0,                0.0, 0.0, 0.0,
-                                                          0.0, 1.0,                1.0,                1.0,                1.0,                  1,                  1,                  1,                  1,                  1,   1, 0.0,
-                                                          0.0, 1.0, 1.1975308641975309, 1.3456790123456792, 1.4444444444444444, 1.4938271604938276, 1.4938271604938276, 1.4444444444444444, 1.3456790123456788, 1.1975308641975306,   1, 0.0,
-                                                          0.0, 1.0,  1.345679012345679, 1.6049382716049385, 1.7777777777777781, 1.8641975308641983, 1.8641975308641983, 1.7777777777777781,  1.604938271604938,  1.345679012345679,   1, 0.0,
-                                                          0.0, 1.0, 1.4444444444444444,  1.777777777777778,                  2, 2.1111111111111125, 2.1111111111111125,                  2, 1.7777777777777772, 1.4444444444444446,   1, 0.0,
-                                                          0.0,   1, 1.4938271604938271, 1.8641975308641974, 2.1111111111111116, 2.2345679012345694, 2.2345679012345694, 2.1111111111111116, 1.8641975308641967, 1.4938271604938274,   1, 0.0,
-                                                          0.0,   1,  1.493827160493827, 1.8641975308641971, 2.1111111111111116,   2.23456790123457,   2.23456790123457, 2.1111111111111116, 1.8641975308641963, 1.4938271604938274,   1, 0.0,
-                                                          0.0,   1,  1.444444444444444, 1.7777777777777772,                  2,  2.111111111111114,  2.111111111111114,                  2,  1.777777777777776, 1.4444444444444446,   1, 0.0,
-                                                          0.0,   1, 1.3456790123456783, 1.6049382716049374, 1.7777777777777781,    1.8641975308642,    1.8641975308642, 1.7777777777777788, 1.6049382716049356,  1.345679012345679,   1, 0.0,
-                                                          0.0,   1,   1.19753086419753, 1.3456790123456774, 1.4444444444444446, 1.4938271604938305, 1.4938271604938305,  1.444444444444445, 1.3456790123456754, 1.1975308641975306,   1, 0.0,
-                                                          0.0,   1,                  1,                  1,                  1,                  1,                  1,                  1,                  1,                  1,   1, 0.0,
-                                                          0.0, 0.0,                0.0,                0.0,                0.0,                0.0,                0.0,                0.0,                0.0,                0.0, 0.0, 0.0};
-
-
-    /**
      * Transformation applicate to source or destination image to considered a pixel orientation center.
      */
-    private static MathTransform pixelInCellCenter = new AffineTransform2D(1, 0, 0, 1, 0.5, 0.5);
-
-    /**
-     * Transformation applicate on source image for resampling.
-     */
-    private MathTransform mathTransform;
+    private static final MathTransform pixelInCellCenter = new AffineTransform2D(1, 0, 0, 1, 0.5, 0.5);
 
     /**
      * Destination image.
@@ -102,53 +79,6 @@ public class ResampleTest extends org.geotoolkit.test.TestBase {
         raster.setSample(0, 2, 0, 1);
         raster.setSample(1, 2, 0, 1);
         raster.setSample(2, 2, 0, 1);
-    }
-
-    /**
-     * Test result obtained from biCubic interpolation and a resampling.
-     */
-    @Test
-    public void jaiBiCubicTest() throws NoninvertibleTransformException, FactoryException, TransformException {
-        final ColorSpace cs = ColorSpace.getInstance(ColorSpace.CS_GRAY);
-        final ColorModel cm = new ComponentColorModel(cs, new int[]{Double.SIZE}, false, false, Transparency.OPAQUE, DataBuffer.TYPE_DOUBLE);
-        final ImageTypeSpecifier imgTypeSpec = new ImageTypeSpecifier(cm, cm.createCompatibleSampleModel(1, 1));
-        sourceImg = imgTypeSpec.createBufferedImage(4, 4);
-        final WritableRaster raster = sourceImg.getWritableTile(0, 0);
-        //band0
-        raster.setSample(0, 0, 0, 1);
-        raster.setSample(1, 0, 0, 1);
-        raster.setSample(2, 0, 0, 1);
-        raster.setSample(3, 0, 0, 1);
-
-        raster.setSample(0, 1, 0, 1);
-        raster.setSample(1, 1, 0, 2);
-        raster.setSample(2, 1, 0, 2);
-        raster.setSample(3, 1, 0, 1);
-
-        raster.setSample(0, 2, 0, 1);
-        raster.setSample(1, 2, 0, 2);
-        raster.setSample(2, 2, 0, 2);
-        raster.setSample(3, 2, 0, 1);
-
-        raster.setSample(0, 3, 0, 1);
-        raster.setSample(1, 3, 0, 1);
-        raster.setSample(2, 3, 0, 1);
-        raster.setSample(3, 3, 0, 1);
-
-        setTargetImage(12, 12, DataBuffer.TYPE_DOUBLE, -1000);
-
-        mathTransform = MathTransforms.concatenate(pixelInCellCenter, new AffineTransform2D(3, 0, 0, 3, 0, 0), pixelInCellCenter.inverse());
-
-        /*
-         * Resampling
-         */
-        final Resample resample = new Resample(mathTransform.inverse(), targetImage, sourceImg,
-                InterpolationCase.BICUBIC, ResampleBorderComportement.FILL_VALUE, new double[]{0});
-        resample.fillImage();
-        final Raster coverageRaster = targetImage.getTile(0, 0);
-
-        java.awt.image.DataBufferDouble datadouble = (java.awt.image.DataBufferDouble) coverageRaster.getDataBuffer();
-        assertArrayEquals(BICUBIC_Result, datadouble.getData(0), 1E-9);
     }
 
     /**
