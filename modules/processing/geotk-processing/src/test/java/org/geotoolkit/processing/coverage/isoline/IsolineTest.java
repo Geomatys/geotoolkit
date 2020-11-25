c45b908dff6bd21bbac87d03ef2ba2dbf34dad2f
jsorel <johann.sorel@geomatys.com>
2020-11-25 15:52:42 +0100

    test(Process): add tiled image isoline test case

--- modules/processing/geotk-processing/src/test/java/org/geotoolkit/processing/coverage/isoline/IsolineTest.java
+++ modules/processing/geotk-processing/src/test/java/org/geotoolkit/processing/coverage/isoline/IsolineTest.java
@@ -16,30 +16,46 @@
  */
 package org.geotoolkit.processing.coverage.isoline;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.WritableRaster;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.apache.sis.coverage.SampleDimension;
 import org.apache.sis.coverage.grid.GridCoverage;
 import org.apache.sis.coverage.grid.GridCoverageBuilder;
 import org.apache.sis.coverage.grid.GridExtent;
 import org.apache.sis.coverage.grid.GridGeometry;
+import org.apache.sis.coverage.grid.GridOrientation;
 import org.apache.sis.geometry.GeneralEnvelope;
+import org.apache.sis.internal.coverage.j2d.WritableTiledImage;
 import org.apache.sis.internal.feature.AttributeConvention;
 import org.apache.sis.internal.referencing.j2d.AffineTransform2D;
 import org.apache.sis.referencing.CommonCRS;
+import org.apache.sis.storage.DataStoreException;
 import org.apache.sis.storage.FeatureSet;
 import org.apache.sis.storage.GridCoverageResource;
 import org.geotoolkit.image.BufferedImages;
 import org.geotoolkit.process.ProcessDescriptor;
+import org.geotoolkit.process.ProcessException;
 import org.geotoolkit.process.ProcessFinder;
 import org.geotoolkit.processing.GeotkProcessingRegistry;
 import org.geotoolkit.storage.feature.FeatureStoreUtilities;
 import org.geotoolkit.storage.memory.InMemoryGridCoverageResource;
+import org.geotoolkit.test.Assert;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.io.WKTReader;
+import org.locationtech.jts.operation.linemerge.LineMerger;
 import org.opengis.feature.Feature;
 import org.opengis.parameter.ParameterValueGroup;
 import org.opengis.referencing.datum.PixelInCell;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.util.NoSuchIdentifierException;
 
 /**
  *
@@ -47,6 +63,11 @@ import org.opengis.referencing.datum.PixelInCell;
  */
 public class IsolineTest extends org.geotoolkit.test.TestBase {
 
+    /**
+     * Test an isoline created arount a central point.
+     *
+     * @throws Exception
+     */
     @Test
     public void test() throws Exception{
 
@@ -55,7 +76,7 @@ public class IsolineTest extends org.geotoolkit.test.TestBase {
         env.setRange(1, 0, 3);
 
         final GridCoverageBuilder gcb = new GridCoverageBuilder();
-        gcb.setDomain(new GridGeometry(new GridExtent(3, 3), env));
+        gcb.setDomain(new GridGeometry(new GridExtent(3, 3), env, GridOrientation.HOMOTHETY));
         gcb.setValues(BufferedImages.toDataBuffer1D(new float[][]{
             {100,100,100},
             {100,200,100},
@@ -72,8 +93,11 @@ public class IsolineTest extends org.geotoolkit.test.TestBase {
         procparams.parameter("inIntervals").setValue(new double[]{150});
         final org.geotoolkit.process.Process process = desc.createProcess(procparams);
         final ParameterValueGroup result = process.call();
-        FeatureSet col = (FeatureSet) result.parameter("outFeatureCollection").getValue();
+        final FeatureSet col = (FeatureSet) result.parameter("outFeatureCollection").getValue();
         assertEquals(1l, FeatureStoreUtilities.getCount(col, true).longValue());
+        final Feature feature = col.features(true).findFirst().get();
+        final LineString geom = (LineString) feature.getPropertyValue(AttributeConvention.GEOMETRY);
+        assertEquals("LINESTRING (1 1.5, 1.5 2, 2 1.5, 1.5 1, 1 1.5)", geom.toText());
 
     }
 
@@ -85,7 +109,7 @@ public class IsolineTest extends org.geotoolkit.test.TestBase {
         env.setRange(1, 0, 3);
 
         final GridCoverageBuilder gcb = new GridCoverageBuilder();
-        gcb.setDomain(new GridGeometry(new GridExtent(3, 3), env));
+        gcb.setDomain(new GridGeometry(new GridExtent(3, 3), env, GridOrientation.HOMOTHETY));
         gcb.setValues(BufferedImages.toDataBuffer1D(new float[][]{
             {10,10,20},
             {10,10,20},
@@ -181,6 +205,71 @@ public class IsolineTest extends org.geotoolkit.test.TestBase {
         assertTrue(geom instanceof LineString);
         LineString line = (LineString) geom;
         assertEquals("LINESTRING (1.5 2.7515672842277548, 1.6830860462828303 2.916666666666667, 2.5 3.5590333533118175, 3.5 3.765209494383988, 4.5 3.8283104640195758, 4.771314100938448 4.083333333333334, 4.5 4.484307738208203, 3.5 5.01232266160712, 2.5 5.131933362127943, 1.5039371438387097 5.25)", line.toText());
+    }
+
+    /**
+     * Test isoline creation on a tiled image.
+     */
+    @Test
+    public void testTiledImage() throws NoSuchIdentifierException, ProcessException, DataStoreException, ParseException {
+
+        /* create a tiled image with 4 tiles
+              0 1   2 3
+            +-----+-----+
+          0 | 0 0 | 0 0 |
+          1 | 0 1 | 1 0 |
+            +-----+-----+
+          2 | 0 1 | 1 0 |
+          3 | 0 0 | 0 0 |
+            +-----+-----+
+            Result geometry is an octogone, a square with cutted edges
+        */
+        final BufferedImage tile00 = BufferedImages.createImage(2, 2, 1, DataBuffer.TYPE_DOUBLE);
+        final BufferedImage tile10 = BufferedImages.createImage(2, 2, 1, DataBuffer.TYPE_DOUBLE);
+        final BufferedImage tile01 = BufferedImages.createImage(2, 2, 1, DataBuffer.TYPE_DOUBLE);
+        final BufferedImage tile11 = BufferedImages.createImage(2, 2, 1, DataBuffer.TYPE_DOUBLE);
+        final WritableRaster raster00 = tile00.getRaster();
+        final WritableRaster raster10 = tile10.getRaster().createWritableTranslatedChild(2, 0);
+        final WritableRaster raster01 = tile01.getRaster().createWritableTranslatedChild(0, 2);
+        final WritableRaster raster11 = tile11.getRaster().createWritableTranslatedChild(2, 2);
+        raster00.setSample(1+0, 1+0, 0, 1);
+        raster10.setSample(0+2, 1+0, 0, 1);
+        raster01.setSample(1+0, 0+2, 0, 1);
+        raster11.setSample(0+2, 0+2, 0, 1);
+
+        final WritableTiledImage img = new WritableTiledImage(null, tile00.getColorModel(), 4, 4, 0, 0, new WritableRaster[]{raster00, raster10, raster01, raster11});
+
+        final MathTransform gridtoCrs = new AffineTransform2D(1, 0, 0, 1, 0, 0);
+        final GridCoverageBuilder gcb = new GridCoverageBuilder();
+        gcb.setDomain(new GridGeometry(new GridExtent(4, 4), PixelInCell.CELL_CENTER, gridtoCrs, CommonCRS.WGS84.normalizedGeographic()));
+        gcb.setValues(img);
+        gcb.setRanges(new SampleDimension.Builder().setName(0).build());
+
+        final GridCoverage coverage = gcb.build();
+        final GridCoverageResource ref = new InMemoryGridCoverageResource(coverage);
+
+        final ProcessDescriptor desc = ProcessFinder.getProcessDescriptor(GeotkProcessingRegistry.NAME, IsolineDescriptor.NAME);
+        final ParameterValueGroup procparams = desc.getInputDescriptor().createValue();
+        procparams.parameter("inCoverageRef").setValue(ref);
+        procparams.parameter("inIntervals").setValue(new double[]{0.5});
+        final org.geotoolkit.process.Process process = desc.createProcess(procparams);
+        final ParameterValueGroup result = process.call();
+        final FeatureSet col = (FeatureSet) result.parameter("outFeatureCollection").getValue();
+        final List<Feature> features = col.features(false).collect(Collectors.toList());
+        assertEquals(4, features.size());
+        final LineString geom1 = (LineString) features.get(0).getPropertyValue(AttributeConvention.GEOMETRY);
+        final LineString geom2 = (LineString) features.get(1).getPropertyValue(AttributeConvention.GEOMETRY);
+        final LineString geom3 = (LineString) features.get(2).getPropertyValue(AttributeConvention.GEOMETRY);
+        final LineString geom4 = (LineString) features.get(3).getPropertyValue(AttributeConvention.GEOMETRY);
+        LineMerger merger = new LineMerger();
+        merger.add(geom1);
+        merger.add(geom2);
+        merger.add(geom3);
+        merger.add(geom4);
+        Collection merged = merger.getMergedLineStrings();
+        Assert.assertEquals(1, merged.size());
+        final Geometry geometry = (Geometry) merged.iterator().next();
+        assertTrue(new WKTReader().read("LINESTRING (1 0.5, 2 0.5, 2.5 1, 2.5 2, 2 2.5, 1 2.5, 0.5 2, 0.5 1, 1 0.5)").equalsTopo(geometry));
     }
 
 }
