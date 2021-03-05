623309505627a48d56e1c868c034d5fe4898c008
glegal <guilhem.legal@geomatys.com>
2021-03-05 09:46:47 +0100

    feat(XML Bindings): add WCS 2 CRS extension binding

---
+++ modules/jaxb-xml-binding/geotk-xml-wcs/src/main/java/org/geotoolkit/wcs/xml/v200/crs/CrsType.java
@@ -0,0 +1,133 @@
+/*
+ *    Geotoolkit - An Open Source Java GIS Toolkit
+ *    http://www.geotoolkit.org
+ *
+ *    (C) 2021, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotoolkit.wcs.xml.v200.crs;
+
+import java.util.Objects;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * CRSs for the request bounding box and for the result coverage.
+ *
+ * <p>
+ * Classe Java pour CrsType complex type.
+ *
+ * <p>
+ * Le fragment de schéma suivant indique le contenu attendu figurant dans cette
+ * classe.
+ *
+ * <pre>
+ * &lt;complexType name="CrsType">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="subsettingCrs" type="{http://www.w3.org/2001/XMLSchema}anyURI" minOccurs="0"/>
+ *         &lt;element name="outputCrs" type="{http://www.w3.org/2001/XMLSchema}anyURI" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ *
+ *
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "CrsType", propOrder = {
+    "subsettingCrs",
+    "outputCrs"
+})
+@XmlRootElement(name = "Crs")
+public class CrsType {
+
+    @XmlSchemaType(name = "anyURI")
+    private String subsettingCrs;
+    @XmlSchemaType(name = "anyURI")
+    private String outputCrs;
+
+    public CrsType() {
+
+    }
+
+    public CrsType(String subsettingCrs, String outputCrs) {
+        this.outputCrs = outputCrs;
+        this.subsettingCrs = subsettingCrs;
+    }
+
+    /**
+     * Obtient la valeur de la propriété subsettingCrs.
+     *
+     * @return possible object is {@link String }
+     *
+     */
+    public String getSubsettingCrs() {
+        return subsettingCrs;
+    }
+
+    /**
+     * Définit la valeur de la propriété subsettingCrs.
+     *
+     * @param value allowed object is {@link String }
+     *
+     */
+    public void setSubsettingCrs(String value) {
+        this.subsettingCrs = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété outputCrs.
+     *
+     * @return possible object is {@link String }
+     *
+     */
+    public String getOutputCrs() {
+        return outputCrs;
+    }
+
+    /**
+     * Définit la valeur de la propriété outputCrs.
+     *
+     * @param value allowed object is {@link String }
+     *
+     */
+    public void setOutputCrs(String value) {
+        this.outputCrs = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o instanceof CrsType && super.equals(o)) {
+            final CrsType that = (CrsType) o;
+            return Objects.equals(this.outputCrs, that.outputCrs)
+                    && Objects.equals(this.subsettingCrs, that.subsettingCrs);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 41 * hash + java.util.Objects.hashCode(this.outputCrs);
+        hash = 41 * hash + java.util.Objects.hashCode(this.subsettingCrs);
+        return hash;
+    }
+}
---
+++ modules/jaxb-xml-binding/geotk-xml-wcs/src/main/java/org/geotoolkit/wcs/xml/v200/crs/ObjectFactory.java
@@ -0,0 +1,87 @@
+/*
+ *    Geotoolkit - An Open Source Java GIS Toolkit
+ *    http://www.geotoolkit.org
+ *
+ *    (C) 2021, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotoolkit.wcs.xml.v200.crs;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+/**
+ * This object contains factory methods for each Java content interface and Java
+ * element interface generated in the net.opengis.wcs_service_extension_crs._1
+ * package.
+ * <p>
+ * An ObjectFactory allows you to programatically construct new instances of the
+ * Java representation for XML content. The Java representation of XML content
+ * can consist of schema derived interfaces and classes representing the binding
+ * of schema type definitions, element declarations and model groups. Factory
+ * methods for each of these are provided in this class.
+ *
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _CrsMetadata_QNAME = new QName("http://www.opengis.net/wcs_service-extension_crs/1.0", "CrsMetadata");
+    private final static QName _Crs_QNAME = new QName("http://www.opengis.net/wcs_service-extension_crs/1.0", "Crs");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of
+     * schema derived classes for package:
+     * net.opengis.wcs_service_extension_crs._1
+     *
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link CrsType }
+     *
+     */
+    public CrsType createCrsType() {
+        return new CrsType();
+    }
+
+    /**
+     * Create an instance of {@link CrsMetadataType }
+     *
+     */
+    public CrsMetadataType createCrsMetadataType() {
+        return new CrsMetadataType();
+    }
+
+    /**
+     * Create an instance of
+     * {@link JAXBElement }{@code <}{@link CrsMetadataType }{@code >}}
+     *
+     */
+    @XmlElementDecl(namespace = "http://www.opengis.net/wcs_service-extension_crs/1.0", name = "CrsMetadata")
+    public JAXBElement<CrsMetadataType> createCrsMetadata(CrsMetadataType value) {
+        return new JAXBElement<>(_CrsMetadata_QNAME, CrsMetadataType.class, null, value);
+    }
+
+    /**
+     * Create an instance of
+     * {@link JAXBElement }{@code <}{@link CrsType }{@code >}}
+     *
+     */
+    @XmlElementDecl(namespace = "http://www.opengis.net/wcs_service-extension_crs/1.0", name = "Crs")
+    public JAXBElement<CrsType> createCrs(CrsType value) {
+        return new JAXBElement<>(_Crs_QNAME, CrsType.class, null, value);
+    }
+
+}
