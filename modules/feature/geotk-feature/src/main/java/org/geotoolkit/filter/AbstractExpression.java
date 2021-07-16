8d7fcde41b4737da125214d276af4a0ec156b935
Alexis Manin <alexis.manin@geomatys.com>
2021-07-16 14:47:19 +0200

    fix(Feature): provide a default implementation for expression value type conversion
    This is a temporary workaround due to the lack of type-safety of the implementation

--- modules/feature/geotk-feature/src/main/java/org/geotoolkit/filter/AbstractExpression.java
+++ modules/feature/geotk-feature/src/main/java/org/geotoolkit/filter/AbstractExpression.java
@@ -18,11 +18,13 @@
 package org.geotoolkit.filter;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.xml.bind.annotation.XmlTransient;
 import org.apache.sis.util.ObjectConverters;
 import org.apache.sis.util.UnconvertibleObjectException;
 import org.apache.sis.util.iso.Names;
 import org.opengis.filter.Expression;
+import org.opengis.filter.InvalidFilterValueException;
 import org.opengis.util.LocalName;
 import org.opengis.util.ScopedName;
 
@@ -58,6 +60,45 @@ public abstract class AbstractExpression implements Expression<Object,Object>, S
 
     @Override
     public <N> Expression<Object,N> toValueType(Class<N> type) {
-        throw new UnsupportedOperationException();
+        // TODO: that is a brut-force workaround to overcome the lack of type-safety at this level
+        return new Conversion<>(type);
+    }
+
+    /**
+     * A fallback to allow expression result conversion.
+     * Once all expression implementations are properly typed, this workaround should not be needed anymore.
+     *
+     * @param <V> Output value type
+     */
+    private class Conversion<V> implements Expression<Object, V> {
+
+        private final Class<V> valueType;
+
+        private Conversion(Class<V> valueType) {
+            this.valueType = valueType;
+        }
+
+        @Override
+        public ScopedName getFunctionName() {
+            return AbstractExpression.this.getFunctionName();
+        }
+
+        @Override
+        public List<Expression<? super Object, ?>> getParameters() {
+            return AbstractExpression.this.getParameters();
+        }
+
+        @Override
+        public V apply(Object input) throws InvalidFilterValueException {
+            final Object baseValue = AbstractExpression.this.apply(input);
+            if (baseValue == null) return (V) baseValue;
+
+            return ObjectConverters.convert(baseValue, valueType);
+        }
+
+        @Override
+        public <N1> Expression<Object, N1> toValueType(Class<N1> type) {
+            return new Conversion<>(type);
+        }
     }
 }
