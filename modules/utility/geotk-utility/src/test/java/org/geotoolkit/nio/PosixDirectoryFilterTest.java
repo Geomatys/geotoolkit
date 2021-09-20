5e3b775f38821fab80eef4190be37cb103cbce25
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2021-09-20 15:11:50 +0200

    Provide a remplacement for the `OS` internal class removed in SIS.

--- modules/utility/geotk-utility/src/test/java/org/geotoolkit/nio/PosixDirectoryFilterTest.java
+++ modules/utility/geotk-utility/src/test/java/org/geotoolkit/nio/PosixDirectoryFilterTest.java
@@ -7,7 +7,7 @@ import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.sis.internal.system.OS;
+import org.geotoolkit.internal.io.OS;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
