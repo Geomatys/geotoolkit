961eef52e66d67b8d5c0a014851936f5c3116309
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2022-03-01 10:22:00 +0100

    Add a comment about a problem that will need to be fixed later.

--- geotk-coverage-sql/src/main/java/org/geotoolkit/coverage/sql/ProductSubset.java
+++ geotk-coverage-sql/src/main/java/org/geotoolkit/coverage/sql/ProductSubset.java
@@ -71,6 +71,11 @@ final class ProductSubset extends AbstractGridResource {
         extent.addElements(areaOfInterest);
         final ExtentSelector<GridCoverageEntry> selector = new ExtentSelector<>(extent);
         selector.setTimeGranularity(product.temporalResolution);
+        /*
+         * TODO: need a better criteria for deciding when to seet `alternateOrdering` to true.
+         * It could be  (requested time range) > (non-overlapping time range between images).
+         * We want `false` when the requested time range is small (sometime 1 ms is used).
+         */
         selector.alternateOrdering = true;
         entries.forEach(entry -> entry.submitTo(selector));
         GridCoverageEntry result = selector.best();
